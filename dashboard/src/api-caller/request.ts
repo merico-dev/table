952149102537c996659eb15c17b7e72ccdfb0bb7
Leto <gerile.tu@merico.dev>
2022-05-19 22:41:25 +0800

    refactor: simplify temp request builder

--- dashboard/src/api-caller/request.ts
+++ dashboard/src/api-caller/request.ts
@@ -24,22 +24,6 @@ const getRequest = (method: Method) => {
 
     return axios(conf)
       .then((res: any) => {
-        if (typeof res.data !== 'object') {
-          console.error('数据格式响应错误:', res.data)
-          alert('前方拥挤，请刷新再试')
-          return Promise.reject(res)
-        }
-        if (res.data.errcode) {
-          if (res.data.errcode == 401) {
-            window.location.href = 'login' // 登录失效跳转登录页
-            return
-          }
-          // silent 选项，错误不提示
-          if (res.data.message && !options.silent)
-            alert(res.data.message)
-          return Promise.reject(res.data)
-        }
-
         return res.data
       })
       .catch((err: any) => {
