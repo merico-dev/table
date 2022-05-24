f90d99d390052e68dea35dc1c857904bedf57ee0
Leto <gerile.tu@merico.dev>
2022-05-25 01:09:00 +0800

    chore: faster build & leaner bundle for dashboard core (pt.2)

--- dashboard/packages/core/vite.config.ts
+++ dashboard/packages/core/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import visualizer from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [
@@ -18,6 +19,9 @@ export default defineConfig({
       fileName: (format) => `dashboard.${format}.js`,
     },
     rollupOptions: {
+      plugins: [
+        visualizer()
+      ],
       external: [
         '@mantine/core',
         '@mantine/dates',
@@ -25,17 +29,20 @@ export default defineConfig({
         '@mantine/hooks',
         '@mantine/notifications',
         '@mantine/prism',
+        'ahooks',
         'axios',
         'echarts/core',
         'echarts/charts',
         'echarts/components',
         'echarts/renderers',
-        'echarts-for-react',
+        'echarts-for-react/lib/core',
         'echarts-gl',
         'lodash',
         'numbro',
         'react',
         'react-dom',
+        'react-grid-layout',
+        'react-hook-form',
         'tabler-icons-react',
       ],
       output: {
@@ -46,17 +53,20 @@ export default defineConfig({
           '@mantine/hooks': '@mantine/hooks',
           '@mantine/notifications': '@mantine/notifications',
           '@mantine/prism': '@mantine/prism',
+          ahooks: 'ahooks',
           'axios': 'axios',
           'echarts/core': 'echarts/core',
           'echarts/charts': 'echarts/charts',
           'echarts/components': 'echarts/components',
           'echarts/renderers': 'echarts/renderers',
-          'echarts-for-react': 'echarts-for-react',
+          'echarts-for-react/lib/core': 'echarts-for-react/lib/core',
           'echarts-gl': 'echarts-gl',
           lodash: '_',
           numbro: 'numbro',
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react-grid-layout': 'react-grid-layout',
+          'react-hook-form': 'react-hook-form',
           'tabler-icons-react': 'tabler-icons-react',
         },
       },
