95772bd8180ba7afaa44445355fd74e8d5090285
Leto <gerile.tu@merico.dev>
2022-06-01 22:59:13 +0800

    feat: call /dashboard/details in demo

--- dashboard/packages/demo/src/api-caller/dashboard.tsx
+++ dashboard/packages/demo/src/api-caller/dashboard.tsx
@@ -1,3 +1,5 @@
+import { IDashboard } from "@devtable/dashboard";
+import { normalizeDBDashboard } from "./dashboard.transform";
 import { IDBDashboard, PaginationResponse } from "./dashboard.typed";
 import { post } from "./request";
 
@@ -5,5 +7,9 @@ export const DashboardAPI = {
   list: async (): Promise<PaginationResponse<IDBDashboard>> => {
     const res = await post('/dashboard/list', {})
     return res;
+  },
+  details: async (id: string): Promise<IDashboard> => {
+    const res = await post('/dashboard/details', { id })
+    return normalizeDBDashboard(res);
   }
 }
--- dashboard/packages/demo/src/app/dashboard-demo.tsx
+++ dashboard/packages/demo/src/app/dashboard-demo.tsx
@@ -3,13 +3,20 @@ import React from 'react';
 import { Dashboard, ContextInfoContext, initialContextInfoContext, IDashboard } from '@devtable/dashboard'
 import { Filters } from '../components/filters';
 
-import { DEMO_COMMIT_DASHBOARD } from './demo-data';
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 import './index.css'
+import { useRequest } from 'ahooks';
+import { DashboardAPI } from '../api-caller/dashboard';
+import { LoadingOverlay } from '@mantine/core';
 
 export function DashboardDemo({ id }: { id: string }) {
-  const [dashboard, setDashboard] = React.useState<IDashboard>(DEMO_COMMIT_DASHBOARD)
+  const { data: dashboard, loading, refresh } = useRequest(async () => {
+    const resp = await DashboardAPI.details(id)
+    return resp;
+  }, {
+    refreshDeps: [id]
+  })
 
   const [context, setContext] = React.useState(initialContextInfoContext);
 
@@ -17,12 +24,20 @@ export function DashboardDemo({ id }: { id: string }) {
 
   const updateDashboard = React.useCallback(async (d: IDashboard) => {
     console.log(d)
+    refresh()
   }, [])
+
+  if (!dashboard) {
+    return null;
+  }
+
+  const ready = hasContext && !loading;
   return (
     <div className='dashboard-demo'>
       <ContextInfoContext.Provider value={context}>
         <Filters submit={setContext} />
-        {hasContext && <Dashboard dashboard={dashboard} update={updateDashboard}/>}
+        <LoadingOverlay visible={loading || !hasContext} />
+        {ready && <Dashboard dashboard={dashboard} update={updateDashboard} />}
       </ContextInfoContext.Provider>
     </div>
   )
