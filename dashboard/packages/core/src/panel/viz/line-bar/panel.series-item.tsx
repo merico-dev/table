1f7e86d289e36bc19a3648d3157b456680a7397b
Leto <gerile.tu@merico.dev>
2022-06-15 21:56:59 +0800

    refactor(line-bar): move SeriesItemField into a separate file

---
+++ dashboard/packages/core/src/panel/viz/line-bar/panel.series-item.tsx
@@ -0,0 +1,93 @@
+import { ActionIcon, Anchor, Group, JsonInput, Select, Text, TextInput } from "@mantine/core";
+import { Trash } from "tabler-icons-react";
+import { MantineColorSelector } from "../../settings/common/mantine-color";
+
+const numbroFormatExample = JSON.stringify({
+  output: "percent",
+  mantissa: 2
+}, null, 2);
+
+const labelPositions = [
+  { label: 'top', value: 'top', },
+  { label: 'left', value: 'left', },
+  { label: 'right', value: 'right', },
+  { label: 'bottom', value: 'bottom', },
+  { label: 'inside', value: 'inside', },
+  { label: 'insideLeft', value: 'insideLeft', },
+  { label: 'insideRight', value: 'insideRight', },
+  { label: 'insideTop', value: 'insideTop', },
+  { label: 'insideBottom', value: 'insideBottom', },
+  { label: 'insideTopLeft', value: 'insideTopLeft', },
+  { label: 'insideBottomLeft', value: 'insideBottomLeft', },
+  { label: 'insideTopRight', value: 'insideTopRight', },
+  { label: 'insideBottomRight', value: 'insideBottomRight', },
+]
+
+export function SeriesItemField({ form, index }: any) {
+  return (
+    <Group key={index} direction="column" grow my={0} p="md" pr={40} sx={{ border: '1px solid #eee', position: 'relative' }}>
+      <Group direction="row" grow noWrap>
+        <Select
+          label="Type"
+          data={[
+            { label: 'Line', value: 'line' },
+            { label: 'Bar', value: 'bar' },
+            { label: 'Scatter', value: 'scatter', disabled: true },
+          ]}
+          {...form.getListInputProps('series', index, 'type')}
+        />
+        <TextInput
+          label="Name"
+          required
+          sx={{ flex: 1 }}
+          {...form.getListInputProps('series', index, 'name')}
+        />
+        <TextInput
+          label="Stack"
+          placeholder="Stack bars by this ID"
+          {...form.getListInputProps('series', index, 'stack')}
+        />
+        <TextInput
+          label="Value key"
+          required
+          {...form.getListInputProps('series', index, 'y_axis_data_key')}
+        />
+      </Group>
+      <Group direction="row" grow noWrap align="top">
+        <Select
+          label="Label Position"
+          data={labelPositions}
+          {...form.getListInputProps('series', index, 'label_position')}
+        />
+        <JsonInput
+          sx={{ label: { width: '100%' } }}
+          label={(
+            <Group position="apart">
+              <Text>Value Formatter</Text>
+              <Anchor href="https://numbrojs.com/format.html" target="_blank">
+                Formats
+              </Anchor>
+            </Group>
+          )}
+          placeholder={numbroFormatExample}
+          minRows={4}
+          maxRows={12}
+          autosize
+          {...form.getListInputProps('series', index, 'y_axis_data_formatter')}
+        />
+      </Group>
+      <Group direction="column" grow>
+        <Text>Color</Text>
+        <MantineColorSelector {...form.getListInputProps('series', index, 'color')} />
+      </Group>
+      <ActionIcon
+        color="red"
+        variant="hover"
+        onClick={() => form.removeListItem('series', index)}
+        sx={{ position: 'absolute', top: 15, right: 5 }}
+      >
+        <Trash size={16} />
+      </ActionIcon>
+    </Group>
+  )
+}
--- dashboard/packages/core/src/panel/viz/line-bar/panel.tsx
+++ dashboard/packages/core/src/panel/viz/line-bar/panel.tsx
@@ -1,101 +1,11 @@
-import { ActionIcon, Anchor, Button, Group, JsonInput, Select, Text, Textarea, TextInput } from "@mantine/core";
+import { ActionIcon, Button, Group, Text, TextInput } from "@mantine/core";
 import { formList, useForm } from "@mantine/form";
 import { randomId } from "@mantine/hooks";
 import _ from "lodash";
 import React from "react";
-import { DeviceFloppy, Trash } from "tabler-icons-react";
-import { MantineColorSelector } from "../../settings/common/mantine-color";
+import { DeviceFloppy } from "tabler-icons-react";
+import { SeriesItemField } from "./panel.series-item";
 import { ILineBarChartSeriesItem, IVizLineBarChartPanel } from "./type";
-
-const numbroFormatExample = JSON.stringify({
-  output: "percent",
-  mantissa: 2
-}, null, 2);
-
-const labelPositions = [
-  { label: 'top', value: 'top', },
-  { label: 'left', value: 'left', },
-  { label: 'right', value: 'right', },
-  { label: 'bottom', value: 'bottom', },
-  { label: 'inside', value: 'inside', },
-  { label: 'insideLeft', value: 'insideLeft', },
-  { label: 'insideRight', value: 'insideRight', },
-  { label: 'insideTop', value: 'insideTop', },
-  { label: 'insideBottom', value: 'insideBottom', },
-  { label: 'insideTopLeft', value: 'insideTopLeft', },
-  { label: 'insideBottomLeft', value: 'insideBottomLeft', },
-  { label: 'insideTopRight', value: 'insideTopRight', },
-  { label: 'insideBottomRight', value: 'insideBottomRight', },
-]
-
-function SeriesItemField({ form, index }: any) {
-  return (
-    <Group key={index} direction="column" grow my={0} p="md" pr={40} sx={{ border: '1px solid #eee', position: 'relative' }}>
-      <Group direction="row" grow noWrap>
-        <Select
-          label="Type"
-          data={[
-            { label: 'Line', value: 'line' },
-            { label: 'Bar', value: 'bar' },
-            { label: 'Scatter', value: 'scatter', disabled: true },
-          ]}
-          {...form.getListInputProps('series', index, 'type')}
-        />
-        <TextInput
-          label="Name"
-          required
-          sx={{ flex: 1 }}
-          {...form.getListInputProps('series', index, 'name')}
-        />
-        <TextInput
-          label="Stack"
-          placeholder="Stack bars by this ID"
-          {...form.getListInputProps('series', index, 'stack')}
-        />
-        <TextInput
-          label="Value key"
-          required
-          {...form.getListInputProps('series', index, 'y_axis_data_key')}
-        />
-      </Group>
-      <Group direction="row" grow noWrap align="top">
-        <Select
-          label="Label Position"
-          data={labelPositions}
-          {...form.getListInputProps('series', index, 'label_position')}
-        />
-        <JsonInput
-          sx={{ label: { width: '100%' } }}
-          label={(
-            <Group position="apart">
-              <Text>Value Formatter</Text>
-              <Anchor href="https://numbrojs.com/format.html" target="_blank">
-                Formats
-              </Anchor>
-            </Group>
-          )}
-          placeholder={numbroFormatExample}
-          minRows={4}
-          maxRows={12}
-          autosize
-          {...form.getListInputProps('series', index, 'y_axis_data_formatter')}
-        />
-      </Group>
-      <Group direction="column" grow>
-        <Text>Color</Text>
-        <MantineColorSelector {...form.getListInputProps('series', index, 'color')} />
-      </Group>
-      <ActionIcon
-        color="red"
-        variant="hover"
-        onClick={() => form.removeListItem('series', index)}
-        sx={{ position: 'absolute', top: 15, right: 5 }}
-      >
-        <Trash size={16} />
-      </ActionIcon>
-    </Group>
-  )
-}
 
 function withDefaults(series: ILineBarChartSeriesItem[]) {
   function setDefaults({
@@ -159,7 +69,7 @@ export function VizLineBarChartPanel({ conf, setConf }: IVizLineBarChartPanel) {
         </Group>
         <Group direction="column" grow>
           <Text mt="xl" mb={0}>Series</Text>
-          {form.values.series.map((item, index) => <SeriesItemField form={form} index={index} />)}
+          {form.values.series.map((_item, index) => <SeriesItemField form={form} index={index} />)}
           <Group position="center" mt="xs">
             <Button onClick={addSeries}>
               Add a Series
