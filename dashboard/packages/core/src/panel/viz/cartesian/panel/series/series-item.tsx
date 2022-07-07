d5b0cdf4f53b5e1e180b3ca5ece79874b8b4108d
Leto <gerile.tu@merico.dev>
2022-07-07 16:55:07 +0800

    refactor(cartesian): folderize series field

--- dashboard/packages/core/src/panel/viz/cartesian/panel/series/series-item.tsx
+++ dashboard/packages/core/src/panel/viz/cartesian/panel/series/series-item.tsx
@@ -3,9 +3,9 @@ import { randomId } from "@mantine/hooks";
 import React from "react";
 import { Control, Controller, useFieldArray, UseFieldArrayRemove, UseFormGetValues, UseFormWatch } from "react-hook-form";
 import { Trash } from "tabler-icons-react";
-import { DataFieldSelector } from "../../../settings/common/data-field-selector";
-import { MantineColorSelector } from "../../../settings/common/mantine-color";
-import { ICartesianChartConf, ICartesianChartSeriesItem } from "../type";
+import { DataFieldSelector } from "../../../../settings/common/data-field-selector";
+import { MantineColorSelector } from "../../../../settings/common/mantine-color";
+import { ICartesianChartConf, ICartesianChartSeriesItem } from "../../type";
 
 const labelPositions = [
   { label: 'off', value: '', },
@@ -37,7 +37,7 @@ interface ISeriesItemField {
   data: any[];
 }
 
-function SeriesItemField({ control, index, remove, seriesItem, yAxisOptions, data }: ISeriesItemField) {
+export function SeriesItemField({ control, index, remove, seriesItem, yAxisOptions, data }: ISeriesItemField) {
   const type = seriesItem.type;
   return (
     <Group key={index} direction="column" grow my={0} p="md" pr={40} sx={{ border: '1px solid #eee', position: 'relative' }}>
@@ -157,66 +157,6 @@ function SeriesItemField({ control, index, remove, seriesItem, yAxisOptions, dat
       >
         <Trash size={16} />
       </ActionIcon>
-    </Group>
-  )
-}
-
-interface ISeriesField {
-  control: Control<ICartesianChartConf, any>;
-  watch: UseFormWatch<ICartesianChartConf>;
-  getValues: UseFormGetValues<ICartesianChartConf>;
-  data: any[];
-}
-export function SeriesField({ control, watch, getValues, data }: ISeriesField) {
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: "series"
-  });
-
-  const watchFieldArray = watch("series");
-  const controlledFields = fields.map((field, index) => {
-    return {
-      ...field,
-      ...watchFieldArray[index]
-    };
-  });
-
-  const addSeries = () => append({
-    type: 'bar',
-    name: randomId(),
-    showSymbol: false,
-    y_axis_data_key: 'value',
-    yAxisIndex: 0,
-    label_position: 'top',
-    stack: '',
-    color: '#000'
-  });
-
-  const yAxisOptions = React.useMemo(() => {
-    return getValues().y_axes.map(({ name }, index) => ({
-      label: name,
-      value: index.toString()
-    }))
-  }, [getValues]);
-
-  return (
-    <Group direction="column" grow>
-      <Text mt="xl" mb={0}>Series</Text>
-      {controlledFields.map((seriesItem, index) => (
-        <SeriesItemField
-          control={control}
-          index={index}
-          remove={remove}
-          seriesItem={seriesItem}
-          yAxisOptions={yAxisOptions}
-          data={data}
-        />
-      ))}
-      <Group position="center" mt="xs">
-        <Button onClick={addSeries}>
-          Add a Series
-        </Button>
-      </Group>
     </Group>
   )
 }
