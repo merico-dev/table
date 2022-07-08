4480169ebf64739b32babb3c4c2f5344d5239449
Leto <gerile.tu@merico.dev>
2022-07-09 06:17:39 +0800

    refactor(cartesian): folderize echarts option

--- dashboard/packages/core/src/panel/viz/cartesian/index.tsx
+++ dashboard/packages/core/src/panel/viz/cartesian/index.tsx
@@ -7,98 +7,14 @@ import { GridComponent, LegendComponent, TooltipComponent } from 'echarts/compon
 import { CanvasRenderer } from 'echarts/renderers';
 import _ from "lodash";
 import React from 'react';
-import numbro from 'numbro';
-import { TopLevelFormatterParams } from 'echarts/types/dist/shared';
-import { ICartesianChartConf, ICartesianChartSeriesItem, IRegressionLineConf, IRegressionTransform, IYAxisConf } from './type';
+import { getOption } from './option';
+import { ICartesianChartConf } from './type';
 
 echarts.use([BarChart, LineChart, ScatterChart, GridComponent, LegendComponent, TooltipComponent, CanvasRenderer]);
 echarts.registerTransform(transform.regression);
 
-
-interface IRegressionDataSetItem {
-  id: string;
-  fromDatasetId?: string;
-  source?: number[][];
-  transform?: IRegressionTransform;
-}
-interface IRegressionSeriesItem extends IRegressionLineConf {
-  datasetId: string;
-  xAxisId: string;
-  name: string;
-  showSymbol: boolean;
-  tooltip: Record<string, any>;
-};
-
-function getRegressionConfs({ regressions = [] }: ICartesianChartConf, data: any[]) {
-  const regressionDataSets: IRegressionDataSetItem[] = [];
-  const regressionSeries: IRegressionSeriesItem[] = [];
-  const regressionXAxes: Record<string, any>[] = [];
-  if (data.length === 0) {
-    return { regressionDataSets, regressionSeries, regressionXAxes }
-  }
-  regressions.forEach(({ transform, plot, name, y_axis_data_key }) => {
-    const xAxisId = `x-axis-for-${name}`
-    const rawDatasetId = `dataset-for-${name}--raw`
-    const regDatasetId = `dataset-for-${name}--transformed`
-
-    regressionDataSets.push({
-      id: rawDatasetId,
-      source: data.map((d, i) => ([i, d[y_axis_data_key]])),
-    })
-    regressionDataSets.push({
-      transform,
-      id: regDatasetId,
-      fromDatasetId: rawDatasetId,
-    })
-    regressionSeries.push(({
-      ...plot,
-      name,
-      datasetId: regDatasetId,
-      xAxisId,
-      showSymbol: false,
-      tooltip: {
-        show: false
-      }
-    }))
-    regressionXAxes.push({
-      type: 'category',
-      id: xAxisId,
-      datasetId: regDatasetId,
-      show: false,
-    })
-  });
-
-  return { regressionDataSets, regressionSeries, regressionXAxes }
-}
-
-const defaultOption = {
-  legend: {
-    show: true,
-    bottom: 0,
-    left: 0,
-  },
-  tooltip: {
-    trigger: 'axis',
-  },
-  xAxis: {
-    type: 'category',
-    nameGap: 25,
-    nameLocation: 'center',
-    nameTextStyle: {
-      fontWeight: 'bold',
-    },
-  },
-  grid: {
-    top: 30,
-    left: 15,
-    right: 15,
-    bottom: 30,
-    containLabel: true,
-  }
-}
-
 interface ICartesianChart {
-  conf: any;
+  conf: ICartesianChartConf;
   data: any[];
   width: number;
   height: number;
@@ -106,90 +22,7 @@ interface ICartesianChart {
 
 export function VizCartesianChart({ conf, data, width, height }: ICartesianChart) {
   const option = React.useMemo(() => {
-    const labelFormatters = conf.y_axes.reduce((ret: Record<string, (params: any) => string>, { label_formatter }: IYAxisConf, index: number) => {
-      ret[index] = function formatter(payload: any) {
-        const value = typeof payload === 'object' ? payload.value : payload;
-        if (!label_formatter) {
-          return value;
-        }
-        try {
-          return numbro(value).format(label_formatter)
-        } catch (error) {
-          console.error(error)
-          return value;
-        }
-      }
-      return ret;
-    }, {
-      default: ({ value }: any) => value,
-    });
-
-    const yAxisIndexMap = conf.series.reduce((ret: Record<string, number>, { yAxisIndex, name }: ICartesianChartSeriesItem) => {
-      ret[name] = yAxisIndex
-      return ret;
-    }, {});
-
-    const series = conf.series.map(({ y_axis_data_key, yAxisIndex, label_position, name, ...rest }: ICartesianChartSeriesItem) => {
-      const ret: any = {
-        data: data.map((d) => d[y_axis_data_key]),
-        label: {
-          show: !!label_position,
-          position: label_position,
-          formatter: labelFormatters[yAxisIndex ?? 'default']
-        },
-        name,
-        xAxisId: 'main-x-axis',
-        yAxisIndex,
-        ...rest,
-      }
-      return ret;
-    });
-
-    const { regressionDataSets, regressionSeries, regressionXAxes } = getRegressionConfs(conf, data)
-
-    const customOptions = {
-      xAxis: [
-        {
-          data: data.map((d) => d[conf.x_axis_data_key]),
-          name: conf.x_axis_name ?? '',
-          id: 'main-x-axis'
-        },
-        ...regressionXAxes,
-      ],
-      yAxis: conf.y_axes.map(({ label_formatter, ...rest }: IYAxisConf, index: number) => ({
-        ...rest,
-        axisLabel: {
-          show: true,
-          formatter: labelFormatters[index] ?? labelFormatters.default,
-        }
-      })),
-      dataset: [
-        ...regressionDataSets,
-      ],
-      series: [
-        ...series,
-        ...regressionSeries,
-      ],
-      tooltip: {
-        formatter: function (params: TopLevelFormatterParams) {
-          const arr = Array.isArray(params) ? params : [params];
-          if (arr.length === 0) {
-            return ''
-          }
-          const lines = arr.map(({ seriesName, value }) => {
-            if (!seriesName) {
-              return value;
-            }
-            const yAxisIndex = yAxisIndexMap[seriesName]
-            const formatter = labelFormatters[yAxisIndex] ?? labelFormatters.default;
-            return `${seriesName}: ${formatter({ value })}`
-          })
-          lines.unshift(`<strong>${arr[0].name}</strong>`)
-          return lines.join('<br />')
-        }
-      }
-    }
-    return _.merge({}, defaultOption, customOptions);
+    return getOption(conf, data);
   }, [conf, data])
 
   if (!width || !height) {
---
+++ dashboard/packages/core/src/panel/viz/cartesian/option/index.ts
@@ -0,0 +1,118 @@
+import _ from "lodash";
+import numbro from 'numbro';
+import { TopLevelFormatterParams } from 'echarts/types/dist/shared';
+import { ICartesianChartConf, ICartesianChartSeriesItem, IYAxisConf } from '../type';
+import { getRegressionConfs } from './regression';
+
+const defaultOption = {
+  legend: {
+    show: true,
+    bottom: 0,
+    left: 0,
+  },
+  tooltip: {
+    trigger: 'axis',
+  },
+  xAxis: {
+    type: 'category',
+    nameGap: 25,
+    nameLocation: 'center',
+    nameTextStyle: {
+      fontWeight: 'bold',
+    },
+  },
+  grid: {
+    top: 30,
+    left: 15,
+    right: 15,
+    bottom: 30,
+    containLabel: true,
+  }
+}
+
+export function getOption(conf: ICartesianChartConf, data: any[]) {
+  const labelFormatters = conf.y_axes.reduce((ret: Record<string, (params: any) => string>, { label_formatter }: IYAxisConf, index: number) => {
+    ret[index] = function formatter(payload: any) {
+      const value = typeof payload === 'object' ? payload.value : payload;
+      if (!label_formatter) {
+        return value;
+      }
+      try {
+        return numbro(value).format(label_formatter)
+      } catch (error) {
+        console.error(error)
+        return value;
+      }
+    }
+    return ret;
+  }, {
+    default: ({ value }: any) => value,
+  });
+
+  const yAxisIndexMap = conf.series.reduce((ret: Record<string, number>, { yAxisIndex, name }: ICartesianChartSeriesItem) => {
+    ret[name] = yAxisIndex
+    return ret;
+  }, {});
+
+  const series = conf.series.map(({ y_axis_data_key, yAxisIndex, label_position, name, ...rest }: ICartesianChartSeriesItem) => {
+    const ret: any = {
+      data: data.map((d) => d[y_axis_data_key]),
+      label: {
+        show: !!label_position,
+        position: label_position,
+        formatter: labelFormatters[yAxisIndex ?? 'default']
+      },
+      name,
+      xAxisId: 'main-x-axis',
+      yAxisIndex,
+      ...rest,
+    }
+    return ret;
+  });
+
+  const { regressionDataSets, regressionSeries, regressionXAxes } = getRegressionConfs(conf, data)
+
+  const customOptions = {
+    xAxis: [
+      {
+        data: data.map((d) => d[conf.x_axis_data_key]),
+        name: conf.x_axis_name ?? '',
+        id: 'main-x-axis'
+      },
+      ...regressionXAxes,
+    ],
+    yAxis: conf.y_axes.map(({ label_formatter, ...rest }: IYAxisConf, index: number) => ({
+      ...rest,
+      axisLabel: {
+        show: true,
+        formatter: labelFormatters[index] ?? labelFormatters.default,
+      }
+    })),
+    dataset: [
+      ...regressionDataSets,
+    ],
+    series: [
+      ...series,
+      ...regressionSeries,
+    ],
+    tooltip: {
+      formatter: function (params: TopLevelFormatterParams) {
+        const arr = Array.isArray(params) ? params : [params];
+        if (arr.length === 0) {
+          return ''
+        }
+        const lines = arr.map(({ seriesName, value }) => {
+          if (!seriesName) {
+            return value;
+          }
+          const yAxisIndex = yAxisIndexMap[seriesName]
+          const formatter = labelFormatters[yAxisIndex] ?? labelFormatters.default;
+          return `${seriesName}: ${formatter({ value })}`
+        })
+        lines.unshift(`<strong>${arr[0].name}</strong>`)
+        return lines.join('<br />')
+      }
+    }
+  }
+  return _.merge({}, defaultOption, customOptions);
+}
