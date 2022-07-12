9a07af164919d90a9e55c7ff42a1493ac72e30bf
Leto <gerile.tu@merico.dev>
2022-07-13 01:26:39 +0800

    feat(aggregation): add aggregation field & util

---
+++ dashboard/packages/core/src/panel/settings/common/aggregation-selector.tsx
@@ -0,0 +1,33 @@
+import { Select } from "@mantine/core";
+import _ from "lodash";
+import React from "react";
+import { AggregationType } from "../../../utils/aggregation";
+
+const options = [
+  { label: 'None', value: 'none' },
+  { label: 'Sum', value: 'sum' },
+  { label: 'Mean', value: 'mean' },
+  { label: 'Median', value: 'median' },
+  { label: 'Max', value: 'max' },
+  { label: 'Min', value: 'min' },
+]
+
+interface IAggregationSelector {
+  value: AggregationType;
+  onChange: (v: AggregationType) => void;
+  label: string;
+}
+
+function _AggregationSelector({ label, value, onChange }: IAggregationSelector, ref: any) {
+  return (
+    <Select
+      ref={ref}
+      label={label}
+      data={options}
+      value={value}
+      onChange={onChange}
+    />
+  )
+}
+
+export const AggregationSelector = React.forwardRef(_AggregationSelector)
