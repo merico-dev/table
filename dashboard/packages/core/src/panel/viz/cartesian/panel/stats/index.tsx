aec07898a476af66af015a4adec608875da3ee54
Leto <gerile.tu@merico.dev>
2022-07-14 01:55:28 +0800

    feat(cartesian): add stats configuration

---
+++ dashboard/packages/core/src/panel/viz/cartesian/panel/stats/index.tsx
@@ -0,0 +1,67 @@
+/**
+ * NOTE: this file is almost a duplicate of stats/panel/variables.tsx
+ * FIXME: remove this when variables' fields are defined in utils/template
+ */
+import { Button, Group, Text } from "@mantine/core";
+import React from "react";
+import { Control, Controller, useFieldArray, UseFormWatch } from "react-hook-form";
+import { ICartesianChartConf } from "../../type";
+import { VariableField } from "./variable";
+import { getANewVariable, TemplateInput } from '../../../../../utils/template/editor';
+
+interface IVariablesField {
+  control: Control<ICartesianChartConf, any>;
+  watch: UseFormWatch<ICartesianChartConf>;
+  data: any[];
+}
+export function StatsField({ control, watch, data }: IVariablesField) {
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "stats.variables"
+  });
+
+  watch("stats.templates")
+  const watchFieldArray = watch("stats.variables");
+  const controlledFields = fields.map((field, index) => {
+    return {
+      ...field,
+      ...watchFieldArray[index]
+    };
+  });
+
+  const add = () => append(getANewVariable());
+
+  return (
+    <Group direction="column" grow>
+      <Group direction="column" grow noWrap spacing={0}>
+        <Controller
+          name='stats.templates.top'
+          control={control}
+          render={(({ field }) => (
+            <TemplateInput label="Template for stats above the chart" py="md" sx={{ flexGrow: 1 }} {...field} />
+          ))}
+        />
+        <Controller
+          name='stats.templates.bottom'
+          control={control}
+          render={(({ field }) => (
+            <TemplateInput label="Template for stats under the chart" py="md" sx={{ flexGrow: 1 }} {...field} />
+          ))}
+        />
+      </Group>
+      {controlledFields.map((_variableItem, index) => (
+        <VariableField
+          control={control}
+          index={index}
+          remove={remove}
+          data={data}
+        />
+      ))}
+      <Group position="center" mt="xs">
+        <Button onClick={add}>
+          Add a Variable
+        </Button>
+      </Group>
+    </Group>
+  )
+}
---
+++ dashboard/packages/core/src/panel/viz/cartesian/panel/stats/variable.tsx
@@ -0,0 +1,41 @@
+/**
+ * NOTE: this file is almost a duplicate of stats/panel/variable.tsx
+ * FIXME: remove this when variables' fields are defined in utils/template
+ */
+import { ActionIcon, Group } from "@mantine/core";
+import React from "react";
+import { Control, Controller, UseFieldArrayRemove } from "react-hook-form";
+import { Trash } from "tabler-icons-react";
+import { TemplateVariableField } from "../../../../../utils/template/editor";
+import { ICartesianChartConf } from "../../type";
+
+
+interface VariableField {
+  control: Control<ICartesianChartConf, any>;
+  index: number;
+  remove: UseFieldArrayRemove;
+  data: any[];
+}
+
+export function VariableField({ control, index, remove, data }: VariableField) {
+  return (
+    <Group key={index} direction="column" grow my="sm" p={0} sx={{ border: '1px solid #eee', borderTopColor: '#333', borderTopWidth: 2, position: 'relative' }}>
+      <Controller
+        name={`stats.variables.${index}`}
+        control={control}
+        render={(({ field }) => (
+          <TemplateVariableField data={data} {...field} />
+        ))}
+      />
+      <ActionIcon
+        color="red"
+        variant="hover"
+        onClick={() => remove(index)}
+        sx={{ position: 'absolute', top: 15, right: 5 }}
+      >
+        <Trash size={16} />
+      </ActionIcon>
+    </Group >
+
+  )
+}
