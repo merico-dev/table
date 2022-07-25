bbb123fbd482b96f2831179daf111a6943c5727b
Leto <gerile.tu@merico.dev>
2022-07-25 22:47:50 +0800

    chore(settings-form): init

---
+++ settings-form/src/datasource/index.tsx
@@ -0,0 +1,6 @@
+export interface IDataSourceList {}
+export function DataSourceList({}: IDataSourceList) {
+  return (
+    <span>DataSourceList</span>
+  )
+}
---
+++ settings-form/vite.config.ts
@@ -0,0 +1,81 @@
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import visualizer from 'rollup-plugin-visualizer'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'settings-form',
+      formats: ['es', 'umd'],
+      fileName: (format) => `settings-form.${format}.js`,
+    },
+    rollupOptions: {
+      plugins: [
+        visualizer()
+      ],
+      external: [
+        '@mantine/core',
+        '@mantine/dates',
+        '@mantine/form',
+        '@mantine/hooks',
+        '@mantine/modals',
+        '@mantine/notifications',
+        '@mantine/prism',
+        '@mantine/rte',
+        'ahooks',
+        'axios',
+        'echarts/core',
+        'echarts/charts',
+        'echarts/components',
+        'echarts/renderers',
+        'echarts-for-react/lib/core',
+        'echarts-gl',
+        'echarts-stat',
+        'lodash',
+        'numbro',
+        'react',
+        'react-dom',
+        'react-grid-layout',
+        'react-hook-form',
+        'tabler-icons-react',
+      ],
+      output: {
+        globals: {
+          '@mantine/core': '@mantine/core',
+          '@mantine/dates': '@mantine/dates',
+          '@mantine/form': '@mantine/form',
+          '@mantine/hooks': '@mantine/hooks',
+          '@mantine/modals': '@mantine/modals',
+          '@mantine/notifications': '@mantine/notifications',
+          '@mantine/prism': '@mantine/prism',
+          '@mantine/rte': '@mantine/rte',
+          ahooks: 'ahooks',
+          'axios': 'axios',
+          'echarts/core': 'echarts/core',
+          'echarts/charts': 'echarts/charts',
+          'echarts/components': 'echarts/components',
+          'echarts/renderers': 'echarts/renderers',
+          'echarts-for-react/lib/core': 'echarts-for-react/lib/core',
+          'echarts-gl': 'echarts-gl',
+          'echarts-stat': 'echarts-stat',
+          lodash: '_',
+          numbro: 'numbro',
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react-grid-layout': 'react-grid-layout',
+          'react-hook-form': 'react-hook-form',
+          'tabler-icons-react': 'tabler-icons-react',
+        },
+      },
+    },
+  },
+});
