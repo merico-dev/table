fee4ddf8848f98cef1cfe9c9b87957ff98689230
Leto <gerile.tu@merico.dev>
2022-08-01 20:08:07 +0800

    feat(dashboard): let filter-select use query options

--- dashboard/src/filter/filter-select/render.tsx
+++ dashboard/src/filter/filter-select/render.tsx
@@ -1,4 +1,7 @@
 import { Select } from "@mantine/core";
+import { useRequest } from "ahooks";
+import React from "react";
+import { queryByStaticSQL } from "../../api-caller";
 import { IDashboardFilter, IFilterConfig_Select } from "../../types";
 
 interface IFilterSelect extends Omit<IDashboardFilter, 'type' | 'config'> {
@@ -8,10 +11,19 @@ interface IFilterSelect extends Omit<IDashboardFilter, 'type' | 'config'> {
 }
 
 export function FilterSelect({ label, config, value, onChange }: IFilterSelect) {
+  const usingRemoteOptions = !!config.options_query.sql;
+  const { data: remoteOptions = [], loading } = useRequest(queryByStaticSQL(config.options_query), {
+    refreshDeps: [config.options_query, usingRemoteOptions]
+  })
+
+  if (usingRemoteOptions) {
+    console.log({ loading, remoteOptions })
+  }
   return (
     <Select
       label={label}
-      data={config.static_options}
+      data={usingRemoteOptions ? remoteOptions : config.static_options}
+      disabled={usingRemoteOptions ? loading : false}
       value={value}
       onChange={onChange}
     />
