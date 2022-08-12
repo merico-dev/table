fc62f0f2a637753c29bc61127c139ffd0bab3f31
Leto <gerile.tu@merico.dev>
2022-08-12 16:25:28 +0800

    feat(dashboard): extract DataTable from DataPreview

--- dashboard/src/definition-editor/query-editor/data-preview.tsx
+++ dashboard/src/definition-editor/query-editor/data-preview.tsx
@@ -1,8 +1,45 @@
-import { ActionIcon, Group, LoadingOverlay, Stack, Table, Text } from '@mantine/core';
+import { ActionIcon, Box, Group, LoadingOverlay, Stack, Table, Text } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Refresh } from 'tabler-icons-react';
 import { DashboardModelInstance } from '../../model';
+
+function DataTable({ loading, data }: { loading: boolean; data: any[] }) {
+  if (loading) {
+    return <LoadingOverlay visible={loading} exitTransitionDuration={0} />;
+  }
+  if (data.length === 0) {
+    return <Table></Table>;
+  }
+  return (
+    <Table>
+      <thead>
+        <tr>
+          {Object.keys(data?.[0]).map((label) => (
+            <th key={label}>
+              <Text weight={700} color="#000">
+                {label}
+              </Text>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.slice(0, 10).map((row: Record<string, any>, index: number) => (
+          <tr key={`row-${index}`}>
+            {Object.values(row).map((v: any, i) => (
+              <td key={`${v}--${i}`}>
+                <Group sx={{ '&, .mantine-Text-root': { fontFamily: 'monospace' } }}>
+                  <Text>{v}</Text>
+                </Group>
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
 
 export const DataPreview = observer(function _DataPreview({
   id,
@@ -13,13 +50,7 @@ export const DataPreview = observer(function _DataPreview({
 }) {
   const { data, state, error } = model.getDataStuffByID(id);
   const loading = state === 'loading';
-  const refresh = () => console.log('under mantainance')
-  if (loading) {
-    return <LoadingOverlay visible={loading} exitTransitionDuration={0} />;
-  }
-  if (data.length === 0) {
-    return <Table></Table>;
-  }
+  const refresh = () => console.log('under mantainance');
   return (
     <Stack my="xl" sx={{ border: '1px solid #eee' }}>
       <Group position="apart" py="md" pl="md" sx={{ borderBottom: '1px solid #eee', background: '#efefef' }}>
@@ -35,32 +66,9 @@ export const DataPreview = observer(function _DataPreview({
           <Refresh size={15} />
         </ActionIcon>
       </Group>
-      <Table>
-        <thead>
-          <tr>
-            {Object.keys(data?.[0]).map((label) => (
-              <th key={label}>
-                <Text weight={700} color="#000">
-                  {label}
-                </Text>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.slice(0, 10).map((row: Record<string, any>, index: number) => (
-            <tr key={`row-${index}`}>
-              {Object.values(row).map((v: any, i) => (
-                <td key={`${v}--${i}`}>
-                  <Group sx={{ '&, .mantine-Text-root': { fontFamily: 'monospace' } }}>
-                    <Text>{v}</Text>
-                  </Group>
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <Box sx={{ position: 'relative' }}>
+        <DataTable loading={loading} data={data} />
+      </Box>
     </Stack>
   );
 });
