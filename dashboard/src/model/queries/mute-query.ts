549c3249db40b883c9aeb88707c03dae485cb8a2
Leto <gerile.tu@merico.dev>
2022-08-17 09:06:16 +0800

    fix(dashboard): add MuteQueryModel to avoid repeated requests

---
+++ dashboard/src/model/queries/mute-query.ts
@@ -0,0 +1,21 @@
+import { Instance, types } from 'mobx-state-tree';
+import { DataSourceType } from './types';
+
+export const MuteQueryModel = types
+  .model('QueryModel', {
+    id: types.string,
+    type: types.enumeration('DataSourceType', [DataSourceType.Postgresql, DataSourceType.MySQL, DataSourceType.HTTP]),
+    key: types.string,
+    sql: types.string,
+  })
+  .views((self) => ({
+    get valid() {
+      return self.id && self.type && self.key && self.sql;
+    },
+    get configurations() {
+      const { id, type, key, sql } = self;
+      return { id, type, key, sql };
+    },
+  }));
+
+export type MuteQueryModelInstance = Instance<typeof MuteQueryModel>;
