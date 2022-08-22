556cc937381c6a43ee533bf2d6518c542a13706f
ZeekoZhu <tao.zhu@merico.dev>
2022-08-22 16:07:42 +0800

    dev(website): improve dev mode speed

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import * as path from 'path';
+import react from '@vitejs/plugin-react';
+
+const workspace = (...segments: string[]) => {
+  return path.resolve(__dirname, '..', ...segments);
+};
 
 // https://vitejs.dev/config/
+const root = path.resolve(__dirname, '..');
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 32000,
-  }
-})
+  },
+  resolve: {
+    alias: {
+      '@devtable/dashboard': workspace('dashboard', 'src'),
+      '@devtable/settings-form': workspace('settings-form', 'src'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@devtable/dashboard', '@devtable/settings-form'],
+  },
+});
