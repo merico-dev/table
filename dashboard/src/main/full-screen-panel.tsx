e3bc987882f3c5187783df65bd0809c6e7c951f2
Leto <gerile.tu@merico.dev>
2022-08-23 14:49:22 +0800

    style(dashboard): misc

--- dashboard/src/main/full-screen-panel.tsx
+++ dashboard/src/main/full-screen-panel.tsx
@@ -12,13 +12,13 @@ export const FullScreenPanel = observer(function _FullScreenPanel({
   exitFullScreen: () => void;
 }) {
   return (
-    <Stack sx={{ flexGrow: 1, justifyContent: 'flex-start' }}>
-      <Group sx={{ flexGrow: 0 }}>
+    <Stack px="10px" pb="5px" sx={{ flexGrow: 1, justifyContent: 'flex-start' }}>
+      <Group mx="-5px" sx={{ flexGrow: 0 }}>
         <Button variant="default" size="sm" onClick={exitFullScreen} leftIcon={<ArrowLeft size={20} />}>
           Exit fullscreen
         </Button>
       </Group>
-      <Group grow sx={{ flexGrow: 1, flexShrink: 0 }}>
+      <Group mx="-5px" grow sx={{ flexGrow: 1, flexShrink: 0 }}>
         <Panel {...panel} />
       </Group>
     </Stack>
