3f96cf61daa029284b994995ad0f1c12926e0b23
Leto <gerile.tu@merico.dev>
2022-09-01 19:23:44 +0800

    feat(dashboard): draw regression chart

---
+++ dashboard/src/plugins/viz-components/regression-chart/option/index.ts
@@ -0,0 +1,62 @@
+import _, { defaultsDeep } from 'lodash';
+import { IRegressionChartConf } from '../type';
+import { getRegressionConf } from './regression';
+
+const defaultOption = {
+  legend: {
+    show: true,
+    bottom: 0,
+    left: 0,
+  },
+  tooltip: {
+    trigger: 'axis',
+  },
+  xAxis: [
+    {
+      type: 'category',
+      nameGap: 25,
+      nameLocation: 'center',
+      nameTextStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  ],
+  grid: {
+    top: 10,
+    left: 30,
+    right: 15,
+    bottom: 30,
+    containLabel: true,
+  },
+};
+
+export function getOption(conf: IRegressionChartConf, data: any[]) {
+  const { regressionDataSets, regressionSeries, regressionXAxes } = getRegressionConf(conf, data);
+
+  const customOptions = {
+    xAxis: [
+      {
+        data: data.map((d) => d[conf.x_axis.data_key]),
+        name: conf.x_axis.name ?? '',
+        id: 'main-x-axis',
+      },
+      ...regressionXAxes,
+    ],
+    yAxis: {
+      name: conf.y_axis.name ?? '',
+    },
+    dataset: [...regressionDataSets],
+    series: [
+      {
+        data: data.map((d) => d[conf.regression.y_axis_data_key]),
+        name: 'scatter',
+        type: 'scatter',
+        symbolSize: 4,
+        color: 'red',
+        xAxisId: 'main-x-axis',
+      },
+      ...regressionSeries,
+    ],
+  };
+  return defaultsDeep({}, customOptions, defaultOption);
+}
--- dashboard/src/plugins/viz-components/regression-chart/viz-regression-chart.tsx
+++ dashboard/src/plugins/viz-components/regression-chart/viz-regression-chart.tsx
@@ -1,8 +1,35 @@
+import { Box } from '@mantine/core';
+import ReactEChartsCore from 'echarts-for-react/lib/core';
+import { ScatterChart } from 'echarts/charts';
+import { GridComponent, LegendComponent, TooltipComponent } from 'echarts/components';
+/* @ts-expect-error */
+import { transform } from 'echarts-stat';
+import * as echarts from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
+import { defaultsDeep } from 'lodash';
+import { useMemo } from 'react';
 import { VizViewProps } from '../../../types/plugin';
 import { useStorageData } from '../../hooks';
+import { getOption } from './option';
 import { DEFAULT_CONFIG, IRegressionChartConf } from './type';
+
+echarts.use([ScatterChart, GridComponent, LegendComponent, TooltipComponent, CanvasRenderer]);
+echarts.registerTransform(transform.regression);
 
 export function VizRegressionChart({ context }: VizViewProps) {
   const { value: conf } = useStorageData<IRegressionChartConf>(context.instanceData, 'config');
-  return <div>Hello World</div>;
+  const { width, height } = context.viewport;
+  const data = context.data as any[];
+  const option = useMemo(() => {
+    return getOption(defaultsDeep({}, conf, DEFAULT_CONFIG), data);
+  }, [conf, data]);
+
+  if (!width || !height || !conf) {
+    return null;
+  }
+  return (
+    <Box>
+      <ReactEChartsCore echarts={echarts} option={option} style={{ width, height }} />
+    </Box>
+  );
 }
