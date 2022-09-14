2e2150a06df87428909016242bff5d9d070a230b
Leto <gerile.tu@merico.dev>
2022-09-15 00:01:58 +0800

    feat(dashboard): add barGap to cartesian

--- dashboard/src/plugins/viz-components/cartesian/panel/series/fields.bar.tsx
+++ dashboard/src/plugins/viz-components/cartesian/panel/series/fields.bar.tsx
@@ -1,7 +1,18 @@
-import { Group, TextInput } from '@mantine/core';
+import { Group, Select, TextInput } from '@mantine/core';
 import React from 'react';
 import { Control, Controller } from 'react-hook-form';
 import { ICartesianChartConf } from '../../type';
+
+const barGapOptions = [
+  {
+    label: 'No gap between bars',
+    value: '0%',
+  },
+  {
+    label: 'Bars overlapping on each other',
+    value: '-100%',
+  },
+];
 
 interface IBarFields {
   control: Control<ICartesianChartConf, any>;
@@ -10,19 +21,26 @@ interface IBarFields {
 
 export function BarFields({ control, index }: IBarFields) {
   return (
-    <Group grow align="top">
+    <>
+      <Group grow align="top">
+        <Controller
+          name={`series.${index}.stack`}
+          control={control}
+          render={({ field }) => (
+            <TextInput label="Stack" placeholder="Stack bars by this ID" sx={{ flexGrow: 1 }} {...field} />
+          )}
+        />
+        <Controller
+          name={`series.${index}.barWidth`}
+          control={control}
+          render={({ field }) => <TextInput label="Bar Width" sx={{ flexGrow: 1 }} {...field} />}
+        />
+      </Group>
       <Controller
-        name={`series.${index}.stack`}
+        name={`series.${index}.barGap`}
         control={control}
-        render={({ field }) => (
-          <TextInput label="Stack" placeholder="Stack bars by this ID" sx={{ flexGrow: 1 }} {...field} />
-        )}
+        render={({ field }) => <Select label="Bar Gap" data={barGapOptions} sx={{ flexGrow: 1 }} {...field} />}
       />
-      <Controller
-        name={`series.${index}.barWidth`}
-        control={control}
-        render={({ field }) => <TextInput label="Bar Width" sx={{ flexGrow: 1 }} {...field} />}
-      />
-    </Group>
+    </>
   );
 }
