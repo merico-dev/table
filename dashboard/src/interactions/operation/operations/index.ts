e249c6bd9eb41eff081e7b62a01ad9144fbd0a89
ZeekoZhu <tao.zhu@merico.dev>
2022-09-14 13:39:03 +0800

    feat(dashboard): add open link operation
    Closes: #211

--- dashboard/src/interactions/operation/operations/index.ts
+++ dashboard/src/interactions/operation/operations/index.ts
@@ -1,4 +1,5 @@
+import { OpenLink } from '~/interactions/operation/operations/open-link';
 import { IDashboardOperationSchema } from '~/types/plugin';
 import { ConsoleLog } from './console-log';
 
-export const OPERATIONS: IDashboardOperationSchema[] = [ConsoleLog];
+export const OPERATIONS: IDashboardOperationSchema[] = [ConsoleLog, OpenLink];
