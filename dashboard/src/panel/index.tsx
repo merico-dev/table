7128bd46ae52e9d59a3945cd9f0cf697f658fcc5
Leto <gerile.tu@merico.dev>
2022-10-16 15:00:25 +0800

    style(dashboard): render border-less panel

--- dashboard/src/panel/index.tsx
+++ dashboard/src/panel/index.tsx
@@ -20,15 +20,36 @@ interface IPanel {
   panel: PanelModelInstance;
 }
 
+const constantBorder = {
+  border: '1px solid #ced4da',
+};
+const hoverBorder = {
+  border: '1px dashed transparent',
+  transition: 'border-color 300ms ease',
+  '&:hover': {
+    borderColor: '#ced4da',
+  },
+};
+
 export const Panel = observer(function _Panel({ panel, view }: IPanel) {
   const model = useModelContext();
 
   const { data, state } = model.getDataStuffByID(panel.queryID);
   const loading = doesVizRequiresData(panel.viz.type) && state === 'loading';
+
   const vizHeight = !panel.title ? '100%' : 'calc(100% - 25px - 5px)';
+  const panelStyle = panel.style.border.enabled ? constantBorder : hoverBorder;
+
   return (
     <PanelContextProvider value={{ panel, data, loading }}>
-      <Box className="panel-root" p={5} pt={0}>
+      <Box
+        className="panel-root"
+        p={5}
+        pt={0}
+        sx={{
+          ...panelStyle,
+        }}
+      >
         <Box sx={{ position: 'absolute', left: 0, top: 0, height: 28, zIndex: 310 }}>
           <DescriptionPopover />
         </Box>
