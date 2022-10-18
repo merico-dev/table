2939f48f266927ce62403e63b91dc0f95a587ce5
Leto <gerile.tu@merico.dev>
2022-10-18 14:48:21 +0800

    chore: publish v4.16.1

--- website/src/api-caller/dashboard.tsx
+++ website/src/api-caller/dashboard.tsx
@@ -53,7 +53,7 @@ export const DashboardAPI = {
           },
         ],
         filters: [],
-        version: '4.16.0',
+        version: '4.16.1',
       };
     }
     const res: IDBDashboard = await post('/dashboard/create', {
