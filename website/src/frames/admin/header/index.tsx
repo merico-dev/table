fd7f0bf59e24184be42988e34be7195a5f1622c7
Leto <gerile.tu@merico.dev>
2022-10-26 19:46:53 +0800

    feat(website): add and use account-dropdown with logout

--- website/src/frames/admin/header/index.tsx
+++ website/src/frames/admin/header/index.tsx
@@ -1,14 +1,18 @@
 import { Group, Header as MantineHeader, Text } from '@mantine/core';
 import React from 'react';
+import { AccountDropdown } from '../../../components/account-dropdown';
 import { MericoLogo } from '../../../resources/merico-logo';
 
 export function AdminHeader() {
   return (
-    <MantineHeader height={60} p="md">
-      <Group position="apart">
+    <MantineHeader height={60} px="md" py={0}>
+      <Group position="apart" sx={{ height: 60 }}>
         <Group>
           <MericoLogo width={40} />
           <Text size="xl">@devtable</Text>
+        </Group>
+        <Group position="right">
+          <AccountDropdown />
         </Group>
       </Group>
     </MantineHeader>
--- website/src/frames/app/header/index.tsx
+++ website/src/frames/app/header/index.tsx
@@ -1,4 +1,5 @@
 import { Group, Header as MantineHeader, Text } from '@mantine/core';
+import { AccountDropdown } from '../../../components/account-dropdown';
 import { MericoLogo } from '../../../resources/merico-logo';
 import { useAccountContext } from '../../require-auth/account-context';
 import { DeleteDashboard } from './delete-dashboard';
@@ -6,13 +7,16 @@ import { DeleteDashboard } from './delete-dashboard';
 export function Header() {
   const { canEdit } = useAccountContext();
   return (
-    <MantineHeader height={60} p="md">
-      <Group position="apart">
+    <MantineHeader height={60} px="md" py={0}>
+      <Group position="apart" sx={{ height: 60 }}>
         <Group>
           <MericoLogo width={40} />
           <Text size="xl">@devtable</Text>
         </Group>
-        <Group position="right">{canEdit && <DeleteDashboard />}</Group>
+        <Group position="right">
+          {canEdit && <DeleteDashboard />}
+          <AccountDropdown />
+        </Group>
       </Group>
     </MantineHeader>
   );
