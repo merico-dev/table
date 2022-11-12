63f0815f9465ba31ffbebab689eb5aa6c704abb4
Leto <gerile.tu@merico.dev>
2022-11-13 01:11:33 +0800

    feat(dashboard): viz-expert-system supports issues count

--- dashboard/src/plugins/viz-components/expert-system/request/schema/performance/quality.structure.ts
+++ dashboard/src/plugins/viz-components/expert-system/request/schema/performance/quality.structure.ts
@@ -6,13 +6,12 @@ export default `
   test_coverage: number;
   dryness: number;
   modularity: number;
-  issues?: {
-    blocker: number;
-    critical: number;
-    info: number;
-    major: number;
-    minor: number;
-  };
   issues_density: number;
+
+  issues_blocker: number;
+  issues_critical: number;
+  issues_info: number;
+  issues_major: number;
+  issues_minor: number;
 }
 `;
