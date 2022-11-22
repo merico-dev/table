f7acae7404bf936a551496d1d6d7945a0fa44763
Leto <gerile.tu@merico.dev>
2022-11-23 06:40:10 +0800

    feat(website): create dashboard with chosen schema file

--- website/src/frames/app/navbar/import-dashboard.tsx
+++ website/src/frames/app/navbar/import-dashboard.tsx
@@ -1,15 +1,17 @@
 import { ActionIcon, Box, Button, FileInput, Group, LoadingOverlay, Modal, TextInput, Tooltip } from '@mantine/core';
 import { showNotification, updateNotification } from '@mantine/notifications';
 import { useRequest } from 'ahooks';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { FileImport } from 'tabler-icons-react';
 import { DashboardAPI } from '../../../api-caller/dashboard';
 
+type TDashboardContent_Temp = Record<string, any> | null; // FIXME: can't use IDashboard, need to fix IDashboard type def first;
+
 interface IFormValues {
   name: string;
-  content: Record<string, any> | null; // FIXME: can't use IDashboard, need to fix IDashboard type def first
+  content: TDashboardContent_Temp;
 }
 
 function ImportDashboardForm({ postSubmit }: { postSubmit: () => void }) {
@@ -19,7 +21,10 @@ function ImportDashboardForm({ postSubmit }: { postSubmit: () => void }) {
     control,
     handleSubmit,
     formState: { errors },
+    setValue,
     watch,
+    setError,
+    clearErrors,
   } = useForm<IFormValues>({
     defaultValues: {
       name: '',
@@ -67,7 +72,37 @@ function ImportDashboardForm({ postSubmit }: { postSubmit: () => void }) {
     },
   );
 
-  const [value, setValue] = useState<File | null>(null);
+  const [file, setFile] = useState<File | null>(null);
+
+  useEffect(() => {
+    if (!file) {
+      return;
+    }
+    try {
+      const fileReader = new FileReader();
+      fileReader.readAsText(file, 'UTF-8');
+      fileReader.onload = (e) => {
+        if (e.target === null) {
+          throw new Error('fileReader failed with null result');
+        }
+
+        const content = e.target.result;
+
+        console.groupCollapsed('content of the chosen file');
+        console.log(content);
+        console.groupEnd();
+
+        if (typeof content !== 'string') {
+          throw new Error(`unparsable file content of type: ${typeof content}`);
+        }
+
+        setValue('content', JSON.parse(content));
+      };
+      clearErrors('content');
+    } catch (error: $TSFixMe | ErrorOptions) {
+      setError('content', error);
+    }
+  }, [file]);
 
   const [name, content] = watch(['name', 'content']);
   const disabled = !name || !content;
@@ -92,7 +127,7 @@ function ImportDashboardForm({ postSubmit }: { postSubmit: () => void }) {
             />
           )}
         />
-        <FileInput label="JSON File" required value={value} onChange={setValue} />
+        <FileInput label="JSON File" required value={file} onChange={setFile} error={errors?.content?.message} />
         <Group position="right" mt="md">
           <Button type="submit" disabled={disabled}>
             Confirm
