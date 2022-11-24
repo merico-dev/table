53c4df1a0057d8898c3c967797398203245d504c
Leto <gerile.tu@merico.dev>
2022-11-24 20:46:09 +0800

    fix(dashboard): rm use-select-first-option

--- dashboard/src/filter/filter-multi-select/render.tsx
+++ dashboard/src/filter/filter-multi-select/render.tsx
@@ -1,5 +1,6 @@
 import { MultiSelect } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
 import { useModelContext } from '~/contexts';
 import { FilterModelInstance } from '../../model';
 import { IFilterConfig_MultiSelect } from '../../model/filters/filter/multi-select';
@@ -15,6 +16,20 @@ export const FilterMultiSelect = observer(({ label, config, value, onChange }: I
   const usingRemoteOptions = !!config.options_query_id;
   const { data: remoteOptions, state } = model.getDataStuffByID(config.options_query_id);
   const loading = state === 'loading';
+
+  useEffect(() => {
+    if (!config.select_first_by_default) {
+      return;
+    }
+    // @ts-expect-error type of remoteOptions
+    const newValue = [remoteOptions[0]?.value] ?? [];
+    if (JSON.stringify(value) === JSON.stringify(newValue)) {
+      return;
+    }
+
+    console.log('Selecting the first option by default. Previous value: ', value, ', new value: ', newValue);
+    onChange(newValue);
+  }, [config.select_first_by_default, remoteOptions, onChange, value]);
 
   return (
     <MultiSelect
--- dashboard/src/filter/filter-select/render.tsx
+++ dashboard/src/filter/filter-select/render.tsx
@@ -1,9 +1,9 @@
 import { Select } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
 import { useModelContext } from '~/contexts';
 import { FilterModelInstance } from '../../model';
 import { IFilterConfig_Select } from '../../model/filters/filter/select';
-import { useSelectFirstOption } from '../use-select-first-option';
 
 interface IFilterSelect extends Omit<FilterModelInstance, 'key' | 'type' | 'config'> {
   config: IFilterConfig_Select;
@@ -17,7 +17,19 @@ export const FilterSelect = observer(({ label, config, value, onChange }: IFilte
   const { data: remoteOptions, state } = model.getDataStuffByID(config.options_query_id);
   const loading = state === 'loading';
 
-  useSelectFirstOption({ config, value, onChange, options: remoteOptions });
+  useEffect(() => {
+    if (!config.select_first_by_default) {
+      return;
+    }
+    // @ts-expect-error type of remoteOptions
+    const newValue = remoteOptions[0]?.value ?? '';
+    if (value === newValue) {
+      return;
+    }
+
+    console.log('Selecting the first option by default. Previous value: ', value, ', new value: ', newValue);
+    onChange(newValue);
+  }, [config.select_first_by_default, remoteOptions, onChange, value]);
 
   return (
     <Select
