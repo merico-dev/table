6acca120e9f596732ced17fc5554071908760fb2
Leto <gerile.tu@merico.dev>
2022-12-01 04:09:17 +0800

    feat(dashboard): render button’s content with template

--- dashboard/src/plugins/viz-components/button/viz-button.tsx
+++ dashboard/src/plugins/viz-components/button/viz-button.tsx
@@ -1,20 +1,28 @@
 import { Button, Center } from '@mantine/core';
-import { defaultsDeep } from 'lodash';
+import { defaultsDeep, template } from 'lodash';
+import { observer } from 'mobx-react-lite';
 import { useMemo } from 'react';
+import { useModelContext } from '~/contexts';
 import { VizViewProps } from '../../../types/plugin';
 import { useStorageData } from '../../hooks';
 import { DEFAULT_CONFIG, IButtonConf } from './type';
 
-export function VizButton({ context }: VizViewProps) {
+export const VizButton = observer(({ context }: VizViewProps) => {
+  const model = useModelContext();
   const { value: confValue } = useStorageData<IButtonConf>(context.instanceData, 'config');
   const conf: IButtonConf = useMemo(() => defaultsDeep({}, confValue, DEFAULT_CONFIG), [confValue]);
 
   const { content, ...mantineProps } = conf;
 
   const { width, height } = context.viewport;
+
+  const params = {
+    filters: model.filters.values,
+    context: model.context.current,
+  };
   return (
     <Center sx={{ width, height }}>
-      <Button {...mantineProps}>{content}</Button>
+      <Button {...mantineProps}>{template(content)(params)}</Button>
     </Center>
   );
-}
+});
