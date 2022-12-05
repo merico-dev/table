957ddcbf8dbd38b7ba498e318aebf73db9fd0a7e
Leto <gerile.tu@merico.dev>
2022-12-05 15:41:59 +0800

    style(dashboard): simplier yAxes editors of cartesian

--- dashboard/src/plugins/viz-components/cartesian/panel/y-axes.tsx
+++ dashboard/src/plugins/viz-components/cartesian/panel/y-axes.tsx
@@ -1,6 +1,6 @@
-import { ActionIcon, Button, Divider, Group, Select, Stack, TextInput } from '@mantine/core';
+import { ActionIcon, Button, Divider, Group, Select, Stack, Tabs, TextInput } from '@mantine/core';
 import { Control, Controller, useFieldArray, UseFieldArrayRemove, UseFormWatch } from 'react-hook-form';
-import { Trash } from 'tabler-icons-react';
+import { Plus, Trash } from 'tabler-icons-react';
 import { defaultNumbroFormat, NumbroFormatSelector } from '~/panel/settings/common/numbro-format-selector';
 import { ICartesianChartConf } from '../type';
 
@@ -22,8 +22,8 @@ interface IYAxisField {
 
 function YAxisField({ control, index, remove }: IYAxisField) {
   return (
-    <Stack my={0} p="md" pr={40} sx={{ border: '1px solid #eee', position: 'relative' }}>
-      <Divider mb={-15} variant="dashed" label="Name" labelPosition="center" />
+    <Stack my={0} p="0" sx={{ position: 'relative' }}>
+      <Divider mb={-15} mt={15} variant="dashed" label="Name" labelPosition="center" />
       <Group grow noWrap>
         <Controller
           name={`y_axes.${index}.name`}
@@ -56,15 +56,16 @@ function YAxisField({ control, index, remove }: IYAxisField) {
           render={({ field }) => <NumbroFormatSelector {...field} />}
         />
       </Stack>
-      <ActionIcon
+      <Button
+        mt={20}
+        leftIcon={<Trash size={16} />}
         color="red"
-        variant="subtle"
+        variant="light"
         onClick={() => remove(index)}
-        sx={{ position: 'absolute', top: 15, right: 5 }}
         disabled={index === 0}
       >
-        <Trash size={16} />
-      </ActionIcon>
+        Delete this YAxis
+      </Button>
     </Stack>
   );
 }
@@ -94,13 +95,36 @@ export function YAxesField({ control, watch }: IYAxesField) {
     });
 
   return (
-    <Stack>
+    <Tabs
+      defaultValue="0"
+      styles={{
+        tab: {
+          paddingTop: '0px',
+          paddingBottom: '0px',
+        },
+        panel: {
+          padding: '0px',
+        },
+      }}
+    >
+      <Tabs.List>
+        {controlledFields.map((field, index) => (
+          <Tabs.Tab key={index} value={index.toString()}>
+            {index + 1}
+            {/* {field.name.trim() ? field.name : index + 1} */}
+          </Tabs.Tab>
+        ))}
+        <Tabs.Tab onClick={addYAxis} value="add">
+          <ActionIcon>
+            <Plus size={18} color="#228be6" />
+          </ActionIcon>
+        </Tabs.Tab>
+      </Tabs.List>
       {controlledFields.map((field, index) => (
-        <YAxisField key={index} control={control} index={index} remove={remove} />
+        <Tabs.Panel key={index} value={index.toString()}>
+          <YAxisField control={control} index={index} remove={remove} />
+        </Tabs.Panel>
       ))}
-      <Group position="center" mt="xs">
-        <Button onClick={addYAxis}>Add a Y Axis</Button>
-      </Group>
-    </Stack>
+    </Tabs>
   );
 }
