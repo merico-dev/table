76fa49c662e06d7f423ca7eff1d36a98124e1bc3
Leto <gerile.tu@merico.dev>
2022-12-08 16:29:24 +0800

    fix(dashboard): catch error from parsing func_content failure

--- dashboard/src/plugins/viz-components/cartesian/panel/scatter-size-select/get-echarts-symbol-size.ts
+++ dashboard/src/plugins/viz-components/cartesian/panel/scatter-size-select/get-echarts-symbol-size.ts
@@ -14,6 +14,12 @@ export function getEchartsSymbolSize({ type, ...rest }: TScatterSize, data: AnyO
   const rows = keyBy(data, x_axis_data_key);
   return (_value: number, params: $TSFixMe) => {
     const row = rows[params.name];
-    return new Function(`return ${func_content}`)()(row, params);
+    try {
+      return new Function(`return ${func_content}`)()(row, params);
+    } catch (error) {
+      // @ts-expect-error Object is of type 'unknown'.
+      console.error(`[getEchartsSymbolSize] failed parsing custom function, error: ${error.message}`);
+      return 10;
+    }
   };
 }
