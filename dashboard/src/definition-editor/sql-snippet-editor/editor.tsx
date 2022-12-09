269b02bbbbfc80e0c5fe51dd8660783699eda4bd
Leto <gerile.tu@merico.dev>
2022-12-09 15:07:12 +0800

    style(dashboard): separate snippets with tabs

--- dashboard/src/definition-editor/sql-snippet-editor/editor.tsx
+++ dashboard/src/definition-editor/sql-snippet-editor/editor.tsx
@@ -1,8 +1,8 @@
-import { ActionIcon, Button, Group, Stack, Text, Textarea, TextInput } from '@mantine/core';
+import { ActionIcon, Button, Group, Stack, Tabs, Text, Textarea, TextInput } from '@mantine/core';
 import { randomId } from '@mantine/hooks';
 import { observer } from 'mobx-react-lite';
 import { cast } from 'mobx-state-tree';
-import { Trash } from 'tabler-icons-react';
+import { Plus, Trash } from 'tabler-icons-react';
 import { useModelContext } from '../../contexts';
 import { PreviewSnippet } from './preview-snippet';
 
@@ -18,55 +18,52 @@ export const SQLSnippetsEditor = observer(function _SQLSnippetsEditor() {
     );
 
   return (
-    <Stack sx={{ border: '1px solid #eee', flexGrow: 1 }}>
-      <Group
-        position="left"
-        pl="md"
-        py="md"
-        sx={{ borderBottom: '1px solid #eee', background: '#efefef', flexGrow: 0 }}
-      >
-        <Text weight={500}>SQL Snippets</Text>
-      </Group>
-      <Group px="md" pb="md" pt="md">
-        <Stack sx={{ width: '100%', position: 'relative' }}>
-          {model.sqlSnippets.current.map((item, index) => (
-            <Stack key={index} my={0} p="md" pr={40} sx={{ border: '1px solid #eee', position: 'relative' }}>
-              <TextInput
-                label="Key"
-                required
-                value={item.key}
-                onChange={(e) => {
-                  item.setKey(e.currentTarget.value);
-                }}
-              />
-              <Textarea
-                minRows={3}
-                label="Value"
-                required
-                value={item.value}
-                onChange={(e) => {
-                  item.setValue(e.currentTarget.value);
-                }}
-                className="code-textarea"
-              />
-              <PreviewSnippet value={item.value} />
-              <ActionIcon
-                color="red"
-                variant="subtle"
-                onClick={() => model.sqlSnippets.remove(index)}
-                sx={{ position: 'absolute', top: 15, right: 5 }}
-              >
-                <Trash size={16} />
-              </ActionIcon>
-            </Stack>
-          ))}
-          <Group position="center" mt="xl" grow sx={{ width: '40%' }} mx="auto">
-            <Button variant="default" onClick={addSnippet}>
-              Add a snippet
+    <Tabs defaultValue={'0'}>
+      <Tabs.List>
+        {model.sqlSnippets.current.map((item, index) => (
+          <Tabs.Tab value={index.toString()}>{index + 1}</Tabs.Tab>
+        ))}
+        <Tabs.Tab onClick={addSnippet} value="add">
+          <ActionIcon>
+            <Plus size={18} color="#228be6" />
+          </ActionIcon>
+        </Tabs.Tab>
+      </Tabs.List>
+      {model.sqlSnippets.current.map((item, index) => (
+        <Tabs.Panel value={index.toString()}>
+          <Stack key={index} my={0} p="md" pr={40}>
+            <TextInput
+              label="Key"
+              required
+              value={item.key}
+              onChange={(e) => {
+                item.setKey(e.currentTarget.value);
+              }}
+            />
+            <Textarea
+              minRows={3}
+              label="Value"
+              required
+              value={item.value}
+              onChange={(e) => {
+                item.setValue(e.currentTarget.value);
+              }}
+              className="code-textarea"
+            />
+            <PreviewSnippet value={item.value} />
+
+            <Button
+              mt={20}
+              leftIcon={<Trash size={16} />}
+              color="red"
+              variant="light"
+              onClick={() => model.sqlSnippets.remove(index)}
+            >
+              Delete this SQL Snippet
             </Button>
-          </Group>
-        </Stack>
-      </Group>
-    </Stack>
+          </Stack>
+        </Tabs.Panel>
+      ))}
+    </Tabs>
   );
 });
