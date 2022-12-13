d5608abed0533083b24c206583869467bfbce4b4
Leto <gerile.tu@merico.dev>
2022-12-14 01:23:29 +0800

    chore(dashboard): misc

--- dashboard/src/filter/filter-tree-select/render/tree-icon.tsx
+++ dashboard/src/filter/filter-tree-select/render/tree-icon.tsx
@@ -9,10 +9,10 @@ const CheckboxIcon = ({ onClick }: { onClick: () => void }) => {
       height="16"
       viewBox="0 0 24 24"
       stroke="currentColor"
-      stroke-width="2"
+      strokeWidth="2"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <rect className="border" x="4" y="4" width="16" height="16" rx="2" />
