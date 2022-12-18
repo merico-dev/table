48fda5607e39349336ff5c922120c3a4046615ba
Leto <gerile.tu@merico.dev>
2022-12-18 22:05:47 +0800

    fix(dashboard): cartesian - category xaxis’ not showing label

--- dashboard/src/plugins/viz-components/cartesian/option/x-axis.ts
+++ dashboard/src/plugins/viz-components/cartesian/option/x-axis.ts
@@ -1,5 +1,4 @@
 import { getEchartsXAxisLabel } from '../panel/x-axis/x-axis-label-formatter/get-echarts-x-axis-tick-label';
-import { DEFAULT_X_AXIS_LABEL_FORMATTER } from '../panel/x-axis/x-axis-label-formatter/types';
 import { ICartesianChartConf } from '../type';
 
 export function getXAxes(conf: ICartesianChartConf, xAxisData: $TSFixMe[], regressionXAxes: $TSFixMe[]) {
