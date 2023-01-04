e45d94c03689119d266dd4e6f3f06b367e28f4b5
Leto <gerile.tu@merico.dev>
2023-01-05 00:55:51 +0800

    feat(settings-form): loading & export version

--- settings-form/src/index.ts
+++ settings-form/src/index.ts
@@ -1,3 +1,6 @@
+export const version = import('../package.json').then((p) => p.version);
+console.log(`[@devtable/settings-form] version: ${version}`);
+
 export * from './datasource';
 export * from './account';
 export * from './api-key';
