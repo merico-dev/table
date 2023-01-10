f7740feb0a390c21eaf17bf5b3ce0a7defd2ab1e
Leto <gerile.tu@merico.dev>
2023-01-10 13:52:39 +0800

    feat(dashboard): draw slider zoom over xAxis of cartesian

--- dashboard/src/plugins/viz-components/cartesian/panel/echarts-zooming-field/get-echarts-data-zoom-option.ts
+++ dashboard/src/plugins/viz-components/cartesian/panel/echarts-zooming-field/get-echarts-data-zoom-option.ts
@@ -6,12 +6,26 @@ export function getEchartsDataZoomOption(conf: TEchartsDataZoomConfig) {
     ret.push({
       type: 'inside',
       xAxisIndex: [0],
+      filterMode: 'none',
     });
   }
   if (conf.y_axis_scroll) {
     ret.push({
       type: 'inside',
       yAxisIndex: [0],
+      filterMode: 'none',
+    });
+  }
+  if (conf.x_axis_slider) {
+    ret.push({
+      type: 'slider',
+      xAxisIndex: [0],
+      filterMode: 'none',
+      bottom: 'auto',
+      top: 0,
+      height: 15,
+      moveHandleSize: 0,
+      showDataShadow: false,
     });
   }
   return ret;
