e16405b32114bda741822b0ac6ecdb262abb7e78
Leto <gerile.tu@merico.dev>
2023-02-07 13:08:55 +0800

    style(dashboard): fix actionIconGroupStyle

--- dashboard/src/main/actions.tsx
+++ dashboard/src/main/actions.tsx
@@ -16,6 +16,14 @@ const actionIconGroupStyle = {
       borderBottomRightRadius: 0,
       borderRightWidth: 0.5,
     },
+    ':not(:first-of-type):not(:last-of-type)': {
+      borderTopRightRadius: 0,
+      borderBottomRightRadius: 0,
+      borderTopLeftRadius: 0,
+      borderBottomLeftRadius: 0,
+      borderLeftWidth: 0.5,
+      borderRightWidth: 0.5,
+    },
     '&:last-of-type': {
       borderTopLeftRadius: 0,
       borderBottomLeftRadius: 0,
