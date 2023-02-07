dee223eaafe4ee7ef9ac57a2ffa5bd3008812ba1
Leto <gerile.tu@merico.dev>
2023-02-07 20:57:34 +0800

    feat(website): reload dashboard list after deleting one

--- website/src/frames/app/navbar/dashboard-links/action-menu/delete-dashboard.tsx
+++ website/src/frames/app/navbar/dashboard-links/action-menu/delete-dashboard.tsx
@@ -1,13 +1,17 @@
 import { Menu, Text } from '@mantine/core';
 import { useModals } from '@mantine/modals';
 import { showNotification, updateNotification } from '@mantine/notifications';
+import { observer } from 'mobx-react-lite';
 import { useNavigate } from 'react-router-dom';
 import { Trash } from 'tabler-icons-react';
 import { DashboardAPI } from '../../../../../api-caller/dashboard';
+import { useDashboardStore } from '../../../models/dashboard-store-context';
 
-export function DeleteDashboard({ id }: { id: string }) {
-  const navigate = useNavigate();
+export const DeleteDashboard = observer(({ id }: { id: string }) => {
   const modals = useModals();
+  const navigate = useNavigate();
+  const { store } = useDashboardStore();
+
   const doDelete = async () => {
     if (!id) {
       return;
@@ -26,7 +30,9 @@ export function DeleteDashboard({ id }: { id: string }) {
       color: 'green',
     });
     navigate('/dashboard');
+    store.load();
   };
+
   const confirmAndDelete = () =>
     modals.openConfirmModal({
       title: 'Delete this dashboard?',
@@ -43,4 +49,4 @@ export function DeleteDashboard({ id }: { id: string }) {
       Delete this dashboard
     </Menu.Item>
   );
-}
+});
