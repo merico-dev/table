4de14ec3cb0b02a1a84c1db5e964d366664417e7
Leto <gerile.tu@merico.dev>
2023-02-08 23:41:15 +0800

    fix(dashboard): omit yaxis’ min&max when they’re emtpy

--- dashboard/src/plugins/viz-components/cartesian/option/y-axis.ts
+++ dashboard/src/plugins/viz-components/cartesian/option/y-axis.ts
@@ -1,13 +1,15 @@
 import { ICartesianChartConf, IYAxisConf } from '../type';
 
 export function getYAxes(conf: ICartesianChartConf, labelFormatters: Record<string, (p: $TSFixMe) => string>) {
-  return conf.y_axes.map(({ nameAlignment, ...rest }: IYAxisConf, index: number) => {
+  return conf.y_axes.map(({ nameAlignment, min, max, ...rest }: IYAxisConf, index: number) => {
     let position = rest.position;
     if (!position) {
       position = index > 0 ? 'right' : 'left';
     }
     return {
       ...rest,
+      min: min ? min : undefined,
+      max: max ? max : undefined,
       position,
       axisLabel: {
         show: true,
