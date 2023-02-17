438dc9157a88f188608450a34fd23261aa9dad92
Leto <gerile.tu@merico.dev>
2023-02-18 01:00:32 +0800

    chore(dashboard): remove unused type def

--- dashboard/src/types/index.ts
+++ dashboard/src/types/index.ts
@@ -1,4 +1,3 @@
 export * from './dashboard';
 export * from './filter';
-export * from './viz-panel';
 export type { AnyObject, Ready } from './utils';
--- dashboard/src/types/viz-panel.ts
+++
@@ -1,7 +0,0 @@
-import { IVizConfig } from './dashboard';
-
-export interface IVizPanelProps {
-  conf: IVizConfig['conf'];
-  setConf: (conf: IVizConfig['conf']) => void;
-  data: $TSFixMe[];
-}
