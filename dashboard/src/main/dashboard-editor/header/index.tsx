81416b40416a7102bc133958fe75f07faf2f1ec7
Leto <gerile.tu@merico.dev>
2023-02-18 15:53:50 +0800

    feat(dashboard): move download buttons to editor-header

--- dashboard/src/main/dashboard-editor/header/index.tsx
+++ dashboard/src/main/dashboard-editor/header/index.tsx
@@ -1,14 +1,39 @@
 import { ActionIcon, Group, Header as MantineHeader, Text, Tooltip } from '@mantine/core';
-import { IconDeviceFloppy, IconRecycle } from '@tabler/icons';
+import { IconCode, IconDeviceFloppy, IconDownload, IconFileDownload, IconRecycle } from '@tabler/icons';
 import { observer } from 'mobx-react-lite';
+import { useCallback } from 'react';
 import { useModelContext } from '~/contexts';
 import { ActionIconGroupStyle } from '~/styles/action-icon-group-style';
+import { downloadJSON } from '~/utils/download';
 
 export const DashboardEditorHeader = observer(({ saveDashboardChanges }: { saveDashboardChanges: () => void }) => {
   const model = useModelContext();
 
+  const getCurrentSchema = useCallback(() => {
+    const queries = model.queries.json;
+    const views = model.views.json;
+    const sqlSnippets = model.sqlSnippets.json;
+    const filters = model.filters.current;
+    const mock_context = model.mock_context.current;
+    return {
+      filters,
+      views,
+      definition: {
+        sqlSnippets,
+        queries,
+        mock_context,
+      },
+      version: model.version,
+    };
+  }, [model]);
+
   const revertChanges = () => {
     model.reset();
+  };
+
+  const downloadSchema = () => {
+    const schema = JSON.stringify(getCurrentSchema(), null, 2);
+    downloadJSON(model.name, schema);
   };
 
   const hasChanges = model.changed;
@@ -20,6 +45,19 @@ export const DashboardEditorHeader = observer(({ saveDashboardChanges }: { saveD
           <Text size="xl">{model.name}</Text>
         </Group>
         <Group position="right">
+          <Group spacing={0} sx={ActionIconGroupStyle}>
+            <Tooltip label="Download Schema">
+              <ActionIcon variant="default" size="md" onClick={downloadSchema}>
+                <IconCode size={18} />
+              </ActionIcon>
+            </Tooltip>
+            <Tooltip label="Download Data">
+              <ActionIcon variant="default" size="md" onClick={model.queries.downloadAllData}>
+                <IconDownload size={18} />
+              </ActionIcon>
+            </Tooltip>
+          </Group>
+
           <Group spacing={0} sx={ActionIconGroupStyle}>
             <Tooltip label="Save Changes">
               <ActionIcon variant="default" size="md" onClick={saveDashboardChanges} disabled={!hasChanges}>
--- dashboard/src/view/dashboard-actions.tsx
+++ dashboard/src/view/dashboard-actions.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { Code, Database, Download, Filter, Link, PlaylistAdd, Share } from 'tabler-icons-react';
 import { InteractionsViewerModal } from '~/interactions/interactions-viewer';
 import { ActionIconGroupStyle } from '~/styles/action-icon-group-style';
-import { downloadJSON } from '~/utils/download';
 import { useModelContext } from '../contexts';
 import { DataEditorModal } from '../definition-editor';
 import { FilterSettingsModal } from '../filter/filter-settings';
@@ -17,24 +16,6 @@ interface IDashboardActions {
 export const DashboardActions = observer(function _DashboardActions({ inUseMode }: IDashboardActions) {
   const model = useModelContext();
 
-  const getCurrentSchema = React.useCallback(() => {
-    const queries = model.queries.json;
-    const views = model.views.json;
-    const sqlSnippets = model.sqlSnippets.json;
-    const filters = model.filters.current;
-    const mock_context = model.mock_context.current;
-    return {
-      filters,
-      views,
-      definition: {
-        sqlSnippets,
-        queries,
-        mock_context,
-      },
-      version: model.version,
-    };
-  }, [model]);
-
   const [dataEditorOpened, setDataEditorOpened] = React.useState(false);
   const openQueries = () => setDataEditorOpened(true);
   const closeQueries = () => setDataEditorOpened(false);
@@ -46,11 +27,6 @@ export const DashboardActions = observer(function _DashboardActions({ inUseMode 
   const [interactionsOpened, setInteractionsOpened] = React.useState(false);
   const openInteractions = () => setInteractionsOpened(true);
   const closeInteractions = () => setInteractionsOpened(false);
-
-  const downloadSchema = () => {
-    const schema = JSON.stringify(getCurrentSchema(), null, 2);
-    downloadJSON(model.name, schema);
-  };
 
   if (inUseMode) {
     return null;
@@ -88,22 +64,6 @@ export const DashboardActions = observer(function _DashboardActions({ inUseMode 
             </ActionIcon>
           </Tooltip>
         </Group>
-
-        <Menu width={180} trigger="hover">
-          <Menu.Target>
-            <ActionIcon variant="default" size="md">
-              <Share size={20} />
-            </ActionIcon>
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Menu.Item icon={<Download size={14} />} onClick={model.queries.downloadAllData}>
-              Download Data
-            </Menu.Item>
-            <Menu.Item icon={<Code size={14} />} onClick={downloadSchema}>
-              Download Schema
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
       </Group>
       <FilterSettingsModal opened={filtersOpened} close={closeFilters} />
       <DataEditorModal opened={dataEditorOpened} close={closeQueries} />
