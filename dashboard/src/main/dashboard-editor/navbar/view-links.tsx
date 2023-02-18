97047c23476a46a3c3e3166642d69773eca1de46
Leto <gerile.tu@merico.dev>
2023-02-18 19:30:14 +0800

    feat(dashboard): open view settings from view list

--- dashboard/src/main/dashboard-editor/navbar/view-links.tsx
+++ dashboard/src/main/dashboard-editor/navbar/view-links.tsx
@@ -1,79 +1,61 @@
-import { ActionIcon, Box, Button, Divider, Group, Stack, Text, Tooltip, UnstyledButton } from '@mantine/core';
-import { IconPlus } from '@tabler/icons';
+import { ActionIcon, Box, Button, Divider, Group, Text, Tooltip, UnstyledButton } from '@mantine/core';
+import { IconAdjustments, IconPlus } from '@tabler/icons';
 import { useBoolean } from 'ahooks';
 import { observer } from 'mobx-react-lite';
 import { useCallback } from 'react';
 import { useModelContext } from '~/contexts';
-import { EViewComponentType, ViewComponentTypeColor, ViewComponentTypeName } from '~/types';
-
-const ViewComponentTypeIndicator = ({ type, active }: { type: EViewComponentType; active: boolean }) => {
-  return (
-    <Tooltip
-      position="right"
-      label={ViewComponentTypeName[type]}
-      withinPortal
-      opened={active}
-      color={ViewComponentTypeColor[type]}
-      withArrow
-    >
-      <Box
-        className="view-type-indicator"
-        sx={{
-          background: ViewComponentTypeColor[type],
-          width: '2px',
-          transition: 'width 200ms ease',
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          zIndex: 3,
-        }}
-      />
-    </Tooltip>
-  );
-};
+import { EditViewModal } from '../switch-views/edit-view-modal';
 
 interface IViewLink {
   onClick: () => void;
   name: string;
   active: boolean;
-  type: EViewComponentType;
+  openSettings: () => void;
 }
 
-function ViewLink({ onClick, name, active, type }: IViewLink) {
-  const [tooltip, { setTrue, setFalse }] = useBoolean(false);
+function ViewLink({ onClick, name, active, openSettings }: IViewLink) {
   return (
-    <UnstyledButton
-      sx={(theme) => ({
-        display: 'block',
-        width: '100%',
-        padding: theme.spacing.xs,
-        borderRadius: 0,
-        position: 'relative',
-        color: theme.black,
+    <Box sx={{ position: 'relative' }}>
+      <UnstyledButton
+        sx={(theme) => ({
+          display: 'block',
+          width: '100%',
+          padding: theme.spacing.xs,
+          borderRadius: 0,
+          position: 'relative',
+          color: theme.black,
 
-        '&:hover': {
-          backgroundColor: theme.colors.gray[0],
-        },
+          '&:hover': {
+            backgroundColor: theme.colors.gray[0],
+          },
 
-        backgroundColor: active ? theme.colors.gray[2] : 'transparent',
-      })}
-      onMouseEnter={setTrue}
-      onMouseLeave={setFalse}
-      onClick={onClick}
-    >
-      <ViewComponentTypeIndicator type={type} active={tooltip} />
-      <Group>
-        <Text size="sm">{name}</Text>
-      </Group>
-    </UnstyledButton>
+          backgroundColor: active ? theme.colors.gray[2] : 'transparent',
+        })}
+      >
+        <Group sx={{ width: 'calc(100% - 28px)' }} onClick={onClick}>
+          <Text size="sm">{name}</Text>
+        </Group>
+      </UnstyledButton>
+      {active && (
+        <Tooltip label="Edit" position="right" withinPortal>
+          <ActionIcon
+            onClick={openSettings}
+            variant="light"
+            color="blue"
+            sx={{ position: 'absolute', top: 0, bottom: 0, height: '100%', right: 0, zIndex: 0, borderRadius: 0 }}
+          >
+            <IconAdjustments size={16} />
+          </ActionIcon>
+        </Tooltip>
+      )}
+    </Box>
   );
 }
 
 export const ViewLinks = observer(() => {
   const model = useModelContext();
   const getClickHandler = useCallback((id: string) => () => model.views.setIDOfVIE(id), [model]);
-
+  const [settingsVisible, { setTrue, setFalse }] = useBoolean(false);
   return (
     <Box pt="sm" sx={{ position: 'relative' }}>
       {model.views.options.map((v) => (
@@ -82,7 +64,7 @@ export const ViewLinks = observer(() => {
           active={model.views.idOfVIE === v.value}
           name={v.label}
           onClick={getClickHandler(v.value)}
-          type={v.type}
+          openSettings={setTrue}
         />
       ))}
       <Divider variant="dashed" />
@@ -102,6 +84,7 @@ export const ViewLinks = observer(() => {
       >
         Add a View
       </Button>
+      <EditViewModal opened={settingsVisible} close={setFalse} />
     </Box>
   );
 });
