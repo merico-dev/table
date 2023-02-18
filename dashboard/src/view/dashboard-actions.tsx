1afb80a312c377d7da1ad51822608292eea60bbd
Leto <gerile.tu@merico.dev>
2023-02-18 15:38:48 +0800

    refactor(dashboard): extract ActionIconGroupStyle

--- dashboard/src/view/dashboard-actions.tsx
+++ dashboard/src/view/dashboard-actions.tsx
@@ -3,34 +3,12 @@ import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Code, Database, DeviceFloppy, Download, Filter, Link, PlaylistAdd, Recycle, Share } from 'tabler-icons-react';
 import { InteractionsViewerModal } from '~/interactions/interactions-viewer';
+import { ActionIconGroupStyle } from '~/styles/action-icon-group-style';
 import { downloadJSON } from '~/utils/download';
 import { useModelContext } from '../contexts';
 import { DataEditorModal } from '../definition-editor';
 import { FilterSettingsModal } from '../filter/filter-settings';
 import { SwitchViews } from '../main/dashboard-editor/switch-views';
-
-const actionIconGroupStyle = {
-  '> button': {
-    '&:first-of-type': {
-      borderTopRightRadius: 0,
-      borderBottomRightRadius: 0,
-      borderRightWidth: 0.5,
-    },
-    ':not(:first-of-type):not(:last-of-type)': {
-      borderTopRightRadius: 0,
-      borderBottomRightRadius: 0,
-      borderTopLeftRadius: 0,
-      borderBottomLeftRadius: 0,
-      borderLeftWidth: 0.5,
-      borderRightWidth: 0.5,
-    },
-    '&:last-of-type': {
-      borderTopLeftRadius: 0,
-      borderBottomLeftRadius: 0,
-      borderLeftWidth: 0.5,
-    },
-  },
-};
 
 interface IDashboardActions {
   saveChanges: () => void;
@@ -100,7 +78,7 @@ export const DashboardActions = observer(function _DashboardActions({ saveChange
         >
           Add a Panel
         </Button>
-        <Group spacing={0} sx={actionIconGroupStyle}>
+        <Group spacing={0} sx={ActionIconGroupStyle}>
           <Tooltip label="Filters">
             <ActionIcon variant="default" size="md" onClick={openFilters}>
               <Filter size={20} />
@@ -117,7 +95,7 @@ export const DashboardActions = observer(function _DashboardActions({ saveChange
             </ActionIcon>
           </Tooltip>
         </Group>
-        <Group spacing={0} sx={actionIconGroupStyle}>
+        <Group spacing={0} sx={ActionIconGroupStyle}>
           <Tooltip label="Save Changes">
             <ActionIcon variant="default" size="md" onClick={saveChanges} disabled={!hasChanges}>
               <DeviceFloppy size={20} color="green" />
