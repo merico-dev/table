f625bf20d83762ac51322e1f4f19399506a716b2
Leto <gerile.tu@merico.dev>
2023-02-18 18:13:42 +0800

    feat(dashboard): move more buttons from dashboard-actions into editor-navbar

--- dashboard/src/main/dashboard-editor/navbar/index.tsx
+++ dashboard/src/main/dashboard-editor/navbar/index.tsx
@@ -1,24 +1,61 @@
-import { Button, Group, Navbar as MantineNavbar, Text } from '@mantine/core';
-import { IconSettings } from '@tabler/icons';
+import { ActionIcon, Button, Group, Navbar as MantineNavbar, Text, Tooltip } from '@mantine/core';
+import { IconDatabase, IconFilter, IconLink, IconSettings } from '@tabler/icons';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { DataEditorModal } from '~/definition-editor';
+import { FilterSettingsModal } from '~/filter/filter-settings';
+import { InteractionsViewerModal } from '~/interactions/interactions-viewer';
+import { ActionIconGroupStyle } from '~/styles/action-icon-group-style';
+import { SwitchViews } from '../switch-views';
 
 export function DashboardEditorNavbar() {
-  const navigate = useNavigate();
+  const [dataEditorOpened, setDataEditorOpened] = useState(false);
+  const openQueries = () => setDataEditorOpened(true);
+  const closeQueries = () => setDataEditorOpened(false);
+
+  const [filtersOpened, setFiltersOpened] = useState(false);
+  const openFilters = () => setFiltersOpened(true);
+  const closeFilters = () => setFiltersOpened(false);
+
+  const [interactionsOpened, setInteractionsOpened] = useState(false);
+  const openInteractions = () => setInteractionsOpened(true);
+  const closeInteractions = () => setInteractionsOpened(false);
 
   return (
-    <MantineNavbar p="md" width={{ base: 200 }}>
+    <MantineNavbar p={0} width={{ base: 200 }}>
       <MantineNavbar.Section>
-        <Group grow pb="md" position="apart">
-          <Text align="center">F</Text>
-          <Text align="center">D</Text>
-          <Text align="center">I</Text>
+        <Group
+          grow
+          spacing={0}
+          sx={{ ...ActionIconGroupStyle, button: { borderWidth: 0, borderBottomWidth: 1, borderColor: '#e9ecef' } }}
+        >
+          <Tooltip label="Filters" withinPortal>
+            <ActionIcon variant="default" radius={0} size="md" onClick={openFilters}>
+              <IconFilter size={20} />
+            </ActionIcon>
+          </Tooltip>
+          <Tooltip label="Data Settings" withinPortal>
+            <ActionIcon variant="default" radius={0} size="md" onClick={openQueries}>
+              <IconDatabase size={20} />
+            </ActionIcon>
+          </Tooltip>
+          <Tooltip label="Interactions Viewer" withinPortal>
+            <ActionIcon variant="default" radius={0} size="md" onClick={openInteractions}>
+              <IconLink size={20} />
+            </ActionIcon>
+          </Tooltip>
         </Group>
+        <FilterSettingsModal opened={filtersOpened} close={closeFilters} />
+        <DataEditorModal opened={dataEditorOpened} close={closeQueries} />
+        <InteractionsViewerModal opened={interactionsOpened} close={closeInteractions} />
       </MantineNavbar.Section>
 
-      <MantineNavbar.Section grow>TODO:Views</MantineNavbar.Section>
+      <MantineNavbar.Section grow mt={10} sx={{ width: '100%', overflow: 'auto' }}>
+        <SwitchViews />
+      </MantineNavbar.Section>
 
       <MantineNavbar.Section>
-        <Group grow pt="sm" sx={{ borderTop: '1px solid #eee' }}>
+        <Group grow p="md" pt="sm" sx={{ borderTop: '1px solid #eee' }}>
           <Button size="sm" leftIcon={<IconSettings size={20} />}>
             Settings
           </Button>
--- dashboard/src/view/dashboard-actions.tsx
+++ dashboard/src/view/dashboard-actions.tsx
@@ -1,13 +1,7 @@
-import { ActionIcon, Button, Group, Menu, Tooltip } from '@mantine/core';
+import { Button, Group } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { Code, Database, Download, Filter, Link, PlaylistAdd, Share } from 'tabler-icons-react';
-import { InteractionsViewerModal } from '~/interactions/interactions-viewer';
-import { ActionIconGroupStyle } from '~/styles/action-icon-group-style';
+import { PlaylistAdd } from 'tabler-icons-react';
 import { useModelContext } from '../contexts';
-import { DataEditorModal } from '../definition-editor';
-import { FilterSettingsModal } from '../filter/filter-settings';
-import { SwitchViews } from '../main/dashboard-editor/switch-views';
 
 interface IDashboardActions {
   inUseMode: boolean;
@@ -16,27 +10,12 @@ interface IDashboardActions {
 export const DashboardActions = observer(function _DashboardActions({ inUseMode }: IDashboardActions) {
   const model = useModelContext();
 
-  const [dataEditorOpened, setDataEditorOpened] = React.useState(false);
-  const openQueries = () => setDataEditorOpened(true);
-  const closeQueries = () => setDataEditorOpened(false);
-
-  const [filtersOpened, setFiltersOpened] = React.useState(false);
-  const openFilters = () => setFiltersOpened(true);
-  const closeFilters = () => setFiltersOpened(false);
-
-  const [interactionsOpened, setInteractionsOpened] = React.useState(false);
-  const openInteractions = () => setInteractionsOpened(true);
-  const closeInteractions = () => setInteractionsOpened(false);
-
   if (inUseMode) {
     return null;
   }
 
   return (
-    <Group position="apart" pt={0} px={10} pb="xs">
-      <Group position="left">
-        <SwitchViews />
-      </Group>
+    <Group position="right" pt={0} px={10} pb="xs">
       <Group position="right" sx={{ button: { minWidth: '40px' } }}>
         <Button
           variant="filled"
@@ -47,27 +26,7 @@ export const DashboardActions = observer(function _DashboardActions({ inUseMode 
         >
           Add a Panel
         </Button>
-        <Group spacing={0} sx={ActionIconGroupStyle}>
-          <Tooltip label="Filters">
-            <ActionIcon variant="default" size="md" onClick={openFilters}>
-              <Filter size={20} />
-            </ActionIcon>
-          </Tooltip>
-          <Tooltip label="Data Settings">
-            <ActionIcon variant="default" size="md" onClick={openQueries}>
-              <Database size={20} />
-            </ActionIcon>
-          </Tooltip>
-          <Tooltip label="Interactions Viewer">
-            <ActionIcon variant="default" size="md" onClick={openInteractions}>
-              <Link size={20} />
-            </ActionIcon>
-          </Tooltip>
-        </Group>
       </Group>
-      <FilterSettingsModal opened={filtersOpened} close={closeFilters} />
-      <DataEditorModal opened={dataEditorOpened} close={closeQueries} />
-      <InteractionsViewerModal opened={interactionsOpened} close={closeInteractions} />
     </Group>
   );
 });
