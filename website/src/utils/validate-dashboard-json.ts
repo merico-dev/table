45a80f5d92af04a0bf2f5beb2070b7021e7403c9
Leto <gerile.tu@merico.dev>
2023-02-21 13:40:09 +0800

    fix(website): throw error when content is not validatable

--- website/src/utils/validate-dashboard-json.ts
+++ website/src/utils/validate-dashboard-json.ts
@@ -4,7 +4,10 @@ import { DashboardJSONTypeDef } from './dashboard-json-type-def';
 export function validateDashboardJSONContent(content: string) {
   try {
     const ajv = new Ajv();
-    ajv.validate(DashboardJSONTypeDef, content);
+    const valid = ajv.validate(DashboardJSONTypeDef, content);
+    if (!valid) {
+      throw new Error(ajv.errorsText(ajv.errors));
+    }
   } catch (error) {
     console.log(error);
     throw new Error('Invalid content of json file, check out console for details');
