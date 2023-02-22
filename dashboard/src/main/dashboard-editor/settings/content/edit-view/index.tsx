9a6953d735d33709477b67c1f42e7d7021c903c8
Leto <gerile.tu@merico.dev>
2023-02-22 17:56:16 +0800

    feat(dashboard): confirm before removing a view

--- dashboard/src/main/dashboard-editor/settings/content/edit-view/index.tsx
+++ dashboard/src/main/dashboard-editor/settings/content/edit-view/index.tsx
@@ -1,19 +1,31 @@
 import { Box, Button, Group, Stack, Text } from '@mantine/core';
+import { useModals } from '@mantine/modals';
 import { observer } from 'mobx-react-lite';
 import { Trash } from 'tabler-icons-react';
 import { useModelContext } from '~/contexts';
 import { EditViewForm } from '~/main/dashboard-editor/settings/content/edit-view/edit-view-form';
 
 export const EditView = observer(({ id }: { id: string }) => {
+  const modals = useModals();
   const model = useModelContext();
   const view = model.views.findByID(id);
   if (!view) {
     return <Text size={14}>View by ID[{id}] is not found</Text>;
   }
+
+  const removeWithConfirmation = () => {
+    modals.openConfirmModal({
+      title: 'Delete this view?',
+      labels: { confirm: 'Confirm', cancel: 'Cancel' },
+      onCancel: () => console.log('Cancel'),
+      onConfirm: () => model.views.removeByID(id),
+      zIndex: 320,
+    });
+  };
   return (
     <Stack sx={{ maxWidth: '600px', height: '100%' }} spacing="sm">
       <Group position="right" pt={10}>
-        <Button size="xs" color="red" leftIcon={<Trash size={16} />} onClick={() => model.views.removeByID(id)}>
+        <Button size="xs" color="red" leftIcon={<Trash size={16} />} onClick={removeWithConfirmation}>
           Delete this view
         </Button>
       </Group>
