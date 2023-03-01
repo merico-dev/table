72bd492927c54e499356756eaaa5e9db3502c45c
Leto <gerile.tu@merico.dev>
2023-03-01 13:12:02 +0800

    feat(api): add GET /website/list

---
+++ api/src/controller/website.controller.ts
@@ -0,0 +1,41 @@
+/**
+ * NOTE(Leto)
+ * This controller is a temporary solution
+ * Will consider storing site settings in db later
+ */
+import * as express from 'express';
+import { controller, httpGet, interfaces } from 'inversify-express-utils';
+import { ApiOperationGet, ApiPath, SwaggerDefinitionConstant } from 'swagger-express-ts';
+
+const WebsiteEnvs = {
+  WEBSITE_LOGO_URL_ZH: process.env.WEBSITE_LOGO_URL_ZH,
+  WEBSITE_LOGO_URL_EN: process.env.WEBSITE_LOGO_URL_EN,
+  WEBSITE_LOGO_JUMP_URL: process.env.WEBSITE_LOGO_JUMP_URL,
+  WEBSITE_FAVICON_URL: process.env.WEBSITE_FAVICON_URL,
+};
+
+@ApiPath({
+  path: '/website',
+  name: 'Website',
+})
+@controller('/website')
+export class WebsiteController implements interfaces.Controller {
+  public static TARGET_NAME = 'Website';
+
+  @ApiOperationGet({
+    path: '/list',
+    description: 'List website settings',
+    responses: {
+      200: { description: 'SUCCESS', type: SwaggerDefinitionConstant.Response.Type.OBJECT },
+      500: { description: 'SERVER ERROR', type: SwaggerDefinitionConstant.Response.Type.OBJECT, model: 'ApiError' },
+    },
+  })
+  @httpGet('/list')
+  public async list(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
+    try {
+      res.json(WebsiteEnvs);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
