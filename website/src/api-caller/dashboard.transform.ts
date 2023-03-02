314181f0df0513ab87379e461017c86f92af2c9f
Leto <gerile.tu@merico.dev>
2023-03-02 23:35:58 +0800

    fix(website): group is missing when running normalizeDBDashboard

--- website/src/api-caller/dashboard.transform.ts
+++ website/src/api-caller/dashboard.transform.ts
@@ -1,10 +1,11 @@
 import { IDashboard } from '@devtable/dashboard';
 import { IDBDashboard } from './dashboard.typed';
 
-export function normalizeDBDashboard({ id, name, content }: IDBDashboard): IDashboard {
+export function normalizeDBDashboard({ id, name, group, content }: IDBDashboard): IDashboard {
   return {
     id,
     name,
+    group,
     ...content,
   } as IDashboard;
 }
