f5924fff727c658796ca387b353abb081cf30281
Leto <gerile.tu@merico.dev>
2023-03-08 20:30:33 +0800

    feat(dashboard): include d3-array to function-utils

--- dashboard/src/utils/function-utils.ts
+++ dashboard/src/utils/function-utils.ts
@@ -2,9 +2,11 @@ import CryptoJS from 'crypto-js';
 import dayjs from 'dayjs';
 import lodash from 'lodash';
 import numbro from 'numbro';
+import * as d3Array from 'd3-array';
 
 export const functionUtils = {
   CryptoJS,
+  d3Array,
   dayjs,
   lodash,
   numbro,
