4ee3718d4a3e78a53044edd3f98e024ec8bdc1fb
Leto <gerile.tu@merico.dev>
2023-03-09 22:58:52 +0800

    feat(dashboard): add description to checkbox

--- dashboard/src/filter/filter-checkbox/render.tsx
+++ dashboard/src/filter/filter-checkbox/render.tsx
@@ -1,30 +1,69 @@
-import { Box, Checkbox, Text } from '@mantine/core';
+import { Box, Checkbox, Group, Text, Tooltip } from '@mantine/core';
+import { IconInfoCircle } from '@tabler/icons';
+import { observer } from 'mobx-react-lite';
+import { ReadonlyRichText } from '~/panel/settings/common/readonly-rich-text-editor';
 import { FilterModelInstance } from '../../model';
 import { IFilterConfig_Checkbox } from '../../model/filters/filter/checkbox';
 
+const CheckboxTooltip = ({ description, isDescriptionEmpty }: { description: string; isDescriptionEmpty: boolean }) => {
+  if (isDescriptionEmpty) {
+    return null;
+  }
+  return (
+    <Tooltip
+      color="white"
+      label={<ReadonlyRichText value={description} styles={{ root: { border: 'none' }, content: { padding: 0 } }} />}
+      styles={{
+        tooltip: {
+          border: '0.0625rem solid rgb(233, 236, 239)',
+          boxShadow:
+            'rgb(0 0 0 / 5%) 0px 0.0625rem 0.1875rem, rgb(0 0 0 / 5%) 0px 1.25rem 1.5625rem -0.3125rem, rgb(0 0 0 / 4%) 0px 0.625rem 0.625rem -0.3125rem',
+        },
+      }}
+      withArrow
+    >
+      <Box sx={{ height: '16px', alignSelf: 'center' }}>
+        <IconInfoCircle size={16} color="gray" />
+      </Box>
+    </Tooltip>
+  );
+};
+
 interface IFilterCheckbox extends Omit<FilterModelInstance, 'key' | 'type' | 'config'> {
   config: IFilterConfig_Checkbox;
-  value: $TSFixMe;
-  onChange: (v: $TSFixMe) => void;
+  value: boolean;
+  onChange: (v: boolean) => void;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function FilterCheckbox({ label, config: { default_value, ...rest }, value, onChange }: IFilterCheckbox) {
-  return (
-    <Box>
-      <Text>&nbsp;</Text>
-      <Checkbox
-        label={label}
-        checked={value || false}
-        onChange={(event) => onChange(event.currentTarget.checked)}
-        {...rest}
-        pt=".4em"
-        styles={{
-          input: {
-            borderColor: '#e9ecef',
-          },
-        }}
-      />
-    </Box>
-  );
-}
+export const FilterCheckbox = observer(
+  ({
+    label,
+    config: { default_value, description, isDescriptionEmpty, ...rest },
+    value,
+    onChange,
+  }: IFilterCheckbox) => {
+    return (
+      <Box>
+        <Text>&nbsp;</Text>
+        <Checkbox
+          label={
+            <Group noWrap position="apart" align="middle" spacing={6}>
+              <Box sx={{ flexGrow: 1 }}>{label}</Box>
+              <CheckboxTooltip description={description} isDescriptionEmpty={isDescriptionEmpty} />
+            </Group>
+          }
+          checked={value || false}
+          onChange={(event) => onChange(event.currentTarget.checked)}
+          {...rest}
+          pt=".4em"
+          styles={{
+            input: {
+              borderColor: '#e9ecef',
+            },
+          }}
+        />
+      </Box>
+    );
+  },
+);
