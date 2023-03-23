cab709abf25889eece0e8f4486ef67d120d84114
Leto <gerile.tu@merico.dev>
2023-03-23 14:43:15 +0800

    feat(dashboard): load & show tables by table_schema

--- dashboard/src/main/dashboard-editor/settings/content/table-structure-modal/table-structure.tsx
+++ dashboard/src/main/dashboard-editor/settings/content/table-structure-modal/table-structure.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, LoadingOverlay, Stack } from '@mantine/core';
+import { Box, Flex, LoadingOverlay, NavLink, Stack } from '@mantine/core';
+import { IconDatabase } from '@tabler/icons';
 import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
 import { DataSourceModelInstance } from '~/model/datasources/datasource';
@@ -14,7 +15,15 @@ export const TableStructure = observer(({ dataSource }: { dataSource: DataSource
     <Flex sx={{ height: '100%' }}>
       <Box w={300} sx={{ flexGrow: 0, flexShrink: 0, position: 'relative' }}>
         <LoadingOverlay visible={dataSource.loading} />
-        <Stack>Tables</Stack>
+        <Box h="100%" sx={{ overflow: 'auto' }}>
+          {Object.entries(dataSource.tables).map(([table_schema, table_infos]) => (
+            <NavLink key={table_schema} label={table_schema} icon={<IconDatabase size={14} />}>
+              {table_infos.map((info) => (
+                <NavLink key={info.table_name} label={info.table_name} />
+              ))}
+            </NavLink>
+          ))}
+        </Box>
       </Box>
       <Box sx={{ flexGrow: 1 }}>Columns</Box>
     </Flex>
--- dashboard/src/model/datasources/datasource.ts
+++ dashboard/src/model/datasources/datasource.ts
@@ -1,14 +1,22 @@
 import axios from 'axios';
 import _ from 'lodash';
-import { Instance, flow, toGenerator, types } from 'mobx-state-tree';
+import { flow, Instance, toGenerator, types } from 'mobx-state-tree';
 import { QueryFailureError } from '~/api-caller';
 import { APIClient } from '~/api-caller/request';
 import { TDataSourceConfig } from '~/api-caller/types';
-import { AnyObject } from '~/types';
 import { DataSourceType } from '../queries/types';
 
+export type TableInfoType = {
+  table_schema: string;
+  table_name: string;
+  table_type: string;
+  engine: string;
+};
+
+export type TableInfoTreeType = Record<string, TableInfoType[]>;
+
 const Queries = {
-  tables: `select * from information_schema.tables where table_schema = 'public' order by table_name`,
+  tables: `select table_schema, table_name, table_type, engine from information_schema.tables where table_type = 'BASE TABLE' order by table_name`,
 };
 
 export const DataSourceModel = types
@@ -21,7 +29,7 @@ export const DataSourceModel = types
       config: types.frozen<TDataSourceConfig>(),
     }),
     types.model({
-      tables: types.optional(types.frozen<AnyObject[]>(), []),
+      tables: types.optional(types.frozen<TableInfoTreeType>(), {}),
       state: types.optional(types.enumeration(['idle', 'loading', 'error']), 'idle'),
       error: types.frozen(),
     }),
@@ -40,18 +48,19 @@ export const DataSourceModel = types
       self.controller = new AbortController();
       self.state = 'loading';
       try {
-        self.tables = yield* toGenerator(
+        const tables = yield* toGenerator(
           APIClient.getRequest('POST', self.controller.signal)(
             '/query',
             { type: self.type, key: self.key, query: Queries.tables },
             {},
           ),
         );
+        self.tables = _.groupBy(tables, 'table_schema');
         self.state = 'idle';
         self.error = null;
       } catch (error) {
         if (!axios.isCancel(error)) {
-          self.tables = [];
+          self.tables = {};
           const fallback = _.get(error, 'message', 'unkown error');
           self.error = _.get(error, 'response.data.detail.message', fallback) as QueryFailureError;
           self.state = 'error';
@@ -62,7 +71,7 @@ export const DataSourceModel = types
     return {
       loadTables,
       loadTablesIfEmpty() {
-        if (self.tables.length === 0) {
+        if (Object.keys(self.tables).length === 0) {
           loadTables();
         }
       },
