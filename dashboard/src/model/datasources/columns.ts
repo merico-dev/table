61a3644c3856e88ff8d2fe54a8986b0f79003fb3
Leto <gerile.tu@merico.dev>
2023-03-23 15:32:01 +0800

    feat(dashboard): add ColumnsModel & load columns by table_schema&table_name

---
+++ dashboard/src/model/datasources/columns.ts
@@ -0,0 +1,38 @@
+import { types } from 'mobx-state-tree';
+
+export type ColumnInfoType = {
+  column_name: string;
+  table_name: string;
+  table_type: string;
+  engine: string;
+};
+
+export const ColumnsModel = types
+  .model({
+    table_schema: types.optional(types.string, ''),
+    table_name: types.optional(types.string, ''),
+    data: types.optional(types.frozen<ColumnInfoType[]>(), []),
+    state: types.optional(types.enumeration(['idle', 'loading', 'error']), 'idle'),
+    error: types.frozen(),
+  })
+  .views((self) => ({
+    get loading() {
+      return self.state === 'loading';
+    },
+    get empty() {
+      return self.data.length === 0;
+    },
+    get sql() {
+      return `
+        SELECT column_name, column_type, is_nullable, column_default, column_comment, ordinal_position
+        FROM information_schema.columns
+        WHERE table_name = '${self.table_name}' AND table_schema = '${self.table_schema}'
+      `;
+    },
+  }))
+  .actions((self) => ({
+    setKeywords(table_schema: string, table_name: string) {
+      self.table_schema = table_schema;
+      self.table_name = table_name;
+    },
+  }));
--- dashboard/src/model/datasources/datasource.ts
+++ dashboard/src/model/datasources/datasource.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import _ from 'lodash';
-import { flow, Instance, toGenerator, types } from 'mobx-state-tree';
+import { reaction } from 'mobx';
+import { addDisposer, flow, Instance, toGenerator, types } from 'mobx-state-tree';
 import { QueryFailureError } from '~/api-caller';
 import { APIClient } from '~/api-caller/request';
 import { TDataSourceConfig } from '~/api-caller/types';
 import { DataSourceType } from '../queries/types';
+import { ColumnsModel } from './columns';
 import { TablesModel } from './tables';
 
 export const DataSourceModel = types
@@ -14,9 +16,20 @@ export const DataSourceModel = types
     key: types.string,
     config: types.frozen<TDataSourceConfig>(),
     tables: types.optional(TablesModel, {}),
+    columns: types.optional(ColumnsModel, {}),
   })
   .volatile(() => ({
     controller: new AbortController(),
+  }))
+  .actions((self) => ({
+    initColumnKeywords() {
+      if (self.tables.empty) {
+        return;
+      }
+      const table_schema = Object.keys(self.tables.data)[0];
+      const table_name = self.tables.data[table_schema][0].table_name;
+      self.columns.setKeywords(table_schema, table_name);
+    },
   }))
   .actions((self) => {
     const loadTables = flow(function* () {
@@ -34,6 +47,8 @@ export const DataSourceModel = types
         self.tables.data = _.groupBy(tables, 'table_schema');
         self.tables.state = 'idle';
         self.tables.error = null;
+
+        self.initColumnKeywords();
       } catch (error) {
         if (!axios.isCancel(error)) {
           self.tables.data = {};
@@ -50,6 +65,42 @@ export const DataSourceModel = types
         if (self.tables.empty) {
           loadTables();
         }
+      },
+      loadColumns: flow(function* () {
+        self.controller?.abort();
+        self.controller = new AbortController();
+        self.columns.state = 'loading';
+        try {
+          self.columns.data = yield* toGenerator(
+            APIClient.getRequest('POST', self.controller.signal)(
+              '/query',
+              { type: self.type, key: self.key, query: self.columns.sql },
+              {},
+            ),
+          );
+          self.columns.state = 'idle';
+          self.columns.error = null;
+        } catch (error) {
+          if (!axios.isCancel(error)) {
+            self.tables.data = {};
+            const fallback = _.get(error, 'message', 'unkown error');
+            self.columns.error = _.get(error, 'response.data.detail.message', fallback) as QueryFailureError;
+            self.columns.state = 'error';
+          }
+        }
+      }),
+    };
+  })
+  .actions((self) => {
+    return {
+      afterCreate() {
+        addDisposer(
+          self,
+          reaction(() => self.columns.sql, self.loadColumns, {
+            fireImmediately: false,
+            delay: 500,
+          }),
+        );
       },
     };
   });
