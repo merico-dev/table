3d1713063bd784327fd03fd68dde56b80d8c8283
Leto <gerile.tu@merico.dev>
2023-03-23 13:21:32 +0800

    feat(dashboard): add modal title of TableStructureModal

--- dashboard/src/main/dashboard-editor/settings/content/table-structure-modal/index.tsx
+++ dashboard/src/main/dashboard-editor/settings/content/table-structure-modal/index.tsx
@@ -1,13 +1,24 @@
-import { Button, Modal } from '@mantine/core';
+import { Badge, Button, Group, Modal, Text } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 import { useState } from 'react';
 import { DataSourceModelInstance } from '~/model/datasources/datasource';
 
 export const TableStructureModal = observer(({ dataSource }: { dataSource: DataSourceModelInstance }) => {
   const [opened, setOpened] = useState(false);
+
   return (
     <>
-      <Modal opened={opened} onClose={() => setOpened(false)} title="Table Structure" zIndex={320} size="800px">
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title={
+          <Group spacing={6}>
+            Table Structure of Data Source <Text fw={700}>{dataSource.key}</Text> <Badge>{dataSource.type}</Badge>
+          </Group>
+        }
+        zIndex={320}
+        size="96vw"
+      >
         TODO
       </Modal>
 
