93a0631199a60d54f70eafcacc554ee960d9db56
Leto <gerile.tu@merico.dev>
2023-03-24 14:18:51 +0800

    feat(dashboard): use ErrorBoundary in table-structure

--- dashboard/src/main/dashboard-editor/settings/content/table-structure-modal/table-structure/index.tsx
+++ dashboard/src/main/dashboard-editor/settings/content/table-structure-modal/table-structure/index.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Stack } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
 import { DataSourceModelInstance } from '~/model/datasources/datasource';
+import { ErrorBoundary } from '~/utils/error-boundary';
 import { ColumnsTable } from './columns-table';
 import { IndexesTable } from './indexes-table';
 import { TableNavLinks } from './table-nav-links';
@@ -14,11 +15,17 @@ export const TableStructure = observer(({ dataSource }: { dataSource: DataSource
   return (
     <Flex sx={{ height: '100%' }}>
       <Box w={300} sx={{ flexGrow: 0, flexShrink: 0, position: 'relative' }}>
-        <TableNavLinks dataSource={dataSource} />
+        <ErrorBoundary>
+          <TableNavLinks dataSource={dataSource} />
+        </ErrorBoundary>
       </Box>
       <Stack spacing={40} sx={{ flexGrow: 1, overflow: 'auto', position: 'relative', borderLeft: '1px solid #efefef' }}>
-        <ColumnsTable dataSource={dataSource} />
-        <IndexesTable dataSource={dataSource} />
+        <ErrorBoundary>
+          <ColumnsTable dataSource={dataSource} />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <IndexesTable dataSource={dataSource} />
+        </ErrorBoundary>
       </Stack>
     </Flex>
   );
