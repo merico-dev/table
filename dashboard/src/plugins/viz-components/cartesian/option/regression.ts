05d83a130be340221555c50926cb5f7097a43201
Leto <gerile.tu@merico.dev>
2023-03-31 13:38:01 +0800

    refactor(dashboard): extract getOneRegressionConf

--- dashboard/src/plugins/viz-components/cartesian/option/regression.ts
+++ dashboard/src/plugins/viz-components/cartesian/option/regression.ts
@@ -1,4 +1,4 @@
-import { ICartesianChartConf, IRegressionLineConf, IRegressionTransform } from '../type';
+import { ICartesianChartConf, IRegressionConf, IRegressionLineConf, IRegressionTransform } from '../type';
 
 interface IRegressionDataSetItem {
   id: string;
@@ -20,6 +20,47 @@ interface IRegressionXAxisItem {
   show: false;
 }
 
+function getOneRegressionConf(reg: IRegressionConf, data: TVizData) {
+  const { transform, plot, name, y_axis_data_key } = reg;
+  const xAxisId = `x-axis-for-${name}`;
+  const rawDatasetId = `dataset-for-${name}--raw`;
+  const regDatasetId = `dataset-for-${name}--transformed`;
+
+  const datasets = [
+    {
+      id: rawDatasetId,
+      source: data.map((d, i) => [i, Number(d[y_axis_data_key])]),
+    },
+    {
+      transform,
+      id: regDatasetId,
+      fromDatasetId: rawDatasetId,
+    },
+  ];
+  const series = {
+    ...plot,
+    name,
+    datasetId: regDatasetId,
+    xAxisId,
+    showSymbol: false,
+    tooltip: {
+      show: false,
+    },
+  };
+  const xaxis: IRegressionXAxisItem = {
+    type: 'category',
+    id: xAxisId,
+    datasetId: regDatasetId,
+    show: false,
+  };
+
+  return {
+    datasets,
+    series: [series],
+    xaxes: [xaxis],
+  };
+}
+
 export function getRegressionConfs({ regressions = [] }: ICartesianChartConf, data: TVizData) {
   const regressionDataSets: IRegressionDataSetItem[] = [];
   const regressionSeries: IRegressionSeriesItem[] = [];
@@ -27,36 +68,12 @@ export function getRegressionConfs({ regressions = [] }: ICartesianChartConf, da
   if (data.length === 0) {
     return { regressionDataSets, regressionSeries, regressionXAxes };
   }
-  regressions.forEach(({ transform, plot, name, y_axis_data_key }) => {
-    const xAxisId = `x-axis-for-${name}`;
-    const rawDatasetId = `dataset-for-${name}--raw`;
-    const regDatasetId = `dataset-for-${name}--transformed`;
 
-    regressionDataSets.push({
-      id: rawDatasetId,
-      source: data.map((d, i) => [i, Number(d[y_axis_data_key])]),
-    });
-    regressionDataSets.push({
-      transform,
-      id: regDatasetId,
-      fromDatasetId: rawDatasetId,
-    });
-    regressionSeries.push({
-      ...plot,
-      name,
-      datasetId: regDatasetId,
-      xAxisId,
-      showSymbol: false,
-      tooltip: {
-        show: false,
-      },
-    });
-    regressionXAxes.push({
-      type: 'category',
-      id: xAxisId,
-      datasetId: regDatasetId,
-      show: false,
-    });
+  regressions.forEach((reg) => {
+    const { datasets, series, xaxes } = getOneRegressionConf(reg, data);
+    regressionDataSets.push(...datasets);
+    regressionSeries.push(...series);
+    regressionXAxes.push(...xaxes);
   });
 
   return { regressionDataSets, regressionSeries, regressionXAxes };
