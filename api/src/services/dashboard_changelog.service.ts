562619174d1ca234d4e48389bb42d419be1c3c25
Miguel Lansdorf <miguel.lansdorf@merico.dev>
2023-04-19 11:45:48 +0800

    optimized code structure for changelog diffs

--- api/src/services/dashboard_changelog.service.ts
+++ api/src/services/dashboard_changelog.service.ts
@@ -1,47 +1,19 @@
 import { dashboardDataSource } from '../data_sources/dashboard';
 import Dashboard from '../models/dashboard';
 import DashboardChangelog from '../models/dashboard_changelog';
-import fs from 'fs-extra';
-import path from 'path';
-import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
 import {
   DashboardChangelogFilterObject,
   DashboardChangelogPaginationResponse,
   DashboardChangelogSortObject,
 } from '../api_models/dashboard_changelog';
 import { PaginationRequest } from '../api_models/base';
-import logger from 'npmlog';
-import { omit } from 'lodash';
+import { getDiff, omitTime } from '../utils/helpers';
 
 export class DashboardChangelogService {
   static async createChangelog(oldDashboard: Dashboard, newDashboard: Dashboard): Promise<string | undefined> {
-    const oldData = omit(oldDashboard, ['create_time', 'update_time']);
-    const newData = omit(newDashboard, ['create_time', 'update_time']);
-    const time = new Date().getTime();
-    const dir = path.join(__dirname, `${time}_${oldData.id}`);
-    await fs.ensureDir(dir);
-    let diff: string | undefined;
-    try {
-      const options: Partial<SimpleGitOptions> = {
-        baseDir: dir,
-        binary: 'git',
-      };
-      const git: SimpleGit = simpleGit(options);
-      await git.init();
-      await git.addConfig('user.name', 'Devtable');
-      await git.addConfig('user.email', 'Devtable@merico.dev');
-      const filename = path.join(dir, 'data.json');
-      await fs.writeJson(filename, oldData, { spaces: '\t' });
-      await git.add(filename);
-      await git.commit('First');
-      await fs.writeJson(filename, newData, { spaces: '\t' });
-      diff = await git.diff();
-    } catch (e) {
-      logger.warn('create dashboard changelog failed');
-      logger.warn(e);
-    }
-    await fs.rm(dir, { recursive: true, force: true });
-    return diff;
+    const oldData = omitTime(oldDashboard);
+    const newData = omitTime(newDashboard);
+    return await getDiff(oldData, newData);
   }
 
   async list(
--- api/src/services/dashboard_content_changelog.service.ts
+++ api/src/services/dashboard_content_changelog.service.ts
@@ -1,50 +1,22 @@
 import { dashboardDataSource } from '../data_sources/dashboard';
 import DashboardContent from '../models/dashboard_content';
 import DashboardContentChangelog from '../models/dashboard_content_changelog';
-import fs from 'fs-extra';
-import path from 'path';
-import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
 import {
   DashboardContentChangelogFilterObject,
   DashboardContentChangelogPaginationResponse,
   DashboardContentChangelogSortObject,
 } from '../api_models/dashboard_content_changelog';
 import { PaginationRequest } from '../api_models/base';
-import logger from 'npmlog';
-import { omit } from 'lodash';
+import { getDiff, omitTime } from '../utils/helpers';
 
 export class DashboardContentChangelogService {
   static async createChangelog(
     oldDashboardContent: DashboardContent,
     newDashboardContent: DashboardContent,
   ): Promise<string | undefined> {
-    const oldData = omit(oldDashboardContent, ['create_time', 'update_time']);
-    const newData = omit(newDashboardContent, ['create_time', 'update_time']);
-    const time = new Date().getTime();
-    const dir = path.join(__dirname, `${time}_${oldData.id}`);
-    await fs.ensureDir(dir);
-    let diff: string | undefined;
-    try {
-      const options: Partial<SimpleGitOptions> = {
-        baseDir: dir,
-        binary: 'git',
-      };
-      const git: SimpleGit = simpleGit(options);
-      await git.init();
-      await git.addConfig('user.name', 'Devtable');
-      await git.addConfig('user.email', 'Devtable@merico.dev');
-      const filename = path.join(dir, 'data.json');
-      await fs.writeJson(filename, oldData, { spaces: '\t' });
-      await git.add(filename);
-      await git.commit('First');
-      await fs.writeJson(filename, newData, { spaces: '\t' });
-      diff = await git.diff();
-    } catch (e) {
-      logger.warn('create dashboard content changelog failed');
-      logger.warn(e);
-    }
-    await fs.rm(dir, { recursive: true, force: true });
-    return diff;
+    const oldData = omitTime(oldDashboardContent);
+    const newData = omitTime(newDashboardContent);
+    return await getDiff(oldData, newData);
   }
 
   async list(
