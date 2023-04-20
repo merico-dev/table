55ce654dae2778bdba65c6980d54b6d82144eb94
Leto <gerile.tu@merico.dev>
2023-04-20 14:55:51 +0800

    fix(website): website should load local monaco

--- website/src/utils/config.ts
+++ website/src/utils/config.ts
@@ -1,14 +1,16 @@
 import { IDashboardConfig } from '@devtable/dashboard';
 import { ISettingsFormConfig } from '@devtable/settings-form';
 
+export const MonacoPath = import.meta.env.VITE_WEBSITE_BASE_URL + '/assets/monaco-editor/min/vs';
+
 export const DashboardConfig: IDashboardConfig = {
   apiBaseURL: import.meta.env.VITE_API_BASE_URL,
   basename: import.meta.env.VITE_WEBSITE_BASE_URL,
-  monacoPath: import.meta.env.VITE_WEBSITE_BASE_URL + '/assets/monaco-editor/min/vs',
+  monacoPath: MonacoPath,
 };
 
 export const SettingsFormConfig: ISettingsFormConfig = {
   apiBaseURL: import.meta.env.VITE_API_BASE_URL,
   basename: import.meta.env.VITE_WEBSITE_BASE_URL,
-  monacoPath: import.meta.env.VITE_WEBSITE_BASE_URL + '/assets/monaco-editor/min/vs',
+  monacoPath: MonacoPath,
 };
