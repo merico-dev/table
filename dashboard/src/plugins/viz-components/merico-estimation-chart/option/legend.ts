f6f450ecf33084e94da0c3691deb1a2e5b251fce
Leto <gerile.tu@merico.dev>
2023-05-05 00:46:42 +0800

    feat(dashboard): add tooltip to legend of estimation-chart

--- dashboard/src/plugins/viz-components/merico-estimation-chart/option/legend.ts
+++ dashboard/src/plugins/viz-components/merico-estimation-chart/option/legend.ts
@@ -1,16 +1,22 @@
 import { AnyObject } from '~/types';
 
 export function getLegend(series: AnyObject[]) {
+  const data = series
+    .filter((s) => s.show_in_legend)
+    .reverse()
+    .map((s) => ({
+      name: `${s.name}`,
+      color: s.color,
+    }));
   return {
     show: true,
     orient: 'vertical',
     top: 'middle',
     right: 0,
-    data: series
-      .filter((s) => s.show_in_legend)
-      .reverse()
-      .map((s) => ({
-        name: `${s.name}`,
-      })),
+    data,
+    tooltip: {
+      show: true,
+      formatter: '档位偏差：{a}',
+    },
   };
 }
