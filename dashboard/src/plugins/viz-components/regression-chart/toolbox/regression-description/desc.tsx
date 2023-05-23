35b3adc85c563df15991e2c3a6dc896aad3bd594
Leto <gerile.tu@merico.dev>
2023-05-23 14:46:24 +0800

    refactor(dashboard): misc

--- dashboard/src/plugins/viz-components/regression-chart/toolbox/regression-description/desc.tsx
+++ dashboard/src/plugins/viz-components/regression-chart/toolbox/regression-description/desc.tsx
@@ -15,6 +15,99 @@ function calculateAdjustedRSquared(r: number, n: number, k: number) {
   return 1 - ((1 - r) * (n - 1)) / (n - k - 1);
 }
 
+function getLinearDescription(rawData: TVizData, basisData: [number, number][], conf: IRegressionChartConf) {
+  const { x_axis, y_axis } = conf;
+  const result = d3Regression.regressionLinear()(basisData);
+  const { a, b, rSquared } = result;
+  return {
+    expression: (
+      <Group position="center" noWrap spacing={10}>
+        <Text>{y_axis.name}</Text>
+        <Text>=</Text>
+        <Text weight="bold" color="red">
+          {numbro(b).format({ mantissa: 2, trimMantissa: true })}
+        </Text>
+        <Text>+</Text>
+        <Text weight="bold" color="red">
+          {numbro(a).format({ mantissa: 2, trimMantissa: true })}
+        </Text>
+        <Text>×</Text>
+        <Text>{x_axis.name}</Text>
+      </Group>
+    ),
+    rSquared,
+    adjustedRSquared: calculateAdjustedRSquared(rSquared, rawData.length, 1),
+  };
+}
+
+function getExponentialDescription(rawData: TVizData, basisData: [number, number][], conf: IRegressionChartConf) {
+  const { x_axis, y_axis } = conf;
+  const { a, b, rSquared } = d3Regression.regressionExp()(basisData);
+  return {
+    expression: (
+      <Group position="center" noWrap spacing={10}>
+        <Text>{y_axis.name}</Text>
+        <Text>=</Text>
+        <Text weight="bold" color="gray">
+          {a}
+        </Text>
+        <Text>×</Text>
+        <Group position="left" noWrap spacing={2}>
+          <Text>Math.exp(</Text>
+          <Text weight="bold" color="gray">
+            {b}
+          </Text>
+          <Text>×</Text>
+          <Text>{x_axis.name}</Text>
+          <Text>)</Text>
+        </Group>
+      </Group>
+    ),
+    rSquared,
+    adjustedRSquared: calculateAdjustedRSquared(rSquared, rawData.length, 1),
+  };
+}
+
+function getLogarithmicDescription(rawData: TVizData, basisData: [number, number][], conf: IRegressionChartConf) {
+  const { x_axis, y_axis } = conf;
+  const { a, b, rSquared } = d3Regression.regressionLog()(basisData);
+  return {
+    expression: (
+      <Group position="center" noWrap spacing={10}>
+        <Text>{y_axis.name}</Text>
+        <Text>=</Text>
+        <Text weight="bold" color="gray">
+          {a}
+        </Text>
+        <Text>×</Text>
+        <Group position="left" noWrap spacing={2}>
+          <Text>Math.log(</Text>
+          <Text>{x_axis.name}</Text>
+          <Text>)</Text>
+          <Text>+</Text>
+          <Text weight="bold" color="gray">
+            {b}
+          </Text>
+        </Group>
+      </Group>
+    ),
+    rSquared,
+    adjustedRSquared: calculateAdjustedRSquared(rSquared, rawData.length, 1),
+  };
+}
+
+function getPolynomialDescription(rawData: TVizData, basisData: [number, number][], conf: IRegressionChartConf) {
+  const { x_axis, y_axis, regression } = conf;
+  const result = d3Regression.regressionPoly().order(regression.transform.config.order)(basisData);
+  const { rSquared } = result;
+  console.log(result);
+  return {
+    expression: '',
+    rSquared,
+    adjustedRSquared: calculateAdjustedRSquared(rSquared, rawData.length, 1),
+  };
+}
+
 export function getRegressionDescription(data: TVizData, conf?: IRegressionChartConf) {
   if (!conf) {
     return {
@@ -24,96 +117,24 @@ export function getRegressionDescription(data: TVizData, conf?: IRegressionChart
     };
   }
   const { regression, x_axis, y_axis } = conf;
-  const dataSource = data.map((d) => [d[x_axis.data_key], d[regression.y_axis_data_key]]);
+  const dataSource: [number, number][] = data.map((d) => [d[x_axis.data_key], d[regression.y_axis_data_key]]);
 
   if (regression.transform.config.method === 'linear') {
-    const result = d3Regression.regressionLinear()(dataSource);
-    const { a, b, rSquared } = result;
-    return {
-      expression: (
-        <Group position="center" noWrap spacing={10}>
-          <Text>{y_axis.name}</Text>
-          <Text>=</Text>
-          <Text weight="bold" color="red">
-            {numbro(b).format({ mantissa: 2, trimMantissa: true })}
-          </Text>
-          <Text>+</Text>
-          <Text weight="bold" color="red">
-            {numbro(a).format({ mantissa: 2, trimMantissa: true })}
-          </Text>
-          <Text>×</Text>
-          <Text>{x_axis.name}</Text>
-        </Group>
-      ),
-      rSquared,
-      adjustedRSquared: calculateAdjustedRSquared(rSquared, data.length, 1),
-    };
+    return getLinearDescription(data, dataSource, conf);
   }
   if (regression.transform.config.method === 'exponential') {
-    const { a, b, rSquared } = d3Regression.regressionExp()(dataSource);
-    return {
-      expression: (
-        <Group position="center" noWrap spacing={10}>
-          <Text>{y_axis.name}</Text>
-          <Text>=</Text>
-          <Text weight="bold" color="gray">
-            {a}
-          </Text>
-          <Text>×</Text>
-          <Group position="left" noWrap spacing={2}>
-            <Text>Math.exp(</Text>
-            <Text weight="bold" color="gray">
-              {b}
-            </Text>
-            <Text>×</Text>
-            <Text>{x_axis.name}</Text>
-            <Text>)</Text>
-          </Group>
-        </Group>
-      ),
-      rSquared,
-      adjustedRSquared: calculateAdjustedRSquared(rSquared, data.length, 1),
-    };
+    return getExponentialDescription(data, dataSource, conf);
   }
 
   if (regression.transform.config.method === 'logarithmic') {
-    const { a, b, rSquared } = d3Regression.regressionLog()(dataSource);
-    return {
-      expression: (
-        <Group position="center" noWrap spacing={10}>
-          <Text>{y_axis.name}</Text>
-          <Text>=</Text>
-          <Text weight="bold" color="gray">
-            {a}
-          </Text>
-          <Text>×</Text>
-          <Group position="left" noWrap spacing={2}>
-            <Text>Math.log(</Text>
-            <Text>{x_axis.name}</Text>
-            <Text>)</Text>
-            <Text>+</Text>
-            <Text weight="bold" color="gray">
-              {b}
-            </Text>
-          </Group>
-        </Group>
-      ),
-      rSquared,
-      adjustedRSquared: calculateAdjustedRSquared(rSquared, data.length, 1),
-    };
+    return getLogarithmicDescription(data, dataSource, conf);
   }
   if (regression.transform.config.method === 'polynomial') {
-    const result = d3Regression.regressionPoly().order(regression.transform.config.order)(dataSource);
-    const { rSquared } = result;
-    console.log(result);
-    return {
-      expression: '',
-      rSquared,
-      adjustedRSquared: calculateAdjustedRSquared(rSquared, data.length, 1),
-    };
+    return getPolynomialDescription(data, dataSource, conf);
   }
   return {
     expression: '',
     rSquared: 0,
+    adjustedRSquared: 0,
   };
 }
