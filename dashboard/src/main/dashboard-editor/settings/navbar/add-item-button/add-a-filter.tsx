cc233d157dde993ced08ef52553d24f6c247b1d4
Leto <gerile.tu@merico.dev>
2023-05-29 15:06:48 +0800

    feat(dashboard): after adding a filter, select it

--- dashboard/src/main/dashboard-editor/settings/navbar/add-item-button/add-a-filter.tsx
+++ dashboard/src/main/dashboard-editor/settings/navbar/add-item-button/add-a-filter.tsx
@@ -1,26 +1,27 @@
 import { Button } from '@mantine/core';
 import { IconPlus } from '@tabler/icons';
 import { observer } from 'mobx-react-lite';
-import { useContentModelContext } from '~/contexts';
+import { useModelContext } from '~/contexts';
 import { FilterModelInstance } from '~/model';
 import { createFilterConfig_TextInput } from '~/model/filters/filter/text-input';
 import { DashboardFilterType } from '~/types';
 
 export const AddAFilter = observer(() => {
-  const model = useContentModelContext();
+  const model = useModelContext();
   const addFilter = () => {
     const id = new Date().getTime().toString();
     const filter = {
       id,
       key: id,
       label: id,
-      order: model.filters.current.length + 1,
+      order: model.content.filters.current.length + 1,
       type: DashboardFilterType.TextInput,
       config: createFilterConfig_TextInput(),
       visibleInViewsIDs: ['Main'],
       auto_submit: false,
     } as FilterModelInstance;
-    model.filters.append(filter);
+    model.content.filters.append(filter);
+    model.editor.setPath(['_FILTERS_', id]);
   };
 
   return (
