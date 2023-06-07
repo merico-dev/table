139881e780152d649ab810e1090ec9dd240cd9b9
Leto <gerile.tu@merico.dev>
2023-06-07 12:08:29 +0800

    feat(website): add sql snippet list page

--- website/src/frames/admin/navbar/admin-page-links.tsx
+++ website/src/frames/admin/navbar/admin-page-links.tsx
@@ -1,5 +1,5 @@
 import { Box, Group, Text, UnstyledButton } from '@mantine/core';
-import { IconDatabase, IconInfoCircle, IconKey, IconUsers } from '@tabler/icons';
+import { IconClipboardCheck, IconClipboardCopy, IconDatabase, IconInfoCircle, IconKey, IconUsers } from '@tabler/icons';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 interface IAdminPageLink {
@@ -44,6 +44,7 @@ type LinkType = {
 
 const links: LinkType[] = [
   { name: 'Data Sources', to: '/admin/data_source/list', icon: <IconDatabase size={16} color="#868e96" /> },
+  { name: 'SQL Snippets', to: '/admin/sql_snippet/list', icon: <IconClipboardCopy size={16} color="#868e96" /> },
   { name: 'Accounts', to: '/admin/account/list', icon: <IconUsers size={16} color="#868e96" /> },
   { name: 'API Keys', to: '/admin/api_key/list', icon: <IconKey size={16} color="#868e96" /> },
   { name: 'Status', to: '/admin/status', icon: <IconInfoCircle size={16} color="#868e96" /> },
