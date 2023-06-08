569424e24997db475717caba40559e50b081c6d4
Leto <gerile.tu@merico.dev>
2023-06-08 13:29:18 +0800

    feat(dashboard): spare room in QueryVariablesGuide fro global sql snippets

--- dashboard/src/main/dashboard-editor/settings/content/view-query-vars/query-variables-guide.tsx
+++ dashboard/src/main/dashboard-editor/settings/content/view-query-vars/query-variables-guide.tsx
@@ -1,5 +1,15 @@
-import { Stack, Sx, Text } from '@mantine/core';
+import { Alert, Stack, Sx, Tabs, Text } from '@mantine/core';
 import { Prism } from '@mantine/prism';
+import {
+  IconAlertCircle,
+  IconInfoCircle,
+  IconInfoSquare,
+  IconMessageCircle,
+  IconPhoto,
+  IconSettings,
+  IconVariable,
+  IconVariablePlus,
+} from '@tabler/icons';
 import { observer } from 'mobx-react-lite';
 import { useContentModelContext, useModelContext } from '~/contexts';
 
@@ -14,12 +24,13 @@ SELECT *
 FROM commit
 WHERE
   -- context
-  user_id IS '\$\{context.currentUserID\}'
+  repo_id IS '\$\{context.root_repo_id\}'
   -- filters
   author_time BETWEEN '\$\{filters.timeRange?.[0].toISOString()\}' AND '\$\{filters.timeRange?.[1].toISOString()\}'
   -- SQL snippets
   AND \$\{sql_snippets.author_email_condition\}
-  \$\{sql_snippets.order_by_clause\}
+  -- Global SQL snippets (shared between dashboards)
+  AND \$\{global_sql_snippets.account_condition\}
 `;
 export const QueryVariablesGuide = observer(function _QueryVariablesGuide({
   showSQLSnippets = true,
@@ -52,20 +63,42 @@ export const QueryVariablesGuide = observer(function _QueryVariablesGuide({
         ...sx,
       }}
     >
-      <Stack sx={{ width: '100%' }}>
-        <Text weight={500} sx={{ flexGrow: 0 }}>
-          Guide
-        </Text>
-        <Prism language="sql" sx={{ width: '100%' }} noCopy colorScheme="dark">
-          {example}
-        </Prism>
-        <Text weight={500} sx={{ flexGrow: 0 }}>
-          Current Values
-        </Text>
-        <Prism language="json" sx={{ width: '100%' }} noCopy colorScheme="dark">
-          {variablesString}
-        </Prism>
-      </Stack>
+      <Tabs defaultValue="guide" keepMounted={false}>
+        <Tabs.List grow>
+          <Tabs.Tab value="guide" icon={<IconAlertCircle size={14} />}>
+            Guide
+          </Tabs.Tab>
+          <Tabs.Tab value="local_query_vars" icon={<IconVariable size={14} />}>
+            Variables in this dashboard
+          </Tabs.Tab>
+          <Tabs.Tab value="global_sql_snippets" icon={<IconVariablePlus size={14} />}>
+            Global SQL Snippets
+          </Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="guide" pt="xs">
+          <Prism language="sql" sx={{ width: '100%' }} noCopy colorScheme="dark">
+            {example}
+          </Prism>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="local_query_vars" pt="xs">
+          <Prism language="json" sx={{ width: '100%' }} noCopy colorScheme="dark">
+            {variablesString}
+          </Prism>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="global_sql_snippets" pt="xs">
+          <Stack spacing={10}>
+            <Alert icon={<IconAlertCircle size={16} />} title="Global SQL Snippets">
+              SQL snippets worth sharing between dashboards are managed in System Settings by admins.
+            </Alert>
+            <Prism language="json" sx={{ width: '100%' }} noCopy colorScheme="dark">
+              {'{}'}
+            </Prism>
+          </Stack>
+        </Tabs.Panel>
+      </Tabs>
     </Stack>
   );
 });
