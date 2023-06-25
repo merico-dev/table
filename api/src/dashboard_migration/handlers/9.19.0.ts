dc684a668d4d7450e3da910af120f20e633d0bdb
Leto <gerile.tu@merico.dev>
2023-06-25 16:26:22 +0800

    feat(api): migrate panel.variables

--- api/src/dashboard_migration/handlers/9.19.0.ts
+++ api/src/dashboard_migration/handlers/9.19.0.ts
@@ -1,9 +1,23 @@
+function upgradeVariables(vars: any[], queryIDs: string[]) {
+  if (queryIDs.length === 0) {
+    return vars;
+  }
+  const queryID = queryIDs[0];
+
+  return vars.map((v) => ({
+    ...v,
+    data_field: `${queryID}.${v.data_field}`,
+  }));
+}
+
 function upgradePanels(panels: Record<string, any>[]) {
   return panels.map((p) => {
-    const { queryID, queryIDs, ...rest } = p;
+    const { queryID, queryIDs, variables, ...rest } = p;
+    const finalQueryIDs = queryIDs ?? [queryID];
     return {
       ...rest,
-      queryIDs: queryIDs ?? [queryID],
+      queryIDs,
+      variables: upgradeVariables(variables, finalQueryIDs),
     };
   });
 }
