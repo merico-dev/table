1e904743e52e986c11c04587d6785889bb9d4d50
Leto <gerile.tu@merico.dev>
2023-06-30 13:27:00 +0800

    fix(dashboard): can’t change a query’s name smoothly

--- dashboard/src/main/dashboard-editor/settings/content/edit-query/query-editor-form/configurations.tsx
+++ dashboard/src/main/dashboard-editor/settings/content/edit-query/query-editor-form/configurations.tsx
@@ -1,14 +1,20 @@
-import { Center, Divider, MultiSelect, Stack, TextInput } from '@mantine/core';
+import { ActionIcon, Center, Divider, MultiSelect, Stack, TextInput } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 import { QueryModelInstance } from '~/model';
 import { SelectDataSource } from './select-data-source';
 import { DeleteQuery } from './delete-query';
+import { useEffect, useState } from 'react';
+import { IconDeviceFloppy } from '@tabler/icons';
 
 interface IQueryConfigurations {
   queryModel: QueryModelInstance;
 }
 
 export const QueryConfigurations = observer(({ queryModel }: IQueryConfigurations) => {
+  const [name, setName] = useState(queryModel.name);
+  useEffect(() => {
+    setName(queryModel.name);
+  }, [queryModel.name]);
   return (
     <Center ml={20} mt={20} sx={{ maxWidth: '600px' }}>
       <Stack spacing={10} sx={{ width: '100%' }}>
@@ -18,9 +24,23 @@ export const QueryConfigurations = observer(({ queryModel }: IQueryConfiguration
           label="Name"
           required
           sx={{ flex: 1 }}
-          value={queryModel.name}
+          value={name}
           onChange={(e) => {
-            queryModel.setName(e.currentTarget.value);
+            setName(e.currentTarget.value);
+          }}
+          rightSection={
+            <ActionIcon
+              variant="filled"
+              color="blue"
+              size="sm"
+              onClick={() => queryModel.setName(name)}
+              disabled={name === queryModel.name}
+            >
+              <IconDeviceFloppy size={16} />
+            </ActionIcon>
+          }
+          onBlur={() => {
+            queryModel.setName(name);
           }}
         />
         <SelectDataSource
