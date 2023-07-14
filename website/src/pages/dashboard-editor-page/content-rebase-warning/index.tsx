7dfaea01f527fb847408e73584d8d1a5914dffed
Leto <gerile.tu@merico.dev>
2023-07-14 16:23:10 +0800

    fix(website): fix a outdated prop

--- website/src/pages/dashboard-editor-page/content-rebase-warning/index.tsx
+++ website/src/pages/dashboard-editor-page/content-rebase-warning/index.tsx
@@ -100,7 +100,7 @@ export const ContentRebaseWarning = observer(() => {
             </Text>
           </Group>
         }
-        disallowClose
+        withCloseButton={false}
         sx={{ position: 'fixed', top: 10, right: 15, zIndex: 410 }}
       >
         <Text mt={10} color="dark">
