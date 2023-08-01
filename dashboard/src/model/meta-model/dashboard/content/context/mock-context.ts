f6fa6f08614ceb494f81112ca916335d58ae14d7
Leto <gerile.tu@merico.dev>
2023-08-01 21:05:33 +0800

    refactor(dashboard): move ContextMeta to the same level of dashboard

--- dashboard/src/model/meta-model/dashboard/content/context/mock-context.ts
+++ dashboard/src/model/meta-model/dashboard/content/context/mock-context.ts
@@ -1,5 +1,5 @@
 import { types } from 'mobx-state-tree';
-import { ContextRecordType } from './context';
+import { ContextRecordType } from '~/model';
 
 export const MockContextMeta = types.model('MockContextMeta', {
   current: types.optional(types.frozen<ContextRecordType>(), {}),
