793502385026c37f4499235bd3a07f0fef6d5c78
Leto <gerile.tu@merico.dev>
2023-08-02 21:38:31 +0800

    refactor(dashboard): add PanelRenderBase

--- dashboard/src/components/panel/panel-editor/index.tsx
+++ dashboard/src/components/panel/panel-editor/index.tsx
@@ -1,22 +1,14 @@
-import { Box } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 import { PanelModelInstance } from '~/dashboard-editor/model/panels';
 import { ViewMetaInstance } from '~/model';
-import { PanelContextProvider } from '~/contexts/panel-context';
-import { DescriptionPopover } from '../panel-render/description-popover';
 import { PanelDropdownMenu } from '../panel-render/dropdown-menu';
-import { PanelVizSection } from '../panel-render/viz/panel-viz-section';
-import { PanelTitleBar } from '../panel-render/title-bar';
-import '../panel-render/panel-render.css';
+import { PanelRenderBase } from '../panel-render/panel-render-base';
 
 interface IPanel {
   view: ViewMetaInstance;
   panel: PanelModelInstance;
 }
 
-const constantBorder = {
-  border: '1px solid #e9ecef',
-};
 const hoverBorder = {
   border: '1px dashed transparent',
   transition: 'border-color 300ms ease',
@@ -27,32 +19,13 @@ const hoverBorder = {
 
 function getPanelBorderStyle(panel: PanelModelInstance) {
   if (panel.style.border.enabled) {
-    return constantBorder;
+    return {};
   }
 
   return hoverBorder;
 }
 
 export const Panel = observer(function _Panel({ panel, view }: IPanel) {
-  const contentHeight = !panel.title ? '100%' : 'calc(100% - 25px - 5px)';
   const panelStyle = getPanelBorderStyle(panel);
-  return (
-    <PanelContextProvider value={{ panel, data: panel.data, loading: panel.dataLoading, errors: panel.queryErrors }}>
-      <Box
-        className="panel-root"
-        p={5}
-        pt={0}
-        sx={{
-          ...panelStyle,
-        }}
-      >
-        <Box sx={{ position: 'absolute', left: 0, top: 0, height: 28, zIndex: 310 }}>
-          <DescriptionPopover />
-        </Box>
-        <PanelDropdownMenu view={view} />
-        <PanelTitleBar />
-        <PanelVizSection panel={panel} height={contentHeight} />
-      </Box>
-    </PanelContextProvider>
-  );
+  return <PanelRenderBase panel={panel} panelStyle={panelStyle} dropdownContent={<PanelDropdownMenu view={view} />} />;
 });
---
+++ dashboard/src/components/panel/panel-render/panel-render-base.tsx
@@ -0,0 +1,41 @@
+import { Box, Sx } from '@mantine/core';
+import { observer } from 'mobx-react-lite';
+import { ReactNode } from 'react';
+import { PanelContextProvider } from '~/contexts/panel-context';
+import { PanelRenderModelInstance, ViewMetaInstance } from '~/model';
+import { DescriptionPopover } from './description-popover';
+import './panel-render-base.css';
+import { PanelTitleBar } from './title-bar';
+import { PanelVizSection } from './viz';
+
+interface IPanelBase {
+  panel: PanelRenderModelInstance;
+  dropdownContent?: ReactNode;
+  panelStyle: Sx;
+}
+
+const baseStyle: Sx = { border: '1px solid #e9ecef' };
+
+export const PanelRenderBase = observer(({ panel, panelStyle, dropdownContent }: IPanelBase) => {
+  const contentHeight = !panel.title ? '100%' : 'calc(100% - 25px - 5px)';
+  return (
+    <PanelContextProvider value={{ panel, data: panel.data, loading: panel.dataLoading, errors: panel.queryErrors }}>
+      <Box
+        className="panel-root"
+        p={5}
+        pt={0}
+        sx={{
+          ...baseStyle,
+          ...panelStyle,
+        }}
+      >
+        <Box sx={{ position: 'absolute', left: 0, top: 0, height: 28, zIndex: 310 }}>
+          <DescriptionPopover />
+        </Box>
+        {dropdownContent}
+        <PanelTitleBar />
+        <PanelVizSection panel={panel} height={contentHeight} />
+      </Box>
+    </PanelContextProvider>
+  );
+});
