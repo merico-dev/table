c6206ee48b6b445bd302e8d5173a486edc9cd804
Leto <gerile.tu@merico.dev>
2023-08-02 16:54:42 +0800

    refactor(dashboard): model-content & content-model-content supports 2 kinds of model instance

--- dashboard/src/contexts/content-model-context.ts
+++ dashboard/src/contexts/content-model-context.ts
@@ -1,14 +1,17 @@
 import React from 'react';
 import { ContentModelInstance } from '~/dashboard-editor/model/content';
+import { ContentRenderModelInstance } from '~/dashboard-render/model/content';
 
-const ContentModelContext = React.createContext<ContentModelInstance | null>(null);
+const ContentModelContext = React.createContext<ContentModelInstance | ContentRenderModelInstance | null>(null);
 
 export const ContentModelContextProvider = ContentModelContext.Provider;
 
-export function useContentModelContext() {
+export function useContentModelContext<T = ContentModelInstance>() {
   const model = React.useContext(ContentModelContext);
   if (!model) {
     throw new Error('Please use ContentModelContextProvider');
   }
-  return model;
+  return model as T;
 }
+export const useEditContentModelContext = () => useContentModelContext<ContentModelInstance>();
+export const useRenderContentModelContext = () => useContentModelContext<ContentRenderModelInstance>();
--- dashboard/src/contexts/model-context.ts
+++ dashboard/src/contexts/model-context.ts
@@ -1,14 +1,18 @@
 import React from 'react';
 import { DashboardModelInstance } from '../dashboard-editor/model';
+import { DashboardRenderModelInstance } from '~/dashboard-render/model/dashboard';
 
-const ModelContext = React.createContext<DashboardModelInstance | null>(null);
+const ModelContext = React.createContext<DashboardModelInstance | DashboardRenderModelInstance | null>(null);
 
 export const ModelContextProvider = ModelContext.Provider;
 
-export function useModelContext() {
+export function useModelContext<T = DashboardModelInstance>() {
   const model = React.useContext(ModelContext);
   if (!model) {
     throw new Error('Please use ModelContextProvider');
   }
-  return model;
+  return model as T;
 }
+
+export const useEditModelContext = () => useModelContext<DashboardModelInstance>();
+export const useRenderModelContext = () => useModelContext<DashboardRenderModelInstance>();
