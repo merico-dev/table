cc3916dff65e6b38027f13f22de5832823b36d9f
Leto <gerile.tu@merico.dev>
2023-08-03 21:31:52 +0800

    feat(dashboard): editor for metric.postfix of merico-stats

--- dashboard/src/components/plugins/viz-components/merico-stats/editors/metrics/metric.tsx
+++ dashboard/src/components/plugins/viz-components/merico-stats/editors/metrics/metric.tsx
@@ -1,17 +1,20 @@
 import { ActionIcon, Divider, Group, Select, SelectItem, Stack, TextInput } from '@mantine/core';
-import { Control, Controller, UseFieldArrayRemove } from 'react-hook-form';
+import { Control, Controller, UseFieldArrayRemove, UseFormWatch } from 'react-hook-form';
 import { Trash } from 'tabler-icons-react';
 import { NumbroFormatSelector } from '~/components/panel/settings/common/numbro-format-selector';
 import { TMericoStatsConf } from '../../type';
+import { PostfixField } from './postfix-field';
 
 interface IProps {
   control: Control<TMericoStatsConf, $TSFixMe>;
   index: number;
   remove: UseFieldArrayRemove;
   variableOptions: SelectItem[];
+  watch: UseFormWatch<TMericoStatsConf>;
 }
 
-export function MetricField({ control, index, remove, variableOptions }: IProps) {
+export function MetricField({ control, index, remove, watch, variableOptions }: IProps) {
+  watch(`metrics.${index}.postfix`);
   return (
     <Stack key={index} my={0} p="md" pr={40} sx={{ border: '1px solid #eee', position: 'relative' }}>
       <Group grow noWrap>
@@ -44,14 +47,18 @@ export function MetricField({ control, index, remove, variableOptions }: IProps)
           )}
         />
       </Group>
-      <Stack>
-        <Divider mb={-15} variant="dashed" label="Format" labelPosition="center" />
-        <Controller
-          name={`metrics.${index}.formatter`}
-          control={control}
-          render={({ field }) => <NumbroFormatSelector {...field} />}
-        />
-      </Stack>
+      <Divider mb={-15} variant="dashed" label="Format" labelPosition="center" />
+      <Controller
+        name={`metrics.${index}.formatter`}
+        control={control}
+        render={({ field }) => <NumbroFormatSelector {...field} />}
+      />
+      <Divider mb={-15} variant="dashed" label="Others" labelPosition="center" />
+      <Controller
+        name={`metrics.${index}.postfix`}
+        control={control}
+        render={({ field }) => <PostfixField {...field} />}
+      />
       <ActionIcon
         color="red"
         variant="subtle"
