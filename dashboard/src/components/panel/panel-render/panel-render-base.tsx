3e6bc3689f476a054af5fda2e88d1a3d38d89705
Leto <gerile.tu@merico.dev>
2023-08-09 22:59:23 +0800

    refactor(dashboard): add useDownloadScreenshot

--- dashboard/src/components/panel/panel-render/panel-render-base.tsx
+++ dashboard/src/components/panel/panel-render/panel-render-base.tsx
@@ -1,14 +1,13 @@
-import { Box, Button, Sx } from '@mantine/core';
+import { Box, Sx } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
-import { ReactNode, useRef } from 'react';
+import { ReactNode } from 'react';
 import { PanelContextProvider } from '~/contexts/panel-context';
 import { PanelRenderModelInstance } from '~/model';
 import { DescriptionPopover } from './description-popover';
 import './panel-render-base.css';
 import { PanelTitleBar } from './title-bar';
+import { useDownloadScreenshot } from './use-download-screenshot';
 import { PanelVizSection } from './viz';
-// @ts-expect-error dom-to-image-more's declaration file
-import domtoimage from 'dom-to-image-more';
 
 interface IPanelBase {
   panel: PanelRenderModelInstance;
@@ -19,27 +18,8 @@ interface IPanelBase {
 const baseStyle: Sx = { border: '1px solid #e9ecef' };
 
 export const PanelRenderBase = observer(({ panel, panelStyle, dropdownContent }: IPanelBase) => {
-  const panelRootRef = useRef<HTMLDivElement>(null);
+  const { ref, downloadPanelScreenshot } = useDownloadScreenshot(panel);
   const contentHeight = !panel.title ? '100%' : 'calc(100% - 25px - 5px)';
-  const downloadPanelScreenshot = () => {
-    const dom = panelRootRef.current;
-    if (!dom) {
-      return;
-    }
-
-    const width = dom.offsetWidth * 2;
-    const height = dom.offsetHeight * 2;
-    domtoimage
-      .toBlob(panelRootRef.current, {
-        bgcolor: 'white',
-        width,
-        height,
-        style: { transformOrigin: '0 0', transform: 'scale(2)' },
-      })
-      .then((blob: string) => {
-        window.saveAs(blob, `${panel.title ? panel.title : panel.viz.type}.png`);
-      });
-  };
   return (
     <PanelContextProvider
       value={{
@@ -52,7 +32,7 @@ export const PanelRenderBase = observer(({ panel, panelStyle, dropdownContent }:
     >
       <Box
         className="panel-root"
-        ref={panelRootRef}
+        ref={ref}
         p={5}
         pt={0}
         sx={{
