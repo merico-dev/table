ef97bfcf44507343fe674e9467be9f5ea803aaae
Leto <gerile.tu@merico.dev>
2023-08-16 23:17:28 +0800

    feat(dashboard): no longer render content in TabsView expect tabs

--- dashboard/src/components/view/view-component/preview/tabs.tsx
+++ dashboard/src/components/view/view-component/preview/tabs.tsx
@@ -2,7 +2,7 @@ import { Box, Button, ColorInput, Overlay, Select, Stack, Sx, Tabs, TextInput } 
 import { useModals } from '@mantine/modals';
 import { IconArrowsLeftRight, IconTrash } from '@tabler/icons-react';
 import { observer } from 'mobx-react-lite';
-import { ReactNode, useMemo } from 'react';
+import { useMemo } from 'react';
 import { Plus } from 'tabler-icons-react';
 import { DashboardViewRender } from '~/components/view';
 import { useEditContentModelContext } from '~/contexts';
@@ -40,7 +40,7 @@ const getTabSX = (t: TabModelInstance): Sx => {
   return {};
 };
 
-export const PreviewViewTabs = observer(({ children, view }: { children: ReactNode; view: ViewMetaInstance }) => {
+export const PreviewViewTabs = observer(({ view }: { view: ViewMetaInstance }) => {
   const modals = useModals();
   const model = useEditContentModelContext();
   const options = useMemo(
@@ -64,71 +64,69 @@ export const PreviewViewTabs = observer(({ children, view }: { children: ReactNo
 
   const onlyOneTabLeft = config.tabs.length === 0;
   return (
-    <Box className="preview-view-tabs">
-      <Tabs
-        variant={config.variant}
-        orientation={config.orientation}
-        defaultValue={config.tabs.length > 0 ? config.tabs[0].id : '0'}
-        styles={getStyles(config)}
-      >
-        <Tabs.List grow={config.grow}>
-          {config.tabs.map((t) => (
-            <Tabs.Tab key={t.id} value={t.id} sx={getTabSX(t)}>
-              {t.name ?? t.id}
-            </Tabs.Tab>
-          ))}
-          <Tabs.Tab onClick={config.addTab} value="add" className="add-a-tab">
-            <Plus size={18} color="#228be6" />
+    <Tabs
+      className="preview-view-tabs"
+      variant={config.variant}
+      orientation={config.orientation}
+      defaultValue={config.tabs.length > 0 ? config.tabs[0].id : '0'}
+      styles={getStyles(config)}
+    >
+      <Tabs.List grow={config.grow}>
+        {config.tabs.map((t) => (
+          <Tabs.Tab key={t.id} value={t.id} sx={getTabSX(t)}>
+            {t.name ?? t.id}
           </Tabs.Tab>
-        </Tabs.List>
-        {config.tabs.map((t, i) => {
-          const tabView = model.views.findByID(t.view_id);
-          return (
-            <Tabs.Panel key={t.id} value={t.id} sx={{ position: 'relative' }}>
-              <Box sx={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 200 }}>
-                <Overlay opacity={0.8} color="#FFF" blur={10} zIndex={100} />
+        ))}
+        <Tabs.Tab onClick={config.addTab} value="add" className="add-a-tab">
+          <Plus size={18} color="#228be6" />
+        </Tabs.Tab>
+      </Tabs.List>
+      {config.tabs.map((t, i) => {
+        const tabView = model.views.findByID(t.view_id);
+        return (
+          <Tabs.Panel key={t.id} value={t.id} sx={{ position: 'relative' }}>
+            <Box sx={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 200 }}>
+              <Overlay opacity={0.8} color="#FFF" blur={10} zIndex={100} />
 
-                <Stack mx="auto" mt={100} sx={{ width: '300px', position: 'relative', zIndex: 200 }}>
-                  <TextInput label="Tab Name" value={t.name} onChange={(e) => t.setName(e.currentTarget.value)} />
-                  <Select label="View" value={t.view_id} onChange={t.setViewID} data={options} />
-                  <ColorInput
-                    label="Color"
-                    value={t.color}
-                    onChange={t.setColor}
-                    disabled={config.variant !== 'default'}
-                  />
+              <Stack mx="auto" mt={100} sx={{ width: '300px', position: 'relative', zIndex: 200 }}>
+                <TextInput label="Tab Name" value={t.name} onChange={(e) => t.setName(e.currentTarget.value)} />
+                <Select label="View" value={t.view_id} onChange={t.setViewID} data={options} />
+                <ColorInput
+                  label="Color"
+                  value={t.color}
+                  onChange={t.setColor}
+                  disabled={config.variant !== 'default'}
+                />
 
-                  {tabView && (
-                    <Button
-                      mt={20}
-                      variant="gradient"
-                      leftIcon={<IconArrowsLeftRight size={18} />}
-                      gradient={{ from: 'cyan', to: 'indigo' }}
-                      onClick={() => model.views.setIDOfVIE(tabView.id)}
-                    >
-                      Swith to View: {tabView.name}
-                    </Button>
-                  )}
-
+                {tabView && (
                   <Button
                     mt={20}
-                    variant="subtle"
-                    color="red"
-                    onClick={() => remove(i)}
-                    disabled={onlyOneTabLeft}
-                    leftIcon={<IconTrash size={14} />}
+                    variant="gradient"
+                    leftIcon={<IconArrowsLeftRight size={18} />}
+                    gradient={{ from: 'cyan', to: 'indigo' }}
+                    onClick={() => model.views.setIDOfVIE(tabView.id)}
                   >
-                    Delete This Tab
+                    Swith to View: {tabView.name}
                   </Button>
-                </Stack>
-              </Box>
+                )}
 
-              {tabView && <DashboardViewRender view={tabView} />}
-            </Tabs.Panel>
-          );
-        })}
-      </Tabs>
-      {children}
-    </Box>
+                <Button
+                  mt={20}
+                  variant="subtle"
+                  color="red"
+                  onClick={() => remove(i)}
+                  disabled={onlyOneTabLeft}
+                  leftIcon={<IconTrash size={14} />}
+                >
+                  Delete This Tab
+                </Button>
+              </Stack>
+            </Box>
+
+            {tabView && <DashboardViewRender view={tabView} />}
+          </Tabs.Panel>
+        );
+      })}
+    </Tabs>
   );
 });
