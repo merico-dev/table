04ee536c909c834e965bc22bd97115fe94400be2
Leto <gerile.tu@merico.dev>
2023-09-15 16:00:40 +0800

    fix(dashboard): regression chart’s toolbox has wrong paddings

--- dashboard/src/components/plugins/viz-components/regression-chart/toolbox/index.tsx
+++ dashboard/src/components/plugins/viz-components/regression-chart/toolbox/index.tsx
@@ -1,9 +1,13 @@
 import { Group } from '@mantine/core';
 import { IRegressionDescription, RegressionDescription } from './regression-description';
+import { paddings } from '~/styles/viz-box';
 
 export function Toolbox(props: IRegressionDescription) {
   return (
-    <Group position="apart" sx={{ position: 'absolute', top: 0, left: 0, height: '22px', zIndex: 1 }}>
+    <Group
+      position="apart"
+      sx={{ position: 'absolute', top: 0, left: paddings.left, right: paddings.right, height: '22px', zIndex: 1 }}
+    >
       <RegressionDescription {...props} />
     </Group>
   );
