dc77760017738adb3a2ca2c366ca6f3b2760e070
Leto <gerile.tu@merico.dev>
2023-09-25 17:25:54 +0800

    fix(dashboard): numeric data name on cartesian's category xaxis leads to wrong yAxis's max value

--- dashboard/src/components/plugins/viz-components/cartesian/option/series/reference_lines.ts
+++ dashboard/src/components/plugins/viz-components/cartesian/option/series/reference_lines.ts
@@ -1,5 +1,21 @@
 import { ITemplateVariable, templateToString } from '~/utils/template';
-import { ICartesianReferenceLine } from '../../type';
+import { ICartesianChartConf, ICartesianReferenceLine } from '../../type';
+
+function getInvisibleScatterData(
+  value: string | number,
+  isHorizontal: boolean,
+  xAxisData: string[],
+  xAxisType: ICartesianChartConf['x_axis']['type'],
+) {
+  if (!isHorizontal) {
+    return [];
+  }
+  if (xAxisType === 'value') {
+    return [[xAxisData[0], value]];
+  } else {
+    return [value];
+  }
+}
 
 export function getReferenceLines(
   reference_lines: ICartesianReferenceLine[],
@@ -7,17 +23,19 @@ export function getReferenceLines(
   variableValueMap: Record<string, string | number>,
   data: TPanelData,
   xAxisData: string[],
+  xAxisType: ICartesianChartConf['x_axis']['type'],
 ) {
   return reference_lines.map((r) => {
     const value = variableValueMap[r.variable_key];
     const isHorizontal = r.orientation === 'horizontal';
     const keyOfAxis = isHorizontal ? 'yAxis' : 'xAxis';
     const position = isHorizontal ? 'insideEndTop' : 'end';
-    const seriesData = isHorizontal ? [xAxisData[0], value] : [];
+    const seriesData = getInvisibleScatterData(value, isHorizontal, xAxisData, xAxisType);
     return {
       name: r.name,
       type: 'scatter',
       hide_in_legend: !r.show_in_legend,
+      xAxisId: 'main-x-axis',
       yAxisIndex: r.yAxisIndex,
       data: seriesData,
       symbol: 'none',
