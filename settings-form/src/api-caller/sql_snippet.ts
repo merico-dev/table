160753f31f91e4e3ee4f8cb226138d3159a2693d
Leto <gerile.tu@merico.dev>
2023-09-26 16:05:10 +0800

    fix(settings-form): fix some type errors

--- settings-form/src/api-caller/sql_snippet.ts
+++ settings-form/src/api-caller/sql_snippet.ts
@@ -10,15 +10,19 @@ import { APIClient } from './request';
 
 export const sql_snippet = {
   list: async ({ pagination }: ListSQLSnippetReqType, signal?: AbortSignal): Promise<ListSQLSnippetRespType> => {
-    const resp: ListSQLSnippetRespType = await APIClient.post(signal)('/sql_snippet/list', {
-      sort: [
-        {
-          field: 'update_time',
-          order: 'DESC',
-        },
-      ],
-      pagination,
-    });
+    const resp: ListSQLSnippetRespType = await APIClient.post(signal)(
+      '/sql_snippet/list',
+      {
+        sort: [
+          {
+            field: 'update_time',
+            order: 'DESC',
+          },
+        ],
+        pagination,
+      },
+      {},
+    );
     resp.data.forEach((d) => {
       d.create_time = dayjs(d.create_time).format('YYYY-MM-DD HH:mm:ss');
       d.update_time = dayjs(d.update_time).format('YYYY-MM-DD HH:mm:ss');
@@ -26,20 +30,24 @@ export const sql_snippet = {
     return resp;
   },
   get: async (id: string, signal?: AbortSignal): Promise<SQLSnippetDBType | null> => {
-    const resp: SQLSnippetDBType = await APIClient.post(signal)('/sql_snippet/get', {
-      id,
-    });
+    const resp: SQLSnippetDBType = await APIClient.post(signal)(
+      '/sql_snippet/get',
+      {
+        id,
+      },
+      {},
+    );
     return resp;
   },
   create: async (payload: TCreateSQLSnippetPayload, signal?: AbortSignal): Promise<SQLSnippetDBType> => {
-    const resp: SQLSnippetDBType = await APIClient.post(signal)('/sql_snippet/create', payload);
+    const resp: SQLSnippetDBType = await APIClient.post(signal)('/sql_snippet/create', payload, {});
     return resp;
   },
   update: async (payload: TUpdateSQLSnippetPayload, signal?: AbortSignal): Promise<SQLSnippetDBType> => {
-    const resp: SQLSnippetDBType = await APIClient.put(signal)('/sql_snippet/update', payload);
+    const resp: SQLSnippetDBType = await APIClient.put(signal)('/sql_snippet/update', payload, {});
     return resp;
   },
   delete: async (id: string, signal?: AbortSignal): Promise<void> => {
-    return APIClient.post(signal)('/sql_snippet/delete', { id });
+    return APIClient.post(signal)('/sql_snippet/delete', { id }, {});
   },
 };
