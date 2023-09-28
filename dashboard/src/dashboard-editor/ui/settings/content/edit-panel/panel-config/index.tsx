83dfa196860cad5d73d8a0d467d1cdd2ebf661f7
Leto <gerile.tu@merico.dev>
2023-09-28 16:11:57 +0800

    feat(dashboard): change panel’s title editor to name editor

--- dashboard/src/dashboard-editor/ui/settings/content/edit-panel/panel-config/index.tsx
+++ dashboard/src/dashboard-editor/ui/settings/content/edit-panel/panel-config/index.tsx
@@ -1,14 +1,14 @@
 import { Divider, Stack } from '@mantine/core';
 import { EditDescription } from './description';
 import { EditStyle } from './edit-style';
-import { EditTitle } from './title';
+import { EditName } from './name';
 
 export function PanelConfig() {
   return (
     <Stack sx={{ height: '100%' }}>
       <EditStyle />
       <Divider label="Info" labelPosition="center" variant="dashed" />
-      <EditTitle />
+      <EditName />
       <EditDescription />
     </Stack>
   );
--- dashboard/src/dashboard-editor/ui/settings/content/edit-panel/panel-config/name.tsx
+++ dashboard/src/dashboard-editor/ui/settings/content/edit-panel/panel-config/name.tsx
@@ -5,31 +5,31 @@ import React, { useEffect } from 'react';
 import { DeviceFloppy } from 'tabler-icons-react';
 import { useEditPanelContext } from '~/contexts/panel-context';
 
-export const EditTitle = observer(() => {
+export const EditName = observer(() => {
   const { panel } = useEditPanelContext();
-  const { title, setTitle } = panel;
-  const [localTitle, setLocalTitle] = useInputState(title);
+  const { name } = panel;
+  const [local, setLocal] = useInputState(name);
 
-  const changed = title !== localTitle;
+  const changed = name !== local;
 
   const submit = React.useCallback(() => {
     if (!changed) {
       return;
     }
-    setTitle(localTitle);
-  }, [changed, localTitle]);
+    panel.setName(local);
+  }, [changed, local]);
 
   useEffect(() => {
-    setLocalTitle(title);
-  }, [title]);
+    setLocal(name);
+  }, [name]);
 
   return (
     <TextInput
-      value={localTitle}
-      onChange={setLocalTitle}
+      value={local}
+      onChange={setLocal}
       label={
         <Group>
-          <Text>Panel Title</Text>
+          <Text>Panel Name</Text>
           <ActionIcon variant="subtle" color="blue" disabled={!changed} onClick={submit}>
             <DeviceFloppy size={20} />
           </ActionIcon>
