10a50b22a7253b51b26b7cb820374f5fa2dfb85c
Leto <gerile.tu@merico.dev>
2023-10-13 13:29:35 +0800

    feat(dashboard): add tooltip to AddAPanel button when it’s disabled

---
+++ dashboard/src/dashboard-editor/ui/header/add-a-panel/index.tsx
@@ -0,0 +1,55 @@
+import { Button, Sx, Tooltip } from '@mantine/core';
+import { IconPlaylistAdd } from '@tabler/icons-react';
+import { observer } from 'mobx-react-lite';
+import { useEditContentModelContext } from '~/contexts';
+import { EViewComponentType } from '~/model';
+
+const ButtonSx: Sx = {
+  height: '30px',
+  borderLeft: 'none',
+  borderTop: 'none',
+  borderRight: '1px solid #e9ecef',
+  borderBottom: '1px solid #e9ecef',
+};
+
+export const AddAPanel = observer(() => {
+  const contentModel = useEditContentModelContext();
+  const cant = contentModel.views.VIE?.type === EViewComponentType.Tabs;
+  const add = () => contentModel.addANewPanel(contentModel.views.idOfVIE);
+  if (cant) {
+    return (
+      <Tooltip label="Please choose a specifc tab first">
+        <Button
+          variant="outline"
+          color="gray"
+          radius={0}
+          size="xs"
+          leftIcon={<IconPlaylistAdd size={20} />}
+          sx={{
+            ...ButtonSx,
+            transform: 'none !important',
+          }}
+        >
+          Add a Panel
+        </Button>
+      </Tooltip>
+    );
+  }
+
+  return (
+    <Button
+      variant="outline"
+      color="blue"
+      radius={0}
+      size="xs"
+      onClick={add}
+      leftIcon={<IconPlaylistAdd size={20} />}
+      sx={{
+        ...ButtonSx,
+        background: 'rgb(231, 245, 255)',
+      }}
+    >
+      Add a Panel
+    </Button>
+  );
+});
