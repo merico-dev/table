607ecad5b5e4f5170a2902f3e8b0fca1e62dafc8
Leto <gerile.tu@merico.dev>
2023-10-24 15:54:06 +0800

    feat(dashboard): don’t preview variables when it’s empty

--- dashboard/src/dashboard-editor/ui/settings/content/edit-panel/variable-config/preview-variables.tsx
+++ dashboard/src/dashboard-editor/ui/settings/content/edit-panel/variable-config/preview-variables.tsx
@@ -1,17 +1,17 @@
 import { Prism } from '@mantine/prism';
 import { observer } from 'mobx-react-lite';
-import { useMemo } from 'react';
 import { useEditPanelContext } from '~/contexts';
-import { variablesToStrings } from '~/utils/template';
 
 export const PreviewVariables = observer(() => {
   const { panel } = useEditPanelContext();
-  const variables = useMemo(() => {
-    return variablesToStrings(panel.variables, panel.data);
-  }, [panel.variables]);
+
+  if (Object.keys(panel.variableStrings).length === 0) {
+    return null;
+  }
+
   return (
     <Prism mt={22} language="json" colorScheme="dark" noCopy>
-      {JSON.stringify(variables, null, 4)}
+      {JSON.stringify(panel.variableStrings, null, 4)}
     </Prism>
   );
 });
