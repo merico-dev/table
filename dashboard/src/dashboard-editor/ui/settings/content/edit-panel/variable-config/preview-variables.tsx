cf275ba80f3298193227411d06719c2e7e022f49
Leto <gerile.tu@merico.dev>
2023-10-25 13:20:46 +0800

    style(dashboard): misc

--- dashboard/src/dashboard-editor/ui/settings/content/edit-panel/variable-config/preview-variables.tsx
+++ dashboard/src/dashboard-editor/ui/settings/content/edit-panel/variable-config/preview-variables.tsx
@@ -1,3 +1,4 @@
+import { Stack, Text } from '@mantine/core';
 import { Prism } from '@mantine/prism';
 import { observer } from 'mobx-react-lite';
 import { useEditPanelContext } from '~/contexts';
@@ -10,8 +11,13 @@ export const PreviewVariables = observer(() => {
   }
 
   return (
-    <Prism mt={22} language="json" colorScheme="dark" noCopy>
-      {JSON.stringify(panel.variableStrings, null, 4)}
-    </Prism>
+    <Stack mt={22} spacing={4}>
+      <Text size="sm" fw={500} color="dimmed">
+        Variables
+      </Text>
+      <Prism language="json" colorScheme="dark" noCopy>
+        {JSON.stringify(panel.variableStrings, null, 4)}
+      </Prism>
+    </Stack>
   );
 });
