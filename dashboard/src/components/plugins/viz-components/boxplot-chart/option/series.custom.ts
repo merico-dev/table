2e7133b9683dc2f3bd9a8ee4c8411f3a22ebafed
Leto <gerile.tu@merico.dev>
2023-11-07 17:58:26 +0800

    feat(dashboard): custom box & scatter

--- dashboard/src/components/plugins/viz-components/boxplot-chart/option/series.custom.ts
+++ dashboard/src/components/plugins/viz-components/boxplot-chart/option/series.custom.ts
@@ -1,8 +1,9 @@
 import { CustomSeriesRenderItemAPI, CustomSeriesRenderItemParams } from 'echarts';
 import { AnyObject } from '~/types';
-import { IBoxplotDataItem } from '../type';
+import { IBoxplotChartConf, IBoxplotDataItem } from '../type';
 
 type Point = any[];
+export type BoxplotDataset = { source: IBoxplotDataItem[] };
 
 function countData(data: number[]) {
   const group: AnyObject = {}; // value to count
@@ -19,18 +20,22 @@ function countData(data: number[]) {
 
 function prepare({ boxplotDataset, api }: Props) {
   const categoryIndex = api.value(0) as number;
-  const { min, max, violinData } = boxplotDataset.source[categoryIndex];
+  const source = boxplotDataset.source[categoryIndex];
+  const { min, max, violinData, outliers } = source;
 
   const { group, maxCount } = countData(violinData);
   const arr = Object.entries(group).sort((a, b) => Number(b[0]) - Number(a[0]));
 
+  const outlierGroup = countData(outliers.map((o) => o[1]));
   return {
     group,
     maxCount,
     min,
     max,
+    source,
     categoryIndex,
     arr,
+    outlierGroup,
   };
 }
 
@@ -185,4 +190,199 @@ export function getDots(props: Props) {
       }),
     })),
   };
+}
+
+type BoxplotSeries = {
+  name: string;
+  type: 'custom';
+  boxWidth: [number, number];
+
+  [key: string]: any;
+};
+
+function getBox(props: Props, seriesConf: BoxplotSeries) {
+  const { api } = props;
+  const { categoryIndex, arr, maxCount, source, outlierGroup } = prepare(props);
+
+  const { boxWidth, itemStyle } = seriesConf;
+  const layout = api.barLayout({
+    barMinWidth: boxWidth[0],
+    barMaxWidth: boxWidth[1],
+    count: 2,
+  });
+
+  const { min, q1, median, q3, max } = source;
+  const centers = {
+    min: api.coord([categoryIndex, min]),
+    q1: api.coord([categoryIndex, q1]),
+    median: api.coord([categoryIndex, median]),
+    q3: api.coord([categoryIndex, q3]),
+    max: api.coord([categoryIndex, max]),
+  };
+
+  const offsets = {
+    left: layout[1].offset,
+    center: (layout[1].offset + layout[1].width) / 2,
+    right: layout[1].width,
+  };
+
+  const box: any = {
+    type: 'group',
+    children: [],
+  };
+  // Lines
+  const lines: any[] = [
+    // max
+    {
+      x1: centers.max[0] + offsets.left,
+      x2: centers.max[0] + offsets.right,
+      y1: centers.max[1],
+      y2: centers.max[1],
+    },
+    // median
+    {
+      x1: centers.median[0] + offsets.left,
+      x2: centers.median[0] + offsets.right,
+      y1: centers.median[1],
+      y2: centers.median[1],
+    },
+    // min
+    {
+      x1: centers.min[0] + offsets.left,
+      x2: centers.min[0] + offsets.right,
+      y1: centers.min[1],
+      y2: centers.min[1],
+    },
+    // vertical center lines
+    {
+      x1: centers.min[0] + offsets.center,
+      x2: centers.min[0] + offsets.center,
+      y1: centers.min[1],
+      y2: centers.q1[1],
+    },
+    {
+      x1: centers.max[0] + offsets.center,
+      x2: centers.max[0] + offsets.center,
+      y1: centers.max[1],
+      y2: centers.q3[1],
+    },
+  ];
+  const lineStyle = {
+    stroke: itemStyle.borderColor,
+    lineWidth: itemStyle.borderWidth,
+  };
+
+  lines.forEach((l) => {
+    box.children.push({
+      type: 'line',
+      shape: l,
+      style: lineStyle,
+    });
+  });
+
+  // Box
+  box.children.push({
+    type: 'rect',
+    shape: {
+      x: centers.q1[0] + offsets.left,
+      y: centers.q1[1],
+      width: offsets.right - offsets.left,
+      height: centers.q3[1] - centers.q1[1],
+    },
+    style: {
+      fill: itemStyle.color,
+      stroke: itemStyle.borderColor,
+      lineWidth: itemStyle.borderWidth,
+    },
+  });
+
+  // Dots
+  const unit = layout[0].width / maxCount;
+  const generalShift = layout[0].offset - layout[1].offset;
+
+  const dots: AnyObject[] = [];
+  arr.forEach(([value, count]) => {
+    const [x, y] = api.coord([categoryIndex, value]);
+    const start = Math.floor((maxCount - count) / 2) * unit;
+    for (let i = 0; i < count; i++) {
+      const shift = start + i * unit + generalShift;
+      dots.push({ cx: x + shift, cy: y });
+    }
+  });
+
+  const dotStyle = {
+    fill: '#ED6A45',
+    stroke: 'transparent',
+    opacity: 0.3,
+    lineDash: 'dotted',
+  };
+
+  const scatter = {
+    type: 'group',
+    children: dots.map((d) => ({
+      type: 'circle',
+      transition: ['shape'],
+      shape: {
+        ...d,
+        r: 1,
+      },
+      style: dotStyle,
+    })),
+  };
+
+  // Outliers
+  const outlierDots: AnyObject[] = [];
+  Object.entries(outlierGroup.group).forEach(([value, count]) => {
+    const [x, y] = api.coord([categoryIndex, value]);
+    const start = Math.floor((maxCount - count) / 2) * unit;
+    for (let i = 0; i < count; i++) {
+      const shift = start + i * unit + generalShift;
+      outlierDots.push({ cx: x + shift, cy: y });
+    }
+  });
+
+  const outlierDotStyle = {
+    fill: '#ED6A45',
+    stroke: 'transparent',
+    lineDash: 'dotted',
+  };
+  const outliers = {
+    type: 'group',
+    children: outlierDots.map((d) => ({
+      type: 'circle',
+      transition: ['shape'],
+      shape: {
+        ...d,
+        r: 3,
+      },
+      style: outlierDotStyle,
+    })),
+  };
+  return {
+    type: 'group',
+    children: [box, scatter, outliers],
+  };
+}
+
+export function getCustomBoxplot(boxplotDataset: BoxplotDataset, conf: IBoxplotChartConf) {
+  const { color } = conf;
+  const series: BoxplotSeries = {
+    name: 'Custom Box',
+    type: 'custom',
+    itemStyle: {
+      color,
+      borderColor: '#2F8CC0',
+      borderWidth: 2,
+    },
+    emphasis: {
+      disabled: true,
+    },
+    boxWidth: [10, 40],
+    datasetIndex: 0,
+  };
+
+  series.renderItem = (params: CustomSeriesRenderItemParams, api: CustomSeriesRenderItemAPI) => {
+    return getBox({ boxplotDataset, params, api }, series);
+  };
+  return series;
 }
--- dashboard/src/components/plugins/viz-components/boxplot-chart/option/series.ts
+++ dashboard/src/components/plugins/viz-components/boxplot-chart/option/series.ts
@@ -1,33 +1,65 @@
 import { CustomSeriesRenderItemAPI, CustomSeriesRenderItemParams } from 'echarts';
-import { IBoxplotChartConf, IBoxplotDataItem } from '../type';
+import { IBoxplotChartConf } from '../type';
+import { BoxplotDataset, getCustomBoxplot, getDots, getLines, getViolin } from './series.custom';
 import { BOXPLOT_DATA_ITEM_KEYS } from './common';
-import { getDots, getLines, getViolin } from './series.custom';
 
 const getRenderItem =
-  (boxplotDataset: { source: IBoxplotDataItem[] }) =>
-  (params: CustomSeriesRenderItemParams, api: CustomSeriesRenderItemAPI) => {
+  (boxplotDataset: BoxplotDataset) => (params: CustomSeriesRenderItemParams, api: CustomSeriesRenderItemAPI) => {
     return {
       type: 'group',
       children: [
-        getViolin({ boxplotDataset, params, api }),
-        getLines({ boxplotDataset, params, api }),
+        // getViolin({ boxplotDataset, params, api }),
+        // getLines({ boxplotDataset, params, api }),
         getDots({ boxplotDataset, params, api }),
       ],
     };
   };
 
 export function getSeries(conf: IBoxplotChartConf, dataset: any[]) {
-  const { color } = conf;
   return [
+    {
+      name: 'Placeholder Box',
+      type: 'boxplot',
+      ignore: true,
+      silent: true,
+      itemStyle: {
+        color: 'none',
+        borderColor: 'none',
+        opacity: 0,
+      },
+      boxWidth: [20, 80],
+      datasetIndex: 0,
+      encode: {
+        y: BOXPLOT_DATA_ITEM_KEYS,
+        x: 'name',
+        itemName: ['name'],
+        tooltip: BOXPLOT_DATA_ITEM_KEYS,
+      },
+    },
+    {
+      name: 'Placeholder Outlier',
+      type: 'scatter',
+      symbolSize: 7,
+      itemStyle: {
+        color: 'transparent',
+      },
+      ignore: true,
+      silent: true,
+      emphasis: {
+        scale: 2,
+      },
+      datasetIndex: 1,
+    },
     // {
-    //   name: 'Placeholder Box',
+    //   name: 'Box',
     //   type: 'boxplot',
-    //   ignore: true,
-    //   silent: true,
     //   itemStyle: {
-    //     color: 'none',
-    //     borderColor: 'none',
-    //     opacity: 0,
+    //     color,
+    //     borderColor: '#2F8CC0',
+    //     borderWidth: 2,
+    //   },
+    //   emphasis: {
+    //     disabled: true,
     //   },
     //   boxWidth: [10, 40],
     //   datasetIndex: 0,
@@ -38,51 +70,20 @@ export function getSeries(conf: IBoxplotChartConf, dataset: any[]) {
     //     tooltip: BOXPLOT_DATA_ITEM_KEYS,
     //   },
     // },
-    {
-      name: 'Box',
-      type: 'boxplot',
-      itemStyle: {
-        color,
-        borderColor: '#2F8CC0',
-        borderWidth: 2,
-      },
-      emphasis: {
-        disabled: true,
-      },
-      boxWidth: [10, 40],
-      datasetIndex: 0,
-      encode: {
-        y: BOXPLOT_DATA_ITEM_KEYS,
-        x: 'name',
-        itemName: ['name'],
-        tooltip: BOXPLOT_DATA_ITEM_KEYS,
-      },
-    },
-    {
-      name: 'Outlier',
-      type: 'scatter',
-      symbolSize: 7,
-      itemStyle: {
-        color: '#ED6A45',
-      },
-      emphasis: {
-        scale: 2,
-      },
-      datasetIndex: 1,
-    },
-    {
-      name: 'Scatter',
-      type: 'custom',
-      renderItem: getRenderItem(dataset[0]),
-      symbolSize: 3,
-      itemStyle: {
-        color: '#ED6A45',
-      },
-      emphasis: {
-        scale: 2,
-      },
-      xAxisIndex: 0,
-      datasetIndex: 0,
-    },
+    getCustomBoxplot(dataset[0], conf),
+    // {
+    //   name: 'Scatter',
+    //   type: 'custom',
+    //   renderItem: getRenderItem(dataset[0]),
+    //   symbolSize: 3,
+    //   itemStyle: {
+    //     color: '#ED6A45',
+    //   },
+    //   emphasis: {
+    //     scale: 2,
+    //   },
+    //   xAxisIndex: 0,
+    //   datasetIndex: 0,
+    // },
   ];
 }
