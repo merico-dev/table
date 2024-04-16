dc34d939576aafe9bab8fa37f92a4f6f82f43cde
Leto <gerile.tu@merico.dev>
2024-04-16 14:02:21 +0800

    i18n(settings-form): add initial i18n things

---
+++ settings-form/src/i18n/en.ts
@@ -0,0 +1,19 @@
+export const en = {
+  translation: {
+    system_settings: {
+      label: 'System Settings',
+    },
+    datasource: {
+      label: 'Data Source',
+    },
+    account: {
+      label: 'Account',
+    },
+    global_sql_snippet: {
+      label: 'SQL Snippet',
+    },
+    api_key: {
+      label: 'API Key',
+    },
+  },
+};
---
+++ settings-form/src/i18n/zh.ts
@@ -0,0 +1,19 @@
+export const zh = {
+  translation: {
+    system_settings: {
+      label: '系统设置',
+    },
+    datasource: {
+      label: '数据源',
+    },
+    account: {
+      label: '账户',
+    },
+    global_sql_snippet: {
+      label: '全局SQL片段',
+    },
+    api_key: {
+      label: 'API令牌',
+    },
+  },
+};
