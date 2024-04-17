1c8f111e373e8ad04fb333de0e1bb5b1bab1e4fd
Leto <gerile.tu@merico.dev>
2024-04-17 15:54:27 +0800

    chore(website):  misc

--- website/src/frames/admin/page-links.tsx
+++ website/src/frames/admin/page-links.tsx
@@ -8,7 +8,7 @@ export type LinkType = {
 
 export const adminPageLinks: LinkType[] = [
   { name: 'Data Sources', to: '/admin/data_source/list', icon: <IconDatabase size={16} color="#868e96" /> },
-  { name: 'SQL Snippets', to: '/admin/sql_snippet/list', icon: <IconClipboardCopy size={16} color="#868e96" /> },
+  { name: 'Global SQL Snippets', to: '/admin/sql_snippet/list', icon: <IconClipboardCopy size={16} color="#868e96" /> },
   { name: 'Accounts', to: '/admin/account/list', icon: <IconUsers size={16} color="#868e96" /> },
   { name: 'API Keys', to: '/admin/api_key/list', icon: <IconKey size={16} color="#868e96" /> },
   { name: 'Status', to: '/admin/status', icon: <IconInfoCircle size={16} color="#868e96" /> },
