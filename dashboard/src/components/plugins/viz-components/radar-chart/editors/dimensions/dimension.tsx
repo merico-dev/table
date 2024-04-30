2960d658ca6ea81ca5c1e6439e8bd54a537b9293
Leto <gerile.tu@merico.dev>
2024-04-30 10:43:42 +0800

    feat(dashboard): apply FieldArrayTabs to radar chart

--- dashboard/src/components/plugins/viz-components/radar-chart/editors/dimensions/dimension.tsx
+++ dashboard/src/components/plugins/viz-components/radar-chart/editors/dimensions/dimension.tsx
@@ -1,18 +1,16 @@
-import { Button, Divider, Group, Stack, TextInput } from '@mantine/core';
-import { Control, Controller, UseFieldArrayRemove } from 'react-hook-form';
-import { Trash } from 'tabler-icons-react';
+import { Divider, Group, Stack, TextInput } from '@mantine/core';
+import { Control, Controller } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
 import { DataFieldSelector } from '~/components/panel/settings/common/data-field-selector';
 import { NumbroFormatSelector } from '~/components/panel/settings/common/numbro-format-selector';
 import { IRadarChartConf } from '../../type';
-import { useTranslation } from 'react-i18next';
 
 interface IDimensionField {
   control: Control<IRadarChartConf, $TSFixMe>;
   index: number;
-  remove: UseFieldArrayRemove;
 }
 
-export function DimensionField({ control, index, remove }: IDimensionField) {
+export function DimensionField({ control, index }: IDimensionField) {
   const { t } = useTranslation();
   return (
     <Stack my={0} p="md" pr={40} sx={{ border: '1px solid #eee', position: 'relative' }}>
@@ -52,16 +50,6 @@ export function DimensionField({ control, index, remove }: IDimensionField) {
           render={({ field }) => <NumbroFormatSelector {...field} />}
         />
       </Stack>
-      <Button
-        mt={20}
-        leftIcon={<Trash size={16} />}
-        color="red"
-        variant="light"
-        onClick={() => remove(index)}
-        disabled={index === 0}
-      >
-        {t('viz.radar_chart.metric.delete')}
-      </Button>
     </Stack>
   );
 }
