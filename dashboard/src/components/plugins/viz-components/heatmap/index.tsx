0b03dbff172f5eba87cf0b6f03eb15f8a4564f6d
Leto <gerile.tu@merico.dev>
2024-05-12 20:38:43 +0800

    refactor(dashboard): add migrators dir to heatmap

--- dashboard/src/components/plugins/viz-components/heatmap/index.tsx
+++ dashboard/src/components/plugins/viz-components/heatmap/index.tsx
@@ -6,72 +6,7 @@ import { DEFAULT_CONFIG, IHeatmapConf } from './type';
 import { ClickHeatBlock } from './triggers';
 import _ from 'lodash';
 import { translation } from './translation';
-
-function v2(legacyConf: any, { panelModel }: IMigrationEnv): IHeatmapConf {
-  try {
-    const queryID = panelModel.queryIDs[0];
-    if (!queryID) {
-      throw new Error('cannot migrate when queryID is empty');
-    }
-    const changeKey = (key: string) => (key ? `${queryID}.${key}` : key);
-    const { x_axis, y_axis, heat_block, tooltip, ...rest } = legacyConf;
-    return {
-      ...rest,
-      x_axis: {
-        ...x_axis,
-        data_key: changeKey(x_axis.data_key),
-      },
-      y_axis: {
-        ...y_axis,
-        data_key: changeKey(y_axis.data_key),
-      },
-      heat_block: {
-        ...heat_block,
-        data_key: changeKey(heat_block.data_key),
-      },
-      tooltip: {
-        ...tooltip,
-        metrics: tooltip.metrics.map((m: any) => ({
-          ...m,
-          data_key: changeKey(m.data_key),
-        })),
-      },
-    };
-  } catch (error) {
-    console.error('[Migration failed]', error);
-    throw error;
-  }
-}
-
-function v3(legacyConf: any): IHeatmapConf {
-  return _.defaultsDeep({}, legacyConf, { heat_block: { label: { show: false, fontSize: 10 } } });
-}
-
-function v4(legacyConf: any): IHeatmapConf {
-  const { heat_block } = legacyConf;
-  let { min, max } = heat_block;
-  if (typeof min !== 'number') {
-    min = 0;
-  }
-  if (typeof max !== 'number') {
-    max = 100;
-  }
-
-  return {
-    ...legacyConf,
-    heat_block: {
-      ...heat_block,
-      min: {
-        type: 'static',
-        value: min,
-      },
-      max: {
-        type: 'static',
-        value: max,
-      },
-    },
-  };
-}
+import * as Migrators from './migrators';
 
 class VizHeatmapMigrator extends VersionBasedMigrator {
   readonly VERSION = 4;
@@ -88,21 +23,21 @@ class VizHeatmapMigrator extends VersionBasedMigrator {
       return {
         ...data,
         version: 2,
-        config: v2(data.config, env),
+        config: Migrators.v2(data.config, env),
       };
     });
     this.version(3, (data) => {
       return {
         ...data,
         version: 3,
-        config: v3(data.config),
+        config: Migrators.v3(data.config),
       };
     });
     this.version(4, (data) => {
       return {
         ...data,
         version: 4,
-        config: v4(data.config),
+        config: Migrators.v4(data.config),
       };
     });
   }
