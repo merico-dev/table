1ab9e39e4207d96f84796241922bb57c0b4f35b7
Leto <gerile.tu@merico.dev>
2024-05-13 22:20:02 +0800

    refactor(dashboard): extract PreviewGradientAndApplyPalette

--- dashboard/src/components/plugins/common-echarts-fields/visual-map/visual-map-editor/gradient-editor.tsx
+++ dashboard/src/components/plugins/common-echarts-fields/visual-map/visual-map-editor/gradient-editor.tsx
@@ -1,27 +1,13 @@
-import {
-  Badge,
-  Box,
-  CloseButton,
-  ColorInput,
-  Group,
-  Menu,
-  Stack,
-  Table,
-  Text,
-  Tooltip,
-  UnstyledButton,
-} from '@mantine/core';
-import { IconSelector } from '@tabler/icons-react';
+import { Badge, CloseButton, ColorInput, Stack, Table } from '@mantine/core';
 import { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { v4 as uuidv4 } from 'uuid';
-import { getVisualMapPalettes } from '../utils';
+import { PreviewGradientAndApplyPalette } from './preview-gradient-and-apply-palette';
 
 type Props = {
   value: string[];
   onChange: (v: string[]) => void;
 };
-const palettes = getVisualMapPalettes();
 
 export const GrandientEditor = ({ value, onChange }: Props) => {
   const { t } = useTranslation();
@@ -56,72 +42,11 @@ export const GrandientEditor = ({ value, onChange }: Props) => {
       setNewColor('');
     }
   };
-  const backgroundImage = `linear-gradient(to right,  ${value.join(', ')})`;
+
   return (
     <Stack>
       <Stack mt={-6}>
-        <Menu shadow="md" width={200}>
-          <Menu.Target>
-            <UnstyledButton>
-              <Text
-                style={{
-                  display: 'inline-block',
-                  fontSize: '0.875rem',
-                  fontWeight: 500,
-                  color: '#212529',
-                  wordBreak: 'break-word',
-                  cursor: 'default',
-                }}
-              >
-                {t('chart.color.color_gradient')}
-              </Text>
-              <Group
-                style={{
-                  padding: '0.4375rem 0.625rem',
-                  border: '0.0625rem solid #ced4da',
-                  borderRadius: '0.25rem',
-                  columnGap: 10,
-                }}
-              >
-                <Box
-                  style={{
-                    height: '20px',
-                    flexGrow: 1,
-                    backgroundImage,
-                    borderRadius: 4,
-                  }}
-                />
-                <IconSelector size={14} color="rgb(134, 142, 150)" />
-              </Group>
-            </UnstyledButton>
-          </Menu.Target>
-
-          <Menu.Dropdown>
-            <Menu.Label>{t('chart.visual_map.built_in_palettes')}</Menu.Label>
-            <Menu.Divider />
-
-            {Object.entries(palettes).map(([name, colors]) => (
-              <Tooltip key={name} label={t('chart.visual_map.use_palette_x', { x: name })}>
-                <Menu.Item>
-                  <Box
-                    style={{
-                      height: '20px',
-                      width: '100%',
-                      backgroundImage: `linear-gradient(to right,  ${colors.join(', ')})`,
-                      borderRadius: 4,
-                      boxShadow: '0 0 3px 0 #eee',
-                      cursor: 'pointer',
-                    }}
-                    onClick={() => {
-                      replace(colors);
-                    }}
-                    title={name}
-                  />
-                </Menu.Item>
-              </Tooltip>
-            ))}
-          </Menu.Dropdown>
-        </Menu>
+        <PreviewGradientAndApplyPalette colors={value} applyPalette={replace} />
         <Table withBorder={false} withColumnBorders={false} sx={{ td: { borderTop: 'none !important' } }}>
           <tbody>
             {colors.map((c, index) => (
---
+++ dashboard/src/components/plugins/common-echarts-fields/visual-map/visual-map-editor/preview-gradient-and-apply-palette.tsx
@@ -0,0 +1,78 @@
+import { Box, Group, Menu, Text, Tooltip, UnstyledButton } from '@mantine/core';
+import { IconSelector } from '@tabler/icons-react';
+import { useTranslation } from 'react-i18next';
+import { getVisualMapPalettes } from '../utils';
+const palettes = getVisualMapPalettes();
+
+type Props = {
+  colors: string[];
+  applyPalette: (colors: string[]) => void;
+};
+export const PreviewGradientAndApplyPalette = ({ colors, applyPalette }: Props) => {
+  const { t } = useTranslation();
+  const backgroundImage = `linear-gradient(to right,  ${colors.join(', ')})`;
+  return (
+    <Menu shadow="md" width={200}>
+      <Menu.Target>
+        <UnstyledButton>
+          <Text
+            style={{
+              display: 'inline-block',
+              fontSize: '0.875rem',
+              fontWeight: 500,
+              color: '#212529',
+              wordBreak: 'break-word',
+              cursor: 'default',
+            }}
+          >
+            {t('chart.color.color_gradient')}
+          </Text>
+          <Group
+            style={{
+              padding: '0.4375rem 0.625rem',
+              border: '0.0625rem solid #ced4da',
+              borderRadius: '0.25rem',
+              columnGap: 10,
+            }}
+          >
+            <Box
+              style={{
+                height: '20px',
+                flexGrow: 1,
+                backgroundImage,
+                borderRadius: 4,
+              }}
+            />
+            <IconSelector size={14} color="rgb(134, 142, 150)" />
+          </Group>
+        </UnstyledButton>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Label>{t('chart.visual_map.built_in_palettes')}</Menu.Label>
+        <Menu.Divider />
+
+        {Object.entries(palettes).map(([name, colors]) => (
+          <Tooltip key={name} label={t('chart.visual_map.use_palette_x', { x: name })}>
+            <Menu.Item>
+              <Box
+                style={{
+                  height: '20px',
+                  width: '100%',
+                  backgroundImage: `linear-gradient(to right,  ${colors.join(', ')})`,
+                  borderRadius: 4,
+                  boxShadow: '0 0 3px 0 #eee',
+                  cursor: 'pointer',
+                }}
+                onClick={() => {
+                  applyPalette(colors);
+                }}
+                title={name}
+              />
+            </Menu.Item>
+          </Tooltip>
+        ))}
+      </Menu.Dropdown>
+    </Menu>
+  );
+};
