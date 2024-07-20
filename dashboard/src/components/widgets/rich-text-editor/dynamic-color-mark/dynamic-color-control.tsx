aa42bc15f1e1798fe354be9e306fbde3d3d3fdac
Leto <gerile.tu@merico.dev>
2024-07-20 21:34:15 +0800

    fix(dashboard): can't clear dynamic-color nicely

--- dashboard/src/components/widgets/rich-text-editor/dynamic-color-mark/dynamic-color-control.tsx
+++ dashboard/src/components/widgets/rich-text-editor/dynamic-color-mark/dynamic-color-control.tsx
@@ -1,5 +1,5 @@
 import { ActionIcon, Tooltip } from '@mantine/core';
-import { IconMathFunction } from '@tabler/icons-react';
+import { IconMathFunction, IconMathFunctionOff } from '@tabler/icons-react';
 import '@tiptap/extension-text-style';
 import { Editor } from '@tiptap/react';
 import { useTranslation } from 'react-i18next';
@@ -33,22 +33,41 @@ export const DynamicColorControl = ({ editor }: { editor: Editor }) => {
   const { t } = useTranslation();
   const currentDynamicColor = editor.getAttributes(DynamicColorName)[DynamicColorAttrKey] ?? '';
   return (
-    <ModalFunctionEditor
-      title={t('rich_text.dynamic_color.edit')}
-      label=""
-      triggerLabel={''}
-      value={currentDynamicColor}
-      onChange={(v: string) => {
-        if (!v) {
-          editor.chain().focus().unsetDynamicColor().run();
-        } else {
-          editor.chain().focus().setDynamicColor(v).run();
-        }
-      }}
-      defaultValue={DefaultDynamicColorFunc}
-      renderTriggerButton={renderTriggerButton}
-      restrictions={getDynamicColorRestrictions(currentDynamicColor)}
-      zIndex={340}
-    />
+    <>
+      <ModalFunctionEditor
+        title={t('rich_text.dynamic_color.edit')}
+        label=""
+        triggerLabel={''}
+        value={currentDynamicColor}
+        onChange={(v: string) => {
+          if (!v) {
+            editor.chain().focus().unsetDynamicColor().run();
+          } else {
+            editor.chain().focus().setDynamicColor(v).run();
+          }
+        }}
+        defaultValue={DefaultDynamicColorFunc}
+        renderTriggerButton={renderTriggerButton}
+        restrictions={getDynamicColorRestrictions(currentDynamicColor)}
+        zIndex={340}
+      />
+      <Tooltip label={t('rich_text.dynamic_color.clear')}>
+        <ActionIcon
+          variant="default"
+          data-rich-text-editor-control="true"
+          sx={{
+            height: '26px',
+            minHeight: '26px',
+            lineHeight: '26px',
+            borderColor: '#ced4da !important',
+            color: '#000',
+          }}
+          disabled={!currentDynamicColor}
+          onClick={() => editor.chain().focus().unsetDynamicColor().run()}
+        >
+          <IconMathFunctionOff stroke={1.5} size={16} />
+        </ActionIcon>
+      </Tooltip>
+    </>
   );
 };
