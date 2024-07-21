e6929163f7c05152bef92f3df374ec0ed8bfb4c7
Leto <gerile.tu@merico.dev>
2024-07-21 18:17:51 +0800

    fix(dashboard): lingering editor state on init or update

--- dashboard/src/components/widgets/function-editor/function-editor.tsx
+++ dashboard/src/components/widgets/function-editor/function-editor.tsx
@@ -1,11 +1,15 @@
-import Editor, { OnMount } from '@monaco-editor/react';
-
+import Editor, { Monaco, OnMount } from '@monaco-editor/react';
+import { useEffect, useRef } from 'react';
+import * as monaco from 'monaco-editor';
 interface IFunctionEditor {
   value: TFunctionString;
   onChange: (v: TFunctionString) => void;
   onMount?: OnMount;
 }
 export const FunctionEditor = ({ value, onChange, onMount }: IFunctionEditor) => {
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const monacoRef = useRef<Monaco | null>(null);
+
   const changeContent = (v?: TFunctionString) => {
     if (!v) {
       onChange('');
@@ -14,6 +18,24 @@ export const FunctionEditor = ({ value, onChange, onMount }: IFunctionEditor) =>
     onChange(v);
   };
 
+  useEffect(() => {
+    const model = editorRef.current?.getModel();
+    if (!model) {
+      return;
+    }
+    if (!model.getValue()) {
+      // init from blank
+      model.setValue(value);
+    }
+  }, [value]);
+
+  const handleOnMount: OnMount = (editor, monaco) => {
+    editorRef.current = editor;
+    monacoRef.current = monaco;
+    onMount?.(editor, monaco);
+  };
+
+  console.log({ value });
   return (
     <Editor
       className="function-editor"
@@ -21,7 +43,6 @@ export const FunctionEditor = ({ value, onChange, onMount }: IFunctionEditor) =>
       defaultLanguage="javascript"
       // https://github.com/suren-atoyan/monaco-react/issues/402
       defaultValue={value}
-      value={value}
       onChange={changeContent}
       theme="vs-dark"
       options={{
@@ -34,7 +55,7 @@ export const FunctionEditor = ({ value, onChange, onMount }: IFunctionEditor) =>
           enabled: false,
         },
       }}
-      onMount={onMount}
+      onMount={handleOnMount}
     />
   );
 };
