14b9c8be0de1aea646e8f5e64fe1e27c8725ff23
Leto <gerile.tu@merico.dev>
2024-11-12 12:22:07 +0800

    feat(dashboard): add DeleteQuery to MMQ

--- dashboard/src/dashboard-editor/ui/settings/content/edit-query/merico-metric-query-editor-form/merico-icons/types.ts
+++ dashboard/src/dashboard-editor/ui/settings/content/edit-query/merico-metric-query-editor-form/merico-icons/types.ts
@@ -1,4 +1,5 @@
 export type MericoIconProps = {
   width: number | string;
   height: number | string;
+  color?: string;
 };
--- dashboard/src/dashboard-editor/ui/settings/content/edit-query/merico-metric-query-editor-form/more-actions.tsx
+++ dashboard/src/dashboard-editor/ui/settings/content/edit-query/merico-metric-query-editor-form/more-actions.tsx
@@ -1,11 +1,67 @@
-import { ActionIcon, Menu } from '@mantine/core';
+import { ActionIcon, Button, Menu, Tooltip } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 import { QueryModelInstance } from '~/dashboard-editor/model';
 import { MericoIconCopy, MericoIconDelete, MericoIconMore } from './merico-icons';
+import { useEditContentModelContext, useEditDashboardContext } from '~/contexts';
+import { useModals } from '@mantine/modals';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
   queryModel: QueryModelInstance;
 };
+
+const DeleteQuery = observer(({ queryModel }: Props) => {
+  const { t } = useTranslation();
+  const model = useEditDashboardContext();
+  const content = useEditContentModelContext();
+  const usage = content.findQueryUsage(queryModel.id);
+  const disabled = usage.length > 0;
+
+  const modals = useModals();
+  const remove = () => {
+    modals.openConfirmModal({
+      title: `${t('query.delete')}?`,
+      labels: { confirm: t('common.actions.confirm'), cancel: t('common.actions.cancel') },
+      onCancel: () => console.log('Cancel'),
+      onConfirm: () => {
+        content.queries.removeQuery(queryModel.id);
+        model.editor.setPath(['_QUERIES_', '']);
+      },
+      confirmProps: { color: 'red' },
+      zIndex: 320,
+    });
+  };
+
+  if (disabled) {
+    return (
+      <Tooltip
+        label="此查询在使用中，不能删除。请查看“设置”标签页以了解详情"
+        withArrow
+        events={{ hover: true, focus: false, touch: false }}
+        withinPortal
+        zIndex={320}
+        position="bottom-end"
+      >
+        <Button
+          leftSection={<MericoIconDelete width={14} height={14} color="gray" />}
+          variant="subtle"
+          color="gray"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          删除查询
+        </Button>
+      </Tooltip>
+    );
+  }
+  return (
+    <Menu.Item leftSection={<MericoIconDelete width={14} height={14} />} color="red" onClick={remove}>
+      删除查询
+    </Menu.Item>
+  );
+});
+
 export const MoreActions = observer(({ queryModel }: Props) => {
   return (
     <Menu shadow="md" width={120} trigger="hover">
@@ -17,9 +73,7 @@ export const MoreActions = observer(({ queryModel }: Props) => {
 
       <Menu.Dropdown>
         <Menu.Item leftSection={<MericoIconCopy width={14} height={14} />}>复制API</Menu.Item>
-        <Menu.Item leftSection={<MericoIconDelete width={14} height={14} />} color="red">
-          删除查询
-        </Menu.Item>
+        <DeleteQuery queryModel={queryModel} />
       </Menu.Dropdown>
     </Menu>
   );
