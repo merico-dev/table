b23db1de31983dbbb6610ae0d174e7fc70b2a0c0
Leto <gerile.tu@merico.dev>
2024-11-27 12:39:29 +0800

    feat(dashboard): disable EditMetricQuery when metricID is not set

--- dashboard/src/dashboard-editor/ui/settings/content/edit-query/merico-metric-query-editor-form/query-tabs/edit-metric-query/edit-metric-query.tsx
+++ dashboard/src/dashboard-editor/ui/settings/content/edit-query/merico-metric-query-editor-form/query-tabs/edit-metric-query/edit-metric-query.tsx
@@ -1,16 +1,28 @@
-import { Stack } from '@mantine/core';
+import { Center, Overlay, Stack, Text } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 import { QueryModelInstance } from '~/dashboard-editor/model';
 import { LinkMetricsToVariables } from './link-metrics-to-variables';
 import { LinkMetricsToTimeAndStep } from './link-metrics-to-time-and-step';
 import { SetGroupByMetrics } from './set-groupby-metrics';
+import { MericoMetricQueryMetaInstance } from '~/model';
 
 type Props = {
   queryModel: QueryModelInstance;
 };
 export const EditMetricQuery = observer(({ queryModel }: Props) => {
+  const config = queryModel.config as MericoMetricQueryMetaInstance;
+  const metricID = config.id;
   return (
-    <Stack gap={16}>
+    <Stack gap={16} pos="relative">
+      {!metricID && (
+        <Overlay color="#fff" backgroundOpacity={0.5} blur={3}>
+          <Center h="100%">
+            <Text size="sm" c="black">
+              请先选择指标
+            </Text>
+          </Center>
+        </Overlay>
+      )}
       <LinkMetricsToVariables queryModel={queryModel} />
       <LinkMetricsToTimeAndStep queryModel={queryModel} />
       <SetGroupByMetrics queryModel={queryModel} />
