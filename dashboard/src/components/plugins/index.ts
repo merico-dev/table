1569ead91460e60b9ed64d7c632d6e1dc3cb7c12
ZeekoZhu <tao.zhu@merico.dev>
2025-04-01 10:59:15 +0800

    feat(dashboard): export plugin system types

--- dashboard/src/components/plugins/index.ts
+++ dashboard/src/components/plugins/index.ts
@@ -4,3 +4,6 @@ export * from './plugin-data-migrator';
 export * from './hooks';
 export * from './color-manager';
 export { onVizRendered, notifyVizRendered } from './viz-components/viz-instance-api';
+export { ServiceLocator, Token } from './service/service-locator';
+export { useServiceLocator } from './service/service-locator/use-service-locator';
+export type { IServiceLocator, IDisposable } from './service/service-locator';
