23f2471d7663cdcfc3f0ae8cbeac50bb3210a3da
Leto <gerile.tu@merico.dev>
2025-04-18 13:19:04 +0800

    feat(dashboard): show unit in legend

--- dashboard/src/components/plugins/viz-components/cartesian/option/legend.ts
+++ dashboard/src/components/plugins/viz-components/cartesian/option/legend.ts
@@ -1,3 +1,4 @@
+import { ICartesianChartConf } from '../type';
 import { IEchartsSeriesItem, TEchartsSeriesType } from './utils/types';
 
 function getStyle(type: TEchartsSeriesType) {
@@ -22,12 +23,23 @@ function getIcon(type: TEchartsSeriesType) {
   }
 }
 
-export function getLegend(series: IEchartsSeriesItem[]) {
+export function getLegend(conf: ICartesianChartConf, series: IEchartsSeriesItem[]) {
+  const unitMap = conf.series.reduce((ret, { unit, name }) => {
+    if (unit.show_in_legend) {
+      ret[name] = unit.text;
+    }
+    return ret;
+  }, {} as Record<string, string>);
+
   const ret: Record<string, any> = {
     show: true,
     bottom: 0,
     left: 'center',
     type: 'scroll',
+    formatter: (name: string) => {
+      const unit = unitMap[name];
+      return !!unit ? `${name}(${unit})` : name;
+    },
   };
 
   ret.data = series
