581e05d9739ffdb7878862baa9c10050adf5366a
Leto <gerile.tu@merico.dev>
2025-05-27 12:59:10 +0800

    feat(dashboard): add tooltip prop to ItemBadge

--- dashboard/src/components/plugins/viz-components/viz-dashboard-state/render/item/item-badge.tsx
+++ dashboard/src/components/plugins/viz-components/viz-dashboard-state/render/item/item-badge.tsx
@@ -1,16 +1,20 @@
 import { forwardRef } from 'react';
 import classes from './item-badge.module.css';
+import { Tooltip } from '@mantine/core';
 
 type Props = {
   label: string;
   value: string;
+  tooltip?: string;
 };
 
-export const ItemBadge = forwardRef<HTMLDivElement, Props>(({ label, value }, ref) => {
+export const ItemBadge = forwardRef<HTMLDivElement, Props>(({ label, value, tooltip }, ref) => {
   return (
     <div ref={ref} className={classes.item_badge}>
       <div className={classes.label}>{label}</div>
-      <div className={classes.value}>{value}</div>
+      <Tooltip label={tooltip} disabled={!tooltip}>
+        <div className={classes.value}>{value}</div>
+      </Tooltip>
     </div>
   );
 });
