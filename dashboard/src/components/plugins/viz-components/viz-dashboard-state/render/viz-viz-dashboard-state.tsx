da049039da18cece6878ccb235d8b7f26f8e9ad7
Leto <gerile.tu@merico.dev>
2025-05-27 17:26:24 +0800

    feat(dashboard): misc

--- dashboard/src/components/plugins/viz-components/viz-dashboard-state/render/viz-viz-dashboard-state.tsx
+++ dashboard/src/components/plugins/viz-components/viz-dashboard-state/render/viz-viz-dashboard-state.tsx
@@ -7,12 +7,24 @@ import { StateItems } from './state-items';
 
 export const VizVizDashboardState = observer(({ context }: VizViewProps) => {
   const { value: conf } = useStorageData<IVizDashboardStateConf>(context.instanceData, 'config');
+  const { width, height } = context.viewport;
   if (!conf) {
     return null;
   }
 
   return (
-    <Group justify="xs" px={10} pt={28}>
+    <Group
+      justify="flex-start"
+      align="flex-start"
+      wrap="wrap"
+      px={0}
+      py={0}
+      gap="xs"
+      w={width}
+      mah={height}
+      style={{ overflow: 'auto' }}
+      data-enable-scrollbar
+    >
       <StateItems conf={conf} />
     </Group>
   );
