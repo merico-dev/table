561656ffd8683092741eef3fa982664244df5c46
Leto <gerile.tu@merico.dev>
2025-05-27 14:27:03 +0800

    feat(dashboard): add SingleSelectState

---
+++ dashboard/src/components/plugins/viz-components/viz-dashboard-state/render/item/single-select-state.tsx
@@ -0,0 +1,32 @@
+import { observer } from 'mobx-react-lite';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  DashboardFilterType,
+  FilterSelectConfigInstance,
+  TDashboardStateItem,
+  TDashboardStateItem_Filter,
+} from '~/model';
+import { ItemBadge } from './item-badge';
+
+type Props = {
+  item: TDashboardStateItem_Filter;
+};
+const _SingleSelectState = observer(({ item }: Props) => {
+  const { t } = useTranslation();
+  const model = item.model;
+  const config = model.config as FilterSelectConfigInstance;
+  const labelOfSelection = useMemo(() => {
+    return config.options.find((o) => o.value === item.value)?.label ?? item.value;
+  }, [item.value, config.options]);
+
+  return <ItemBadge label={item.label} value={labelOfSelection} label_tooltip={item.key} value_tooltip={item.value} />;
+});
+
+export const SingleSelectState = ({ item }: { item: TDashboardStateItem }) => {
+  if (item.type !== DashboardFilterType.Select) {
+    return null;
+  }
+
+  return <_SingleSelectState item={item} />;
+};
--- dashboard/src/components/plugins/viz-components/viz-dashboard-state/render/item/state-item.tsx
+++ dashboard/src/components/plugins/viz-components/viz-dashboard-state/render/item/state-item.tsx
@@ -1,6 +1,7 @@
 import { Badge } from '@mantine/core';
 import { DashboardFilterType, TDashboardStateItem } from '~/model';
 import { DateRangeState } from './date-range-state';
+import { SingleSelectState } from './single-select-state';
 
 type Props = {
   item: TDashboardStateItem;
@@ -10,6 +11,8 @@ export const StateItem = ({ item }: Props) => {
   switch (item.type) {
     case DashboardFilterType.DateRange:
       return <DateRangeState item={item} />;
+    case DashboardFilterType.Select:
+      return <SingleSelectState item={item} />;
     default:
       return (
         <Badge variant="default" color="blue" radius="xs">
