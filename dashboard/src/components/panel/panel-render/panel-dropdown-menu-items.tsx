b26f0299d79288096d137dd86b64878f8dc79b89
Leto <gerile.tu@merico.dev>
2025-07-31 21:18:23 +0800

    feat(dashboard): able to sort panel menu items before rendering them

--- dashboard/src/components/panel/panel-render/panel-dropdown-menu-items.tsx
+++ dashboard/src/components/panel/panel-render/panel-dropdown-menu-items.tsx
@@ -2,48 +2,85 @@ import { observer } from 'mobx-react-lite';
 
 import { Menu } from '@mantine/core';
 import { IconArrowsMaximize, IconCamera, IconDownload, IconRefresh } from '@tabler/icons-react';
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import { useTranslation } from 'react-i18next';
 import { useAdditionalPanelMenuItems } from '~/contexts';
 import { DashboardActionContext } from '~/contexts/dashboard-action-context';
 import { useRenderPanelContext } from '~/contexts/panel-context';
 import { EViewComponentType, ViewMetaInstance } from '~/model';
+import { PanelMenuItem } from '~/types';
+
+const useItems = (view: ViewMetaInstance) => {
+  const { t } = useTranslation();
+  const { panel, downloadPanelScreenshot } = useRenderPanelContext();
+  const { items: additionalItems } = useAdditionalPanelMenuItems();
+
+  const { viewPanelInFullScreen, inFullScreen } = React.useContext(DashboardActionContext);
+
+  const { id } = panel;
+  const enterFullScreen = useCallback(() => {
+    viewPanelInFullScreen(id);
+  }, [id, viewPanelInFullScreen]);
+  const showFullScreenOption = !inFullScreen && view.type !== EViewComponentType.Modal;
+
+  return useMemo(() => {
+    const ret: PanelMenuItem[] = [
+      {
+        order: 10,
+        render: () => (
+          <Menu.Item onClick={panel.refreshData} leftSection={<IconRefresh size={14} />}>
+            {t('common.actions.refresh')}
+          </Menu.Item>
+        ),
+      },
+      {
+        order: 20,
+        render: () => (
+          <Menu.Item onClick={panel.downloadData} leftSection={<IconDownload size={14} />}>
+            {t('common.actions.download_data')}
+          </Menu.Item>
+        ),
+      },
+      {
+        order: 30,
+        render: () => (
+          <Menu.Item onClick={downloadPanelScreenshot} leftSection={<IconCamera size={14} />}>
+            {t('common.actions.download_screenshot')}
+          </Menu.Item>
+        ),
+      },
+      {
+        order: 40,
+        render: () => (
+          <Menu.Item onClick={downloadPanelScreenshot} leftSection={<IconCamera size={14} />}>
+            {t('common.actions.download_screenshot')}
+          </Menu.Item>
+        ),
+      },
+      ...additionalItems,
+    ];
+    if (showFullScreenOption) {
+      ret.push({
+        order: 50,
+        render: () => (
+          <Menu.Item onClick={enterFullScreen} leftSection={<IconArrowsMaximize size={14} />}>
+            {t('common.actions.enter_fullscreen')}
+          </Menu.Item>
+        ),
+      });
+    }
+
+    return ret.sort((a, b) => a.order - b.order);
+  }, [additionalItems, showFullScreenOption]);
+};
 
 type Props = {
   view: ViewMetaInstance;
 };
 
 export const PanelDropdownMenuItems = observer(({ view }: Props) => {
-  const { t } = useTranslation();
-  const { panel, downloadPanelScreenshot, echartsOptions } = useRenderPanelContext();
-  const { items: additionalItems } = useAdditionalPanelMenuItems();
-
-  const { id } = panel;
-
-  const { viewPanelInFullScreen, inFullScreen } = React.useContext(DashboardActionContext);
-
-  const enterFullScreen = React.useCallback(() => {
-    viewPanelInFullScreen(id);
-  }, [id, viewPanelInFullScreen]);
-  const showFullScreenOption = !inFullScreen && view.type !== EViewComponentType.Modal;
-  return (
-    <>
-      <Menu.Item onClick={panel.refreshData} leftSection={<IconRefresh size={14} />}>
-        {t('common.actions.refresh')}
-      </Menu.Item>
-      <Menu.Item onClick={panel.downloadData} leftSection={<IconDownload size={14} />}>
-        {t('common.actions.download_data')}
-      </Menu.Item>
-      <Menu.Item onClick={downloadPanelScreenshot} leftSection={<IconCamera size={14} />}>
-        {t('common.actions.download_screenshot')}
-      </Menu.Item>
-      {showFullScreenOption && (
-        <Menu.Item onClick={enterFullScreen} leftSection={<IconArrowsMaximize size={14} />}>
-          {t('common.actions.enter_fullscreen')}
-        </Menu.Item>
-      )}
-      {additionalItems.map((item) => item.render({ echartsOptions, inEditMode: false }))}
-    </>
-  );
+  const { echartsOptions } = useRenderPanelContext();
+  const items = useItems(view);
+  return items.map((item) => item.render({ echartsOptions, inEditMode: false }));
 });
