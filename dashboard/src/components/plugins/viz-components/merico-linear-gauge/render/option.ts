70e51c7858b1d96f76d9198e534c7a39d25bc419
Leto <gerile.tu@merico.dev>
2025-09-16 22:53:39 +0800

    feat(dashboard): render merico-linear-gauge regarding order

--- dashboard/src/components/plugins/viz-components/merico-linear-gauge/render/option.ts
+++ dashboard/src/components/plugins/viz-components/merico-linear-gauge/render/option.ts
@@ -1,8 +1,9 @@
 import _ from 'lodash';
+import { extractData } from '~/utils';
 import { IMericoLinearGaugeConf } from '../type';
-import { extractData, parseDataKey } from '~/utils';
 
 export const getOption = (conf: IMericoLinearGaugeConf, data: TPanelData) => {
+  const { order } = conf;
   const ret = {
     value: _.get(extractData(data, conf.value), '0', 'null'),
     sections: conf.sections.map((section) => {
@@ -16,15 +17,33 @@ export const getOption = (conf: IMericoLinearGaugeConf, data: TPanelData) => {
     }),
   };
   const v = ret.value;
-  for (let i = 0; i < ret.sections.length; i++) {
-    if (v < ret.sections[i].min) {
-      break;
+  if (order === 'asc') {
+    for (let i = 0; i < ret.sections.length; i++) {
+      const min = ret.sections[i].min;
+      if (v < min) {
+        break;
+      }
+      ret.sections[i].pointer = true;
+      ret.sections[i].pointer_equal = v === min;
+      if (i > 0) {
+        ret.sections[i - 1].pointer = false;
+        ret.sections[i - 1].pointer_equal = false;
+      }
     }
-    ret.sections[i].pointer = true;
-    ret.sections[i].pointer_equal = v === ret.sections[i].min;
-    if (i > 0) {
-      ret.sections[i - 1].pointer = false;
-      ret.sections[i - 1].pointer_equal = false;
+  }
+
+  if (order === 'desc') {
+    for (let i = ret.sections.length - 1; i >= 0; i--) {
+      const min = ret.sections[i].min;
+      ret.sections[i].pointer = true;
+      ret.sections[i].pointer_equal = v === min;
+      if (i < ret.sections.length - 1) {
+        ret.sections[i + 1].pointer = false;
+        ret.sections[i + 1].pointer_equal = false;
+      }
+      if (v <= min) {
+        break;
+      }
     }
   }
   return ret;
